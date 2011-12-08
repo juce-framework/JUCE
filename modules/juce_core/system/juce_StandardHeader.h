9b6780f15ae08999e790c8594a35807492780da5
jules <julianstorer@gmail.com>
2011-12-08 19:52:34 +0000

    Version number bump.

--- modules/juce_core/system/juce_StandardHeader.h
+++ modules/juce_core/system/juce_StandardHeader.h
@@ -33,7 +33,7 @@
 */
 #define JUCE_MAJOR_VERSION      2
 #define JUCE_MINOR_VERSION      0
-#define JUCE_BUILDNUMBER        11
+#define JUCE_BUILDNUMBER        12
 
 /** Current Juce version number.
 
