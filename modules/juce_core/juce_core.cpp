a6c9992957508fb7da8452bd114d2fe8bddcc48c
jules <julianstorer@gmail.com>
2012-02-24 18:12:21 +0000

    Updated some module usage warning code.

--- modules/juce_core/juce_core.cpp
+++ modules/juce_core/juce_core.cpp
@@ -23,7 +23,7 @@
   ==============================================================================
 */
 
-#ifdef __JUCE_CORE_JUCEHEADER__
+#if defined (__JUCE_CORE_JUCEHEADER__) && ! JUCE_AMALGAMATED_INCLUDE
  /* When you add this cpp file to your project, you mustn't include it in a file where you've
     already included any other headers - just put it inside a file on its own, possibly with your config
     flags preceding it, but don't include anything else. That also includes avoiding any automatic prefix
--- modules/juce_events/juce_events.cpp
+++ modules/juce_events/juce_events.cpp
@@ -23,7 +23,7 @@
   ==============================================================================
 */
 
-#if defined (__JUCE_EVENTS_JUCEHEADER__) && ! defined (JUCE_AMALGAMATED_INCLUDE)
+#if defined (__JUCE_EVENTS_JUCEHEADER__) && ! JUCE_AMALGAMATED_INCLUDE
  /* When you add this cpp file to your project, you mustn't include it in a file where you've
     already included any other headers - just put it inside a file on its own, possibly with your config
     flags preceding it, but don't include anything else. That also includes avoiding any automatic prefix
