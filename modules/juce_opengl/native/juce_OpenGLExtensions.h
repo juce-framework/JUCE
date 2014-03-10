c83432e2821201e214b0836614d4d8f920ffde13
Ka Ming Chan <ming4883@gmail.com>
2014-03-11 00:24:42 +0800

    added JUCE_OSX_USE_GL_3_2 module option to juce_opengl module, so that users can enable OpenGL 3.2 core profile on OSX at compile time

--- modules/juce_opengl/native/juce_OpenGLExtensions.h
+++ modules/juce_opengl/native/juce_OpenGLExtensions.h
@@ -112,12 +112,12 @@ struct OpenGLExtensionFunctions
     JUCE_GL_EXTENSION_FUNCTIONS (JUCE_DECLARE_GL_FUNCTION, JUCE_DECLARE_GL_FUNCTION)
    #else
     #define JUCE_DECLARE_GL_FUNCTION(name, returnType, params, callparams)      inline static returnType name params { return ::name callparams; }
-#if JUCE_OSX_USE_GL_3_2
-    #define JUCE_DECLARE_GL_FUNCTION_EXT(name, returnType, params, callparams)  inline static returnType name params { return ::name callparams; }
-#else
     #define JUCE_DECLARE_GL_FUNCTION_EXT(name, returnType, params, callparams)  inline static returnType name params { return ::name ## EXT callparams; }
-#endif
+    #if JUCE_OSX_USE_GL_3_2
+    JUCE_GL_EXTENSION_FUNCTIONS (JUCE_DECLARE_GL_FUNCTION, JUCE_DECLARE_GL_FUNCTION)
+    #else
     JUCE_GL_EXTENSION_FUNCTIONS (JUCE_DECLARE_GL_FUNCTION, JUCE_DECLARE_GL_FUNCTION_EXT)
+    #endif
     #undef JUCE_DECLARE_GL_FUNCTION_EXT
    #endif
 
