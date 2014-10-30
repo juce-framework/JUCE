06b9bdefb6557bbffe58f80efce4385f09430baf
Felix Faire <felixfaire@gmail.com>
2014-10-30 11:16:51 +0000

    Added AudioComponentTemplate

--- extras/Introjucer/JuceLibraryCode/BinaryData.h
+++ extras/Introjucer/JuceLibraryCode/BinaryData.h
@@ -9,6 +9,9 @@
 
 namespace BinaryData
 {
+    extern const char*   jucer_AudioComponentTemplate_cpp;
+    const int            jucer_AudioComponentTemplate_cppSize = 1835;
+
     extern const char*   jucer_MainTemplate_SimpleWindow_cpp;
     const int            jucer_MainTemplate_SimpleWindow_cppSize = 3866;
 
@@ -145,7 +148,7 @@ namespace BinaryData
     extern const char* namedResourceList[];
 
     // Number of elements in the namedResourceList array.
-    const int namedResourceListSize = 44;
+    const int namedResourceListSize = 45;
 
     // If you provide the name of one of the binary resource variables above, this function will
     // return the corresponding data and its size (or a null pointer if the name isn't found).
--- modules/juce_audio_utils/gui/juce_AudioAppComponent.h
+++ modules/juce_audio_utils/gui/juce_AudioAppComponent.h
@@ -43,7 +43,33 @@ public:
 
     /** A subclass should call this from their constructor, to set up the audio. */
     void setAudioChannels (int numInputChannels, int numOutputChannels);
+    
+    /** Tells the source to prepare for playing.
 
+        An AudioSource has two states: prepared and unprepared.
+
+        The prepareToPlay() method is guaranteed to be called at least once on an 'unpreprared'
+        source to put it into a 'prepared' state before any calls will be made to getNextAudioBlock().
+        This callback allows the source to initialise any resources it might need when playing.
+
+        Once playback has finished, the releaseResources() method is called to put the stream
+        back into an 'unprepared' state.
+
+        Note that this method could be called more than once in succession without
+        a matching call to releaseResources(), so make sure your code is robust and
+        can handle that kind of situation.
+
+        @param samplesPerBlockExpected  the number of samples that the source
+                                        will be expected to supply each time its
+                                        getNextAudioBlock() method is called. This
+                                        number may vary slightly, because it will be dependent
+                                        on audio hardware callbacks, and these aren't
+                                        guaranteed to always use a constant block size, so
+                                        the source should be able to cope with small variations.
+        @param sampleRate               the sample rate that the output will be used at - this
+                                        is needed by sources such as tone generators.
+        @see releaseResources, getNextAudioBlock
+    */
     virtual void prepareToPlay (int samplesPerBlockExpected,
                                 double sampleRate) = 0;
 
