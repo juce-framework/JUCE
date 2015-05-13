bb8b36aa72f2705dcd6c476ddcc9b806988475f7
Timur Doumler <timur@roli.com>
2015-05-13 12:50:18 +0100

    Fixed bug where Introjucer would produce invalid code in case two images added via the GUI editor use the same image resource

--- extras/Introjucer/Source/ComponentEditor/paintelements/jucer_PaintElementImage.h
+++ extras/Introjucer/Source/ComponentEditor/paintelements/jucer_PaintElementImage.h
@@ -100,7 +100,7 @@ public:
         {
             if (dynamic_cast <const DrawableImage*> (getDrawable()) != 0)
             {
-                const String imageVariable ("cachedImage_" + resourceName.replace ("::", "_"));
+                const String imageVariable ("cachedImage_" + resourceName.replace ("::", "_") + "_" + String (code.getUniqueSuffix()));
 
                 code.addImageResourceLoader (imageVariable, resourceName);
 
