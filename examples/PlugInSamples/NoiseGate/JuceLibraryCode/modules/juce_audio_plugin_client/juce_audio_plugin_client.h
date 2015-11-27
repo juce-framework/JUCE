81cdc6581b5aa3621472a0dc89203a194974dcf9
hogliux <fabian.renn@gmail.com>
2015-11-27 16:35:58 +0000

    Update module paths for NoiseGate sample and re-save project

--- examples/PlugInSamples/NoiseGate/JuceLibraryCode/modules/juce_audio_plugin_client/juce_audio_plugin_client.h
+++ examples/PlugInSamples/NoiseGate/JuceLibraryCode/modules/juce_audio_plugin_client/juce_audio_plugin_client.h
@@ -1,4 +1,4 @@
 // This is an auto-generated file to redirect any included
 // module headers to the correct external folder.
 
-#include "../../../../../modules/juce_audio_plugin_client/juce_audio_plugin_client.h"
+#include "../../../../../../modules/juce_audio_plugin_client/juce_audio_plugin_client.h"
