2bd3931c494ba13193cc39013392b21b803bbf82
hogliux <fabian.renn@gmail.com>
2016-02-03 11:58:55 +0000

    Fixed typo in setPlayConfigDetails comment

--- modules/juce_audio_processors/processors/juce_AudioProcessor.h
+++ modules/juce_audio_processors/processors/juce_AudioProcessor.h
@@ -884,7 +884,7 @@ public:
 
     //==============================================================================
     /** This is called by the processor to specify its details before being played. Use this
-        version of the function if you are not interested in any sidechain or aux buses
+        version of the function if you are not interested in any sidechain and/or aux buses
         and do not care about the layout of channels. Otherwise use setRateAndBufferSizeDetails.*/
     void setPlayConfigDetails (int numIns, int numOuts, double sampleRate, int blockSize);
 
