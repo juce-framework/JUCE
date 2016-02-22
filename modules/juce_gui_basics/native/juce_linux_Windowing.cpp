688969da207add22ec68ba1be591145943aa68de
hogliux <fabian.renn@gmail.com>
2016-02-22 16:39:44 +0000

    Fix missing window decorations in KDE

--- modules/juce_gui_basics/native/juce_linux_Windowing.cpp
+++ modules/juce_gui_basics/native/juce_linux_Windowing.cpp
@@ -2824,6 +2824,14 @@ private:
             ScopedXLock xlock;
             xchangeProperty (wndH, hints, hints, 32, &kwmHints, 1);
         }
+
+        hints = Atoms::getIfExists ("_KDE_NET_WM_WINDOW_TYPE_OVERRIDE");
+
+        if (hints != None)
+        {
+            ScopedXLock xlock;
+            xchangeProperty (wndH, atoms.windowType, XA_ATOM, 32, &hints, 1);
+        }
     }
 
     void addWindowButtons (Window wndH)
@@ -2898,9 +2906,7 @@ private:
         else
             netHints [0] = Atoms::getIfExists ("_NET_WM_WINDOW_TYPE_NORMAL");
 
-        netHints[1] = Atoms::getIfExists ("_KDE_NET_WM_WINDOW_TYPE_OVERRIDE");
-
-        xchangeProperty (windowH, atoms.windowType, XA_ATOM, 32, &netHints, 2);
+        xchangeProperty (windowH, atoms.windowType, XA_ATOM, 32, &netHints, 1);
 
         int numHints = 0;
 
