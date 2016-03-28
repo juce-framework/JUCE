7a1360cb553157f6cb251b648309313ad1370aa3
drmad <drmad@doctor-mad.com>
2016-03-28 18:18:36 +0200

    More comments and more features. Using buttons instead of #define
    - Added even more comments...
    - Using buttons to select various colorization procedures and also
    showing use of core graphics JUCE elements with OpenGL
    - Added ImageTextButton class
    - Added jpg texture file don't forget to add it to resources

--- examples/OpenGLAppExample/JuceLibraryCode/BinaryData.h
+++ examples/OpenGLAppExample/JuceLibraryCode/BinaryData.h
@@ -9,6 +9,9 @@
 
 namespace BinaryData
 {
+    extern const char*   rainbow_gradient_vertical_jpg;
+    const int            rainbow_gradient_vertical_jpgSize = 31071;
+
     extern const char*   teapot_obj;
     const int            teapot_objSize = 95000;
 
@@ -16,7 +19,7 @@ namespace BinaryData
     extern const char* namedResourceList[];
 
     // Number of elements in the namedResourceList array.
-    const int namedResourceListSize = 1;
+    const int namedResourceListSize = 2;
 
     // If you provide the name of one of the binary resource variables above, this function will
     // return the corresponding data and its size (or a null pointer if the name isn't found).
