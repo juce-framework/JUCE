f6efbedee7b678dac85ca7c00f3b3becb426e007
hogliux <fabian.renn@gmail.com>
2016-04-19 11:29:07 +0100

    Remove default symbol visibility for JUCE shared code

--- modules/juce_core/system/juce_StandardHeader.h
+++ modules/juce_core/system/juce_StandardHeader.h
@@ -130,7 +130,7 @@
  #ifdef __INTEL_COMPILER
   #pragma warning (disable: 1125) // (virtual override warning)
  #endif
-#elif defined (JUCE_DLL) || defined (JUCE_DLL_BUILD) || defined (JUCE_SHARED_CODE)
+#elif defined (JUCE_DLL) || defined (JUCE_DLL_BUILD)
  #define JUCE_API __attribute__ ((visibility("default")))
 #endif
 
