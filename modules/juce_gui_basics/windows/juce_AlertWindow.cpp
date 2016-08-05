675f5943a160ed9d5d5aa59f73b7c3f46c4d4511
ed <ed_davies95@hotmail.co.uk>
2016-08-05 15:20:28 +0100

    fixed silly typo

--- modules/juce_gui_basics/windows/juce_AlertWindow.cpp
+++ modules/juce_gui_basics/windows/juce_AlertWindow.cpp
@@ -593,7 +593,7 @@ private:
 
         jassert (alertBox != nullptr); // you have to return one of these!
 
-        alertBox->setAlwaysOnTop (juce_areThereAnyAlwaysOnTopWindows);
+        alertBox->setAlwaysOnTop (juce_areThereAnyAlwaysOnTopWindows());
 
        #if JUCE_MODAL_LOOPS_PERMITTED
         if (modal)
