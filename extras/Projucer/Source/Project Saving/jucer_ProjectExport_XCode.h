juce-framework/JUCE

commit b48ee440c04d1c4cbc6240bdbee8672cd113db42

Force Xcode RTAS Target to always be 32 bit

extras/Projucer/Source/Project Saving/jucer_ProjectExport_XCode.h
@@ -885,7 +885,8 @@ public:
 
             s.add ("PRODUCT_BUNDLE_IDENTIFIER = " + bundleIdentifier);
 
-            const String arch ((! owner.isiOS() && type == Target::AudioUnitv3PlugIn) ? osxArch_64Bit : config.osxArchitecture.get());
+            const String arch ((! owner.isiOS() && type == Target::AudioUnitv3PlugIn) ? osxArch_64Bit : (type == Target::RTASPlugIn) ? osxArch_32BitUniversal : config.osxArchitecture.get());
+            
             if (arch == osxArch_Native)                s.add ("ARCHS = \"$(NATIVE_ARCH_ACTUAL)\"");
             else if (arch == osxArch_32BitUniversal)   s.add ("ARCHS = \"$(ARCHS_STANDARD_32_BIT)\"");
             else if (arch == osxArch_64BitUniversal)   s.add ("ARCHS = \"$(ARCHS_STANDARD_32_64_BIT)\"");
@@ -1067,7 +1068,7 @@ public:
             }
 
             s.add ("GCC_PREPROCESSOR_DEFINITIONS = " + indentParenthesisedList (defsList));
-
+            if (type == Target::RTASPlugIn) s.add("VALID_ARCHS=\"i386\"");
             s.addTokens (config.customXcodeFlags.get(), ",", "\"'");
 
             return getCleanedStringArray (s);

commit cb5cf47ebe624c12b501a54d16424edb28a80f9a

In Debug build RTAS in 64-bit too, to avoid build errors introduced in b48ee440c04

extras/Projucer/Source/Project Saving/jucer_ProjectExport_XCode.h
@@ -885,7 +885,7 @@ public:
 
             s.add ("PRODUCT_BUNDLE_IDENTIFIER = " + bundleIdentifier);
 
-            const String arch ((! owner.isiOS() && type == Target::AudioUnitv3PlugIn) ? osxArch_64Bit : (type == Target::RTASPlugIn) ? osxArch_32BitUniversal : config.osxArchitecture.get());
+            const String arch ((! owner.isiOS() && type == Target::AudioUnitv3PlugIn) ? osxArch_64Bit : config.osxArchitecture.get());
             
             if (arch == osxArch_Native)                s.add ("ARCHS = \"$(NATIVE_ARCH_ACTUAL)\"");
             else if (arch == osxArch_32BitUniversal)   s.add ("ARCHS = \"$(ARCHS_STANDARD_32_BIT)\"");
@@ -1068,7 +1068,7 @@ public:
             }
 
             s.add ("GCC_PREPROCESSOR_DEFINITIONS = " + indentParenthesisedList (defsList));
-            if (type == Target::RTASPlugIn) s.add("VALID_ARCHS=\"i386\"");
+            if (type == Target::RTASPlugIn && !config.isDebug()) s.add("VALID_ARCHS=\"i386\"");
             s.addTokens (config.customXcodeFlags.get(), ",", "\"'");
 
             return getCleanedStringArray (s);