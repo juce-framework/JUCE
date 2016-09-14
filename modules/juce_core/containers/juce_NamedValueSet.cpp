e35aba3444edf8d8a8073a7df39058c12cde5439
hogliux <fabian.renn@gmail.com>
2016-09-14 09:26:48 +0100

    Added a standard iterator to NamedValueSet

--- modules/juce_core/containers/juce_NamedValueSet.cpp
+++ modules/juce_core/containers/juce_NamedValueSet.cpp
@@ -26,40 +26,6 @@
   ==============================================================================
 */
 
-struct NamedValueSet::NamedValue
-{
-    NamedValue() noexcept {}
-    NamedValue (const Identifier& n, const var& v)  : name (n), value (v) {}
-    NamedValue (const NamedValue& other) : name (other.name), value (other.value) {}
-
-   #if JUCE_COMPILER_SUPPORTS_MOVE_SEMANTICS
-    NamedValue (NamedValue&& other) noexcept
-        : name (static_cast<Identifier&&> (other.name)),
-          value (static_cast<var&&> (other.value))
-    {
-    }
-
-    NamedValue (Identifier&& n, var&& v) noexcept
-        : name (static_cast<Identifier&&> (n)),
-          value (static_cast<var&&> (v))
-    {
-    }
-
-    NamedValue& operator= (NamedValue&& other) noexcept
-    {
-        name = static_cast<Identifier&&> (other.name);
-        value = static_cast<var&&> (other.value);
-        return *this;
-    }
-   #endif
-
-    bool operator== (const NamedValue& other) const noexcept   { return name == other.name && value == other.value; }
-    bool operator!= (const NamedValue& other) const noexcept   { return ! operator== (other); }
-
-    Identifier name;
-    var value;
-};
-
 //==============================================================================
 NamedValueSet::NamedValueSet() noexcept
 {
