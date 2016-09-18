081d8b20002124f80b7f9d3345be57a81ca2bbd1
Yair Chuchem <yairchu@gmail.com>
2016-09-18 14:39:16 +0300

    Revert "SVG support parsing colors with alpha"
    As we (SR) ended up not needing it, and my patch had a warning which needed fixing.
    
    This reverts commit 45845cb3c1c6d20d133475d94100f21469e24714.

--- modules/juce_gui_basics/drawables/juce_SVGParser.cpp
+++ modules/juce_gui_basics/drawables/juce_SVGParser.cpp
@@ -1337,8 +1337,6 @@ private:
               && s [index + 1] == 'g'
               && s [index + 2] == 'b')
         {
-            const bool hasAlpha = s[index + 3] == 'a';
-
             const int openBracket = s.indexOfChar (index, '(');
             const int closeBracket = s.indexOfChar (openBracket, ')');
 
@@ -1351,20 +1349,14 @@ private:
                 tokens.trim();
                 tokens.removeEmptyStrings();
 
-                float alpha = 1.0f;
-                if (hasAlpha)
-                    alpha = tokens[3].getDoubleValue();
-
                 if (tokens[0].containsChar ('%'))
                     return Colour ((uint8) roundToInt (2.55 * tokens[0].getDoubleValue()),
                                    (uint8) roundToInt (2.55 * tokens[1].getDoubleValue()),
-                                   (uint8) roundToInt (2.55 * tokens[2].getDoubleValue()),
-                                   alpha);
+                                   (uint8) roundToInt (2.55 * tokens[2].getDoubleValue()));
                 else
                     return Colour ((uint8) tokens[0].getIntValue(),
                                    (uint8) tokens[1].getIntValue(),
-                                   (uint8) tokens[2].getIntValue(),
-                                   alpha);
+                                   (uint8) tokens[2].getIntValue());
             }
         }
 
