juce-framework/JUCE

commit a5e3b7ad1cfb1f0e8be3311c444a7d547c67f900

Added check and warning when entering only lowercase AU plugin identifier codes

extras/Projucer/Source/Project Saving/jucer_ProjectExport_XCode.h
@@ -1310,13 +1310,24 @@ public:
             XmlElement plistEntry ("array");
             XmlElement* dict = plistEntry.createNewChildElement ("dict");
 
+            const String pluginManufacturerCode = owner.project.getPluginManufacturerCode().toString().trim().substring (0, 4);
+            const String pluginSubType          = owner.project.getPluginCode()            .toString().trim().substring (0, 4);
+
+            if (pluginManufacturerCode.toLowerCase() == pluginManufacturerCode || pluginSubType.toLowerCase() == pluginSubType)
+            {
+                throw SaveError ("AudioUnit plugin code identifiers invalid!\n\n"
+                                 "You have used only lower case letters in your AU plugin code identifiers. "
+                                 "You must have at least one uppercase letter in both your AU plugin manufacturer "
+                                 "identifier code and your AU plugin subtype identifier code.");
+            }
+
             addPlistDictionaryKey (dict, "name", owner.project.getPluginManufacturer().toString()
                                    + ": " + owner.project.getPluginName().toString());
             addPlistDictionaryKey (dict, "description", owner.project.getPluginDesc().toString());
             addPlistDictionaryKey (dict, "factoryFunction", owner.project.getPluginAUExportPrefix().toString() + "Factory");
-            addPlistDictionaryKey (dict, "manufacturer", owner.project.getPluginManufacturerCode().toString().trim().substring (0, 4));
+            addPlistDictionaryKey (dict, "manufacturer", pluginManufacturerCode);
             addPlistDictionaryKey (dict, "type", owner.project.getAUMainTypeCode());
-            addPlistDictionaryKey (dict, "subtype", owner.project.getPluginCode().toString().trim().substring (0, 4));
+            addPlistDictionaryKey (dict, "subtype", pluginSubType);
             addPlistDictionaryKeyInt (dict, "version", owner.project.getVersionAsHexInteger());
 
             xcodeExtraPListEntries.add (plistKey);

extras/Projucer/Source/Project Saving/jucer_ProjectSaver.h
@@ -624,33 +624,40 @@ private:
         // keep a copy of the basic generated files group, as each exporter may modify it.
         const ValueTree originalGeneratedGroup (generatedFilesGroup.state.createCopy());
 
-        for (Project::ExporterIterator exporter (project); exporter.next();)
+        try
         {
-            if (exporter->getTargetFolder().createDirectory())
+            for (Project::ExporterIterator exporter (project); exporter.next();)
             {
-                exporter->copyMainGroupFromProject();
-                exporter->settings = exporter->settings.createCopy();
+                if (exporter->getTargetFolder().createDirectory())
+                {
+                    exporter->copyMainGroupFromProject();
+                    exporter->settings = exporter->settings.createCopy();
 
-                exporter->addToExtraSearchPaths (RelativePath ("JuceLibraryCode", RelativePath::projectFolder));
+                    exporter->addToExtraSearchPaths (RelativePath ("JuceLibraryCode", RelativePath::projectFolder));
 
-                generatedFilesGroup.state = originalGeneratedGroup.createCopy();
-                exporter->addSettingsForProjectType (project.getProjectType());
+                    generatedFilesGroup.state = originalGeneratedGroup.createCopy();
+                    exporter->addSettingsForProjectType (project.getProjectType());
 
-                for (auto& module: modules)
-                    module->addSettingsForModuleToExporter (*exporter, *this);
+                    for (auto& module: modules)
+                        module->addSettingsForModuleToExporter (*exporter, *this);
 
-                if (project.getProjectType().isAudioPlugin())
-                    writePluginCharacteristicsFile();
+                    if (project.getProjectType().isAudioPlugin())
+                        writePluginCharacteristicsFile();
 
-                generatedFilesGroup.sortAlphabetically (true, true);
-                exporter->getAllGroups().add (generatedFilesGroup);
+                    generatedFilesGroup.sortAlphabetically (true, true);
+                    exporter->getAllGroups().add (generatedFilesGroup);
 
-                threadPool.addJob (new ExporterJob (*this, exporter.exporter.release(), modules), true);
+                    threadPool.addJob (new ExporterJob (*this, exporter.exporter.release(), modules), true);
+                }
+                else
+                {
+                    addError ("Can't create folder: " + exporter->getTargetFolder().getFullPathName());
+                }
             }
-            else
-            {
-                addError ("Can't create folder: " + exporter->getTargetFolder().getFullPathName());
-            }
+        }
+        catch (ProjectExporter::SaveError& saveError)
+        {
+            addError (saveError.message);
         }
 
         while (threadPool.getNumJobs() > 0)

commit 1fb8dc0019783e08094b49f992df25887a01fb70

Fix projucer project saving error introduced in a5e3b7a
See relevant thread https://forum.juce.com/t/au-midi-effect-solved/19510/14

extras/Projucer/Source/Project Saving/jucer_ProjectExport_XCode.h
@@ -1326,7 +1326,7 @@ public:
             const String pluginManufacturerCode = owner.project.getPluginManufacturerCode().toString().trim().substring (0, 4);
             const String pluginSubType          = owner.project.getPluginCode()            .toString().trim().substring (0, 4);
 
-            if (pluginManufacturerCode.toLowerCase() == pluginManufacturerCode || pluginSubType.toLowerCase() == pluginSubType)
+            if (pluginManufacturerCode.toLowerCase() == pluginManufacturerCode)
             {
                 throw SaveError ("AudioUnit plugin code identifiers invalid!\n\n"
                                  "You have used only lower case letters in your AU plugin code identifiers. "