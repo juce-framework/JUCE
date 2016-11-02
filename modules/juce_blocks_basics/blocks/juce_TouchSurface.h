7bb0fe4077f77a5a21e42c962aa8488f6debdda4
tpoole <t.b.poole@gmail.com>
2016-11-02 20:39:39 +0000

    Update copyright and make docstrings more compatible with Doxygen

--- modules/juce_blocks_basics/blocks/juce_TouchSurface.h
+++ modules/juce_blocks_basics/blocks/juce_TouchSurface.h
@@ -2,7 +2,7 @@
   ==============================================================================
 
    This file is part of the JUCE library.
-   Copyright (c) 2015 - ROLI Ltd.
+   Copyright (c) 2016 - ROLI Ltd.
 
    Permission is granted to use this software under the terms of either:
    a) the GPL v2 (or any later version)
@@ -24,7 +24,7 @@
 
 
 /**
-    Represents the touch surface of a block device.
+    Represents the touch surface of a BLOCKS device.
 */
 class TouchSurface
 {
@@ -41,55 +41,62 @@ public:
         int index;
 
         /** The X position of this touch on the device, in logical units starting from 0 (left).
+
             See Block::getWidth() for the maximum X value on the device.
         */
         float x;
 
-        /** An approximation of the velocity at which the X value is changing.
-            Measured in units/second. This is intended as a useful hint to help with gesture
-            detection, but may be 0 if the device doesn't provide this data.
+        /** An approximation of the velocity at which the X value is changing, measured in
+            units/second. This is intended as a useful hint to help with gesture detection, but
+            may be 0 if the device doesn't provide this data.
         */
         float xVelocity;
 
         /** The Y position of this touch on the device, in logical units starting from 0 (top).
+
             See Block::getHeight() to find the maximum Y on the device.
         */
         float y;
 
-        /** An approximation of the velocity at which the Y value is changing.
-            Measured in units/second. This is intended as a useful hint to help with gesture
-            detection, but may be 0 if the device doesn't provide this data.
+        /** An approximation of the velocity at which the Y value is changing, measured in
+            units/second. This is intended as a useful hint to help with gesture detection, but
+            may be 0 if the device doesn't provide this data.
         */
         float yVelocity;
 
-        /** The current pressure of this touch, in the range 0 (no pressure) to 1 (very hard) */
+        /** The current pressure of this touch, in the range 0.0 (no pressure) to 1.o (very hard). */
         float z;
 
-        /** The rate at which pressure is currently changing.
-            Measured in units/second. This is intended as a useful hint to help with gesture
-            detection, but may be 0 if the device doesn't provide this data.
+        /** The rate at which pressure is currently changing, measured in units/second. This is
+            intended as a useful hint to help with gesture detection, but may be 0 if the device
+            doesn't provide this data.
         */
         float zVelocity;
 
         /** The timestamp of this event, in milliseconds since the device was booted. */
         Block::Timestamp eventTimestamp;
 
-        /** True if this is the first event for this finger. */
+        /** True if this is the first event for this finger/index. */
         bool isTouchStart;
 
-        /** True if this is the final event as this finger is lifted off. */
+        /** True if this is the final event as this finger/index is lifted off. */
         bool isTouchEnd;
 
         /** The ID of the block that generated this touch. */
         Block::UID blockUID;
 
-        /** The initial X position of the touchStart event corresponding to this finger. */
+        /** The initial X position of the touchStart event corresponding to this finger/index. */
         float startX;
 
-        /** The initial Y position of the touchStart event corresponding to this finger. */
+        /** The initial Y position of the touchStart event corresponding to this finger/index. */
         float startY;
     };
 
+    //==============================================================================
+    /** Forces a touch-off message for all active touches. */
+    virtual void cancelAllActiveTouches() noexcept = 0;
+
+    //==============================================================================
     /** Receives callbacks when a touch moves or changes pressure. */
     struct Listener
     {
@@ -98,30 +105,24 @@ public:
         virtual void touchChanged (TouchSurface&, const Touch&) = 0;
     };
 
-    /** Adds a listener to be called when there's a change to this surface. */
-    void addListener (Listener*);
-
-    /** Removes a previously-registered listener. */
-    void removeListener (Listener*);
-
-    /** For a Seaboard unit, this will return the number of keys.
-        For other types of touch-surface, it will return 0.
-    */
-    virtual int getNumberOfKeywaves() const = 0;
-
-    /** The block that owns this touch surface. */
-    Block& block;
-
-    /** Testing feature: allows you to inject touches into a touch-surface. */
+    /** Testing feature: this allows you to inject touches onto a touch surface. */
     void callListenersTouchChanged (const TouchSurface::Touch& t)
     {
         listeners.call (&Listener::touchChanged, *this, t);
     }
 
-    /** Forces touch-off messages for all active touches. */
-    virtual void cancelAllActiveTouches() noexcept = 0;
+    /** Adds a listener to be called when the surface is touched. */
+    void addListener (Listener*);
+
+    /** Removes a previously-registered listener. */
+    void removeListener (Listener*);
+
+    //==============================================================================
+    /** The block that owns this touch surface. */
+    Block& block;
 
 protected:
+    //==============================================================================
     juce::ListenerList<Listener> listeners;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (TouchSurface)
