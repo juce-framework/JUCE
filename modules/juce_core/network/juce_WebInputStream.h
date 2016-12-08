3fa62bcaf78c00d19676f4e8cd802375a0735454
tpoole <t.b.poole@gmail.com>
2016-12-08 11:05:11 +0000

    Fixed some misnamed Doxygen parameters

--- modules/juce_core/network/juce_WebInputStream.h
+++ modules/juce_core/network/juce_WebInputStream.h
@@ -178,7 +178,7 @@ class JUCE_API WebInputStream : public InputStream
         @returns    the actual number of bytes that were read, which may be less than
                     maxBytesToRead if the stream is exhausted before it gets that far
     */
-    int read (void* buffer, int bytesToRead) override;
+    int read (void* destBuffer, int maxBytesToRead) override;
 
     /** Returns true if the stream has no more data to read. */
     bool isExhausted() override;
