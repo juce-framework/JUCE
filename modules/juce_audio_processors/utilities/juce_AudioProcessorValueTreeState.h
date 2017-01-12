a554d0e95e01fd22f832bf0ca0853bf07ea1b0a1
tpoole <t.b.poole@gmail.com>
2017-01-12 14:14:19 +0000

    Added to the AudioProcessorValueTreeState documentation

--- modules/juce_audio_processors/utilities/juce_AudioProcessorValueTreeState.h
+++ modules/juce_audio_processors/utilities/juce_AudioProcessorValueTreeState.h
@@ -122,6 +122,8 @@ public:
     AudioProcessor& processor;
 
     /** The state of the whole processor.
+
+        This must be initialised after all calls to createAndAddParameter().
         You can replace this with your own ValueTree object, and can add properties and
         children to the tree. This class will automatically add children for each of the
         parameter objects that are created by createParameter().
