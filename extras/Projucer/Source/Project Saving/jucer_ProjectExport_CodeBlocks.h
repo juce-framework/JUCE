da3f3a8eac1177f22afa63b34a41aace4bb75d5d
tpoole <t.b.poole@gmail.com>
2017-01-12 11:54:51 +0000

    Fixed bug creating dynamic libraries with the Linux Makefile and Code::Blocks exporters

--- extras/Projucer/Source/Project Saving/jucer_ProjectExport_CodeBlocks.h
+++ extras/Projucer/Source/Project Saving/jucer_ProjectExport_CodeBlocks.h
@@ -138,9 +138,9 @@ public:
     }
 
     //==============================================================================
-    void addPlatformSpecificSettingsForProjectType (const ProjectType&) override
+    void addPlatformSpecificSettingsForProjectType (const ProjectType& type) override
     {
-        // no-op.
+        createDynamicLibrary = type.isAudioPlugin() || type.isDynamicLibrary();
     }
 
 private:
@@ -243,6 +243,9 @@ private:
 
         if (config.exporter.isLinux())
         {
+            if (createDynamicLibrary)
+                flags.add ("-fPIC");
+
             if (linuxPackages.size() > 0)
             {
                 auto pkgconfigFlags = String ("`pkg-config --cflags");
@@ -272,6 +275,9 @@ private:
 
         if (config.exporter.isLinux() && linuxPackages.size() > 0)
         {
+            if (createDynamicLibrary)
+                flags.add ("-shared");
+
             auto pkgconfigLibs = String ("`pkg-config --libs");
             for (auto p : linuxPackages)
                 pkgconfigLibs << " " << p;
@@ -467,6 +473,7 @@ private:
     }
 
     CodeBlocksOS os;
+    bool createDynamicLibrary = false;
 
     JUCE_DECLARE_NON_COPYABLE (CodeBlocksProjectExporter)
 };
