224e019fa71a7ffd23c2d23a533db044130da7d2
Ignacio Fraga <ignacio@ntopology.com>
2017-02-15 14:43:09 -0500

    getting glDrawArraysInstanced

--- modules/juce_opengl/native/juce_OpenGLExtensions.h
+++ modules/juce_opengl/native/juce_OpenGLExtensions.h
@@ -93,7 +93,8 @@
     USE_FUNCTION (glCheckFramebufferStatus, GLenum, (GLenum p1), (p1))\
     USE_FUNCTION (glFramebufferTexture2D,   void, (GLenum p1, GLenum p2, GLenum p3, GLuint p4, GLint p5), (p1, p2, p3, p4, p5))\
     USE_FUNCTION (glFramebufferRenderbuffer,  void, (GLenum p1, GLenum p2, GLenum p3, GLuint p4), (p1, p2, p3, p4))\
-    USE_FUNCTION (glGetFramebufferAttachmentParameteriv, void, (GLenum p1, GLenum p2, GLenum p3, GLint* p4), (p1, p2, p3, p4))
+    USE_FUNCTION (glGetFramebufferAttachmentParameteriv, void, (GLenum p1, GLenum p2, GLenum p3, GLint* p4), (p1, p2, p3, p4))\
+    USE_FUNCTION (glDrawArraysInstanced, void, (GLenum p1, GLint p2, GLsizei p3, GLsizei p4), (p1, p2, p3, p4))
 
 /** @internal This macro contains a list of GL extension functions that need to be dynamically loaded on Windows/Linux.
     @see OpenGLExtensionFunctions
