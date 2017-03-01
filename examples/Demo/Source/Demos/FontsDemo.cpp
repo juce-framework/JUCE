5fe328925682241d224c96f0e9bb2d8809cc4a76
hogliux <fabian.renn@gmail.com>
2017-03-01 16:16:34 +0000

    Fixed several Android text rendering bugs

--- examples/Demo/Source/Demos/FontsDemo.cpp
+++ examples/Demo/Source/Demos/FontsDemo.cpp
@@ -179,7 +179,7 @@ public:
         AttributedString s;
         s.setWordWrap (AttributedString::none);
         s.setJustification (Justification::centredLeft);
-        s.append (font.getTypefaceName(), font.withPointHeight (height * 0.7f), Colours::black);
+        s.append (font.getTypefaceName(), font.withHeight (height * 0.7f), Colours::black);
         s.append ("   " + font.getTypefaceName(), Font (height * 0.5f, Font::italic), Colours::grey);
 
         s.draw (g, Rectangle<int> (width, height).expanded (-4, 50).toFloat());
--- modules/juce_core/native/juce_android_JNIHelpers.h
+++ modules/juce_core/native/juce_android_JNIHelpers.h
@@ -272,7 +272,7 @@ extern AndroidSystem android;
  METHOD (getClipboardContent,    "getClipboardContent",  "()Ljava/lang/String;") \
  METHOD (setClipboardContent,    "setClipboardContent",  "(Ljava/lang/String;)V") \
  METHOD (excludeClipRegion,      "excludeClipRegion",    "(Landroid/graphics/Canvas;FFFF)V") \
- METHOD (renderGlyph,            "renderGlyph",          "(CLandroid/graphics/Paint;Landroid/graphics/Matrix;Landroid/graphics/Rect;)[I") \
+ METHOD (renderGlyph,            "renderGlyph",          "(CCLandroid/graphics/Paint;Landroid/graphics/Matrix;Landroid/graphics/Rect;)[I") \
  STATICMETHOD (createHTTPStream, "createHTTPStream",     "(Ljava/lang/String;Z[BLjava/lang/String;I[ILjava/lang/StringBuffer;ILjava/lang/String;)L" JUCE_ANDROID_ACTIVITY_CLASSPATH "$HTTPStream;") \
  METHOD (launchURL,              "launchURL",            "(Ljava/lang/String;)V") \
  METHOD (showMessageBox,         "showMessageBox",       "(Ljava/lang/String;Ljava/lang/String;J)V") \
