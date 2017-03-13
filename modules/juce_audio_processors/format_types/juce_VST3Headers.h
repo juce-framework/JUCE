9c7ee8710a2dad06714a77e7cce58397f55a7803
hogliux <fabian.renn@gmail.com>
2017-03-13 17:15:57 +0000

    Added support for VST3 SDK 3.6.7
    You will need to update your VST3 SDK!

--- modules/juce_audio_processors/format_types/juce_VST3Headers.h
+++ modules/juce_audio_processors/format_types/juce_VST3Headers.h
@@ -84,14 +84,12 @@
  #include <public.sdk/source/vst/vsteditcontroller.h>
 #else
  #include <base/source/baseiids.cpp>
- #include <base/source/fatomic.cpp>
  #include <base/source/fbuffer.cpp>
  #include <base/source/fdebug.cpp>
  #include <base/source/fobject.cpp>
- #include <base/source/frect.cpp>
  #include <base/source/fstreamer.cpp>
  #include <base/source/fstring.cpp>
- #include <base/source/fthread.cpp>
+ #include <base/source/flock.cpp>
  #include <base/source/updatehandler.cpp>
  #include <pluginterfaces/base/conststringtable.cpp>
  #include <pluginterfaces/base/funknown.cpp>
