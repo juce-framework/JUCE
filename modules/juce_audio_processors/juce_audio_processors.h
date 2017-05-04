84136147444cc935477022f0f1fdaabb87936524
hogliux <fabian.renn@gmail.com>
2017-05-04 15:26:54 +0100

    Bump version number to 5.0.1

--- modules/juce_audio_processors/juce_audio_processors.h
+++ modules/juce_audio_processors/juce_audio_processors.h
@@ -35,7 +35,7 @@
 
   ID:               juce_audio_processors
   vendor:           juce
-  version:          5.0.0
+  version:          5.0.1
   name:             JUCE audio processor classes
   description:      Classes for loading and playing VST, AU, or internally-generated audio processors.
   website:          http://www.juce.com/juce
