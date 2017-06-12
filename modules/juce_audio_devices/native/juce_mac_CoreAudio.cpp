bfd59e5f871b68a97b40357f2fd0d85ed955bce9
tpoole <thomas.poole@roli.com>
2017-06-12 10:20:05 +0100

    CoreAudioDevice:  Fixed potential race condition

--- modules/juce_audio_devices/native/juce_mac_CoreAudio.cpp
+++ modules/juce_audio_devices/native/juce_mac_CoreAudio.cpp
@@ -2037,7 +2037,7 @@ private:
 
     static OSStatus hardwareListenerProc (AudioDeviceID, UInt32, const AudioObjectPropertyAddress*, void* clientData)
     {
-        static_cast<CoreAudioIODeviceType*> (clientData)->audioDeviceListChanged();
+        static_cast<CoreAudioIODeviceType*> (clientData)->triggerAsyncAudioDeviceListChange();
         return noErr;
     }
 
