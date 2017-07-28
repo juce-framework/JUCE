3b8f33e9a82b086c80e2adb6057c5eea9854914e
hogliux <fabian.renn@gmail.com>
2017-07-28 11:35:11 +0100

    Fix the documentation of the Oscillator class

--- modules/juce_dsp/processors/juce_Oscillator.h
+++ modules/juce_dsp/processors/juce_Oscillator.h
@@ -54,10 +54,10 @@ public:
     }
 
     //==============================================================================
-    /** Applies a new gain as a linear value. */
+    /** Sets the frequency of the oscillator. */
     void setFrequency (NumericType newGain) noexcept             { frequency.setValue (newGain); }
 
-    /** Returns the current gain as a linear value. */
+    /** Returns the current frequency of the oscillator. */
     NumericType getFrequency() const noexcept                    { return frequency.getTargetValue(); }
 
     //==============================================================================
@@ -70,7 +70,7 @@ public:
         reset();
     }
 
-    /** Resets the internal state of the gain */
+    /** Resets the internal state of the oscillator */
     void reset() noexcept
     {
         pos = 0.0;
