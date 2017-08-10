a092c19bcc0ee759b64b39dfa6d069da9390b8ae
hogliux <fabian.renn@gmail.com>
2017-08-10 12:05:46 +0100

    Added In-App Purchase sample code app

---
+++ examples/InAppPurchase/JuceLibraryCode/BinaryData.h
@@ -0,0 +1,92 @@
+/* =========================================================================================
+
+   This is an auto-generated file: Any edits you make may be overwritten!
+
+*/
+
+#pragma once
+
+namespace BinaryData
+{
+    extern const char*   Ed_png;
+    const int            Ed_pngSize = 139106;
+
+    extern const char*   Ed0_ogg;
+    const int            Ed0_oggSize = 7771;
+
+    extern const char*   Ed1_ogg;
+    const int            Ed1_oggSize = 11553;
+
+    extern const char*   Ed2_ogg;
+    const int            Ed2_oggSize = 8351;
+
+    extern const char*   Fabian_png;
+    const int            Fabian_pngSize = 90415;
+
+    extern const char*   Fabian0_ogg;
+    const int            Fabian0_oggSize = 10287;
+
+    extern const char*   Fabian1_ogg;
+    const int            Fabian1_oggSize = 15211;
+
+    extern const char*   Fabian2_ogg;
+    const int            Fabian2_oggSize = 10763;
+
+    extern const char*   JB_png;
+    const int            JB_pngSize = 79173;
+
+    extern const char*   JB0_ogg;
+    const int            JB0_oggSize = 8340;
+
+    extern const char*   JB1_ogg;
+    const int            JB1_oggSize = 13545;
+
+    extern const char*   JB2_ogg;
+    const int            JB2_oggSize = 8943;
+
+    extern const char*   Jules_png;
+    const int            Jules_pngSize = 81743;
+
+    extern const char*   Jules0_ogg;
+    const int            Jules0_oggSize = 8277;
+
+    extern const char*   Jules1_ogg;
+    const int            Jules1_oggSize = 12303;
+
+    extern const char*   Jules2_ogg;
+    const int            Jules2_oggSize = 9912;
+
+    extern const char*   Lukasz_png;
+    const int            Lukasz_pngSize = 120402;
+
+    extern const char*   Lukasz0_ogg;
+    const int            Lukasz0_oggSize = 14767;
+
+    extern const char*   Lukasz1_ogg;
+    const int            Lukasz1_oggSize = 21756;
+
+    extern const char*   Lukasz2_ogg;
+    const int            Lukasz2_oggSize = 13962;
+
+    extern const char*   Robot_png;
+    const int            Robot_pngSize = 31502;
+
+    extern const char*   Robot0_ogg;
+    const int            Robot0_oggSize = 9224;
+
+    extern const char*   Robot1_ogg;
+    const int            Robot1_oggSize = 16365;
+
+    extern const char*   Robot2_ogg;
+    const int            Robot2_oggSize = 11029;
+
+    // Points to the start of a list of resource names.
+    extern const char* namedResourceList[];
+
+    // Number of elements in the namedResourceList array.
+    const int namedResourceListSize = 24;
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding data and its size (or a null pointer if the name isn't found).
+    const char* getNamedResource (const char* resourceNameUTF8, int& dataSizeInBytes) throw();
+}
