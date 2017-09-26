3627603c8314da9a73bb14c9fbaf6e795d1620a4
tpoole <thomas.poole@roli.com>
2017-09-26 08:43:08 +0100

    Fixed a performance regression parsing doubles

--- modules/juce_core/system/juce_StandardHeader.h
+++ modules/juce_core/system/juce_StandardHeader.h
@@ -114,11 +114,6 @@
  #include "../misc/juce_StdFunctionCompat.h"
 #endif
 
-// The live build fails to compile std::stringstream
-#if ! JUCE_PROJUCER_LIVE_BUILD
- #include <sstream>
-#endif
-
 // Include std::atomic if it's supported by the compiler
 #if JUCE_ATOMIC_AVAILABLE
  #include <atomic>
