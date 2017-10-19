75236aa61d7a7eaa239bcd80e82809ec55c9a58b
tpoole <thomas.poole@roli.com>
2017-10-19 19:53:06 +0100

    Fixed a FlexBox rounding bug

--- modules/juce_gui_basics/layout/juce_FlexBox.cpp
+++ modules/juce_gui_basics/layout/juce_FlexBox.cpp
@@ -790,16 +790,13 @@ void FlexBox::performLayout (Rectangle<float> targetArea)
         {
             item.currentBounds += targetArea.getPosition();
 
-            if (auto comp = item.associatedComponent)
-            {
-                auto position = item.currentBounds.getPosition().roundToInt();
-                comp->setBounds (position.getX(),
-                                 position.getY(),
-                                 roundToInt (item.currentBounds.getRight())  - position.getX(),
-                                 roundToInt (item.currentBounds.getBottom()) - position.getY());
-            }
+            if (auto* comp = item.associatedComponent)
+                comp->setBounds (Rectangle<int>::leftTopRightBottom ((int) item.currentBounds.getX(),
+                                                                     (int) item.currentBounds.getY(),
+                                                                     (int) item.currentBounds.getRight(),
+                                                                     (int) item.currentBounds.getBottom()));
 
-            if (auto box = item.associatedFlexBox)
+            if (auto* box = item.associatedFlexBox)
                 box->performLayout (item.currentBounds);
         }
     }
