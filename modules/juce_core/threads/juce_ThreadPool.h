6873859159e52bcafdd39ac8a52b774103761866
Jesse Chappell <jesse@quikau-4.local>
2017-11-15 10:27:02 -0500

    made threadpool job signalJobShouldExit() virtual, so subclasses can override

--- modules/juce_core/threads/juce_ThreadPool.h
+++ modules/juce_core/threads/juce_ThreadPool.h
@@ -111,7 +111,7 @@ public:
 
         @see shouldExit()
     */
-    void signalJobShouldExit();
+    virtual void signalJobShouldExit();
 
     /** Add a listener to this thread job which will receive a callback when
         signalJobShouldExit was called on this thread job.
@@ -340,4 +340,4 @@ private:
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ThreadPool)
 };
 
-} // namespace juce
+} // namespace juce
