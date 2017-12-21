3519de9a22658997398b548e0402176d4bcd535f
Tom Poole <thomas.poole@roli.com>
2017-12-21 10:58:44 +0000

    A more careful assertion about allowed file type filters in MacOS FileChoosers

--- modules/juce_gui_basics/native/juce_mac_FileChooser.mm
+++ modules/juce_gui_basics/native/juce_mac_FileChooser.mm
@@ -40,7 +40,8 @@ static NSMutableArray* createAllowedTypesArray (const StringArray& filters)
        #if defined (MAC_OS_X_VERSION_10_6) && (MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_6)
         // From OS X 10.6 you can only specify allowed extensions, so any filters containing wildcards
         // must be of the form "*.extension"
-        jassert (filters[i].indexOf ("*") <= 0);
+        jassert (filters[i] == "*"
+                 || (filters[i].startsWith ("*.") && filters[i].lastIndexOfChar ('*') == 0));
        #endif
 
         const String f (filters[i].replace ("*.", ""));
