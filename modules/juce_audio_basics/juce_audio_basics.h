fa0f7a4281b8a863dc2202e09b5b8e2baf8c1f24
ed <eddavies95@gmail.com>
2018-02-08 10:55:52 +0000

    Bump version number to 5.2.1

--- modules/juce_audio_basics/juce_audio_basics.h
+++ modules/juce_audio_basics/juce_audio_basics.h
@@ -31,7 +31,7 @@
 
   ID:               juce_audio_basics
   vendor:           juce
-  version:          5.2.0
+  version:          5.2.1
   name:             JUCE audio and MIDI data classes
   description:      Classes for audio buffer manipulation, midi message handling, synthesis, etc.
   website:          http://www.juce.com/juce
--- modules/juce_osc/juce_osc.h
+++ modules/juce_osc/juce_osc.h
@@ -35,7 +35,7 @@
 
   ID:               juce_osc
   vendor:           juce
-  version:          5.2.0
+  version:          5.2.1
   name:             JUCE OSC classes
   description:      Open Sound Control implementation.
   website:          http://www.juce.com/juce
