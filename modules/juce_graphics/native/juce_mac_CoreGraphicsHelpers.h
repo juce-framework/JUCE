9027d1e5a192911ad0d9e8b68f0df7bd6175c62a
ed <eddavies95@gmail.com>
2018-02-12 12:47:57 +0000

    Implemented setIcon() in NSViewComponentPeer

--- modules/juce_graphics/native/juce_mac_CoreGraphicsHelpers.h
+++ modules/juce_graphics/native/juce_mac_CoreGraphicsHelpers.h
@@ -55,11 +55,15 @@ namespace
     }
 }
 
-extern CGImageRef juce_createCoreGraphicsImage (const Image&, CGColorSpaceRef, bool mustOutliveSource);
-extern CGContextRef juce_getImageContext (const Image&);
+CGImageRef juce_createCoreGraphicsImage (const Image&, CGColorSpaceRef, bool mustOutliveSource);
+CGContextRef juce_getImageContext (const Image&);
 
 #if JUCE_IOS
- extern Image juce_createImageFromUIImage (UIImage*);
+ Image juce_createImageFromUIImage (UIImage*);
+#endif
+
+#if JUCE_MAC
+ NSImage* imageToNSImage (const Image& image, float scaleFactor = 1.0f);
 #endif
 
 } // namespace juce
