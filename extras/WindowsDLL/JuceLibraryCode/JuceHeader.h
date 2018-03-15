18c0c48ce65c7981fc5c1df8e7dc233cc873e79f
ed <eddavies95@gmail.com>
2018-03-15 10:50:46 +0000

    Rename folders and projects in extras/ to be consistent

--- extras/WindowsDLL/JuceLibraryCode/JuceHeader.h
+++ extras/WindowsDLL/JuceLibraryCode/JuceHeader.h
@@ -39,7 +39,7 @@
 #if ! JUCE_DONT_DECLARE_PROJECTINFO
 namespace ProjectInfo
 {
-    const char* const  projectName    = "juce_dll";
+    const char* const  projectName    = "WindowsDLL";
     const char* const  versionString  = "1.0.0";
     const int          versionNumber  = 0x10000;
 }
