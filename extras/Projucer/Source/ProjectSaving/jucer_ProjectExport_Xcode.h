0f9a0e8c7afb1d8684c799ff49e5de421808bfe1
ed <eddavies95@gmail.com>
2018-04-05 12:21:48 +0100

    Projucer:  Fixed a bug where the OSX base SDK version was not being set due to an extra "SDK" being added to the end of the version string

--- extras/Projucer/Source/ProjectSaving/jucer_ProjectExport_Xcode.h
+++ extras/Projucer/Source/ProjectSaving/jucer_ProjectExport_Xcode.h
@@ -1631,7 +1631,7 @@ public:
 
         String getOSXDeploymentTarget (const XcodeBuildConfiguration& config, String* sdkRoot = nullptr) const
         {
-            auto sdk = config.getOSXSDKVersionString() + " SDK";
+            auto sdk = config.getOSXSDKVersionString();
             auto sdkCompat = config.getOSXDeploymentTargetString();
 
             // The AUv3 target always needs to be at least 10.11
