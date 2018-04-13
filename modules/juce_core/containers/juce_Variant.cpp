1ea03d099c110e8b93e8d0cbbcf23d0a3b483651
Ben Kuper <bkuperberg@gmail.com>
2018-04-13 12:23:37 -0700

    Revert "gpl mode and fix var double->toString() decimals"
    This reverts commit 6b6b906aca36e3e16db0894d344ead5baf7b0200.

--- modules/juce_core/containers/juce_Variant.cpp
+++ modules/juce_core/containers/juce_Variant.cpp
@@ -171,7 +171,7 @@ public:
     int toInt (const ValueUnion& data) const noexcept override       { return (int) data.doubleValue; }
     int64 toInt64 (const ValueUnion& data) const noexcept override   { return (int64) data.doubleValue; }
     double toDouble (const ValueUnion& data) const noexcept override { return data.doubleValue; }
-    String toString (const ValueUnion& data) const override          { return String (data.doubleValue); }
+    String toString (const ValueUnion& data) const override          { return String (data.doubleValue, 20); }
     bool toBool (const ValueUnion& data) const noexcept override     { return data.doubleValue != 0.0; }
     bool isDouble() const noexcept override                          { return true; }
 
