juce-framework/JUCE

commit c7c26fa9f033c196c48e743deb6ae16e524ff29a

Removed the OS X 10.5 and 10.6 deployment target options from the Projucer and enabled more C++11 features across all platforms

extras/Projucer/Source/ProjectSaving/jucer_ProjectExport_Xcode.h
@@ -33,7 +33,7 @@ namespace
     const char* const osxVersionDefault         = "10.11";
     const char* const iosVersionDefault         = "9.3";
 
-    const int oldestSDKVersion  = 5;
+    const int oldestSDKVersion  = 7;
     const int currentSDKVersion = 13;
     const int minimumAUv3SDKVersion = 11;
 
@@ -421,7 +421,6 @@ protected:
               osxArchitecture              (config, Ids::osxArchitecture,              getUndoManager(), osxArch_Default),
               customXcodeFlags             (config, Ids::customXcodeFlags,             getUndoManager()),
               plistPreprocessorDefinitions (config, Ids::plistPreprocessorDefinitions, getUndoManager()),
-              cppStandardLibrary           (config, Ids::cppLibType,                   getUndoManager()),
               codeSignIdentity             (config, Ids::codeSigningIdentity,          getUndoManager(), iOS ? "iPhone Developer" : "Mac Developer"),
               fastMathEnabled              (config, Ids::fastMath,                     getUndoManager()),
               stripLocalSymbolsEnabled     (config, Ids::stripLocalSymbols,            getUndoManager()),
@@ -487,11 +486,6 @@ protected:
             props.add (new TextPropertyComponent (plistPreprocessorDefinitions, "PList Preprocessor Definitions", 2048, true),
                        "Preprocessor definitions used during PList preprocessing (see PList Preprocess).");
 
-            props.add (new ChoicePropertyComponent (cppStandardLibrary, "C++ Library",
-                                                    { "LLVM libc++", "GNU libstdc++" },
-                                                    { "libc++",      "libstdc++" }),
-                       "The type of C++ std lib that will be linked.");
-
             props.add (new TextPropertyComponent (codeSignIdentity, "Code-Signing Identity", 1024, false),
                        "The name of a code-signing identity for Xcode to apply.");
 
@@ -513,8 +507,6 @@ protected:
         String getPListPreprocessorDefinitionsString() const    { return plistPreprocessorDefinitions.get(); }
 
         bool isFastMathEnabled() const                          { return fastMathEnabled.get(); }
-
-        String getCPPStandardLibraryString() const              { return cppStandardLibrary.get(); }
 
         bool isStripLocalSymbolsEnabled() const                 { return stripLocalSymbolsEnabled.get(); }
 
@@ -540,7 +532,7 @@ protected:
         bool iOS;
 
         ValueWithDefault osxSDKVersion, osxDeploymentTarget, iosDeploymentTarget, osxArchitecture,
-                         customXcodeFlags, plistPreprocessorDefinitions, cppStandardLibrary, codeSignIdentity,
+                         customXcodeFlags, plistPreprocessorDefinitions, codeSignIdentity,
                          fastMathEnabled, stripLocalSymbolsEnabled, pluginBinaryCopyStepEnabled,
                          vstBinaryLocation, vst3BinaryLocation, auBinaryLocation, rtasBinaryLocation, aaxBinaryLocation;
 
@@ -1129,8 +1121,7 @@ public:
                                                                                               : "c++") + cppStandard).quoted());
             }
 
-            if (config.getCPPStandardLibraryString().isNotEmpty())
-                s.set ("CLANG_CXX_LIBRARY", config.getCPPStandardLibraryString().quoted());
+            s.set ("CLANG_CXX_LIBRARY", "\"libc++\"");
 
             s.set ("COMBINE_HIDPI_IMAGES", "YES");
 
@@ -1577,8 +1568,8 @@ public:
                     = RelativePath (owner.getAAXPathValue().toString(), RelativePath::projectFolder)
                         .getChildFile ("Libs");
 
-                String libraryPath (config.isDebug() ? "Debug/libAAXLibrary" : "Release/libAAXLibrary");
-                libraryPath += (isUsingClangCppLibrary (config) ? "_libcpp.a" : ".a");
+                String libraryPath (config.isDebug() ? "Debug" : "Release");
+                libraryPath += "/libAAXLibrary_libcpp.a";
 
                 extraLibs.add   (aaxLibsFolder.getChildFile (libraryPath));
             }
@@ -1637,25 +1628,6 @@ public:
             }
 
             return targetExtraSearchPaths;
-        }
-
-        bool isUsingClangCppLibrary (const BuildConfiguration& config) const
-        {
-            if (auto xcodeConfig = dynamic_cast<const XcodeBuildConfiguration*> (&config))
-            {
-                auto configValue = xcodeConfig->getCPPStandardLibraryString();
-
-                if (configValue.isNotEmpty())
-                    return (configValue == "libc++");
-
-                auto minorOSXDeploymentTarget = getOSXDeploymentTarget (*xcodeConfig)
-                                               .fromLastOccurrenceOf (".", false, false)
-                                               .getIntValue();
-
-                return (minorOSXDeploymentTarget > 8);
-            }
-
-            return false;
         }
 
         String getOSXDeploymentTarget (const XcodeBuildConfiguration& config, String* sdkRoot = nullptr) const

examples/DemoRunner/Builds/MacOSX/DemoRunner.xcodeproj/project.pbxproj
@@ -214,6 +214,7 @@
 					BFDAF16175D03695EEB466BC, ); name = Source; sourceTree = "<group>"; };
 		B18D059E5616FA729F764229 = {isa = XCBuildConfiguration; buildSettings = {
 				CLANG_CXX_LANGUAGE_STANDARD = "c++14";
+				CLANG_CXX_LIBRARY = "libc++";
 				CLANG_LINK_OBJC_RUNTIME = NO;
 				COMBINE_HIDPI_IMAGES = YES;
 				CONFIGURATION_BUILD_DIR = "$(PROJECT_DIR)/build/$(CONFIGURATION)";
@@ -248,6 +249,7 @@
 				USE_HEADERMAP = NO; }; name = Debug; };
 		69330F27DD2C71609336C7D2 = {isa = XCBuildConfiguration; buildSettings = {
 				CLANG_CXX_LANGUAGE_STANDARD = "c++14";
+				CLANG_CXX_LIBRARY = "libc++";
 				CLANG_LINK_OBJC_RUNTIME = NO;
 				COMBINE_HIDPI_IMAGES = YES;
 				CONFIGURATION_BUILD_DIR = "$(PROJECT_DIR)/build/$(CONFIGURATION)";

examples/DemoRunner/Builds/iOS/DemoRunner.xcodeproj/project.pbxproj
@@ -227,6 +227,7 @@
 				ASSETCATALOG_COMPILER_APPICON_NAME = AppIcon;
 				ASSETCATALOG_COMPILER_LAUNCHIMAGE_NAME = LaunchImage;
 				CLANG_CXX_LANGUAGE_STANDARD = "c++14";
+				CLANG_CXX_LIBRARY = "libc++";
 				CLANG_LINK_OBJC_RUNTIME = NO;
 				CODE_SIGN_ENTITLEMENTS = "DemoRunner.entitlements";
 				COMBINE_HIDPI_IMAGES = YES;
@@ -260,6 +261,7 @@
 				ASSETCATALOG_COMPILER_APPICON_NAME = AppIcon;
 				ASSETCATALOG_COMPILER_LAUNCHIMAGE_NAME = LaunchImage;
 				CLANG_CXX_LANGUAGE_STANDARD = "c++14";
+				CLANG_CXX_LIBRARY = "libc++";
 				CLANG_LINK_OBJC_RUNTIME = NO;
 				CODE_SIGN_ENTITLEMENTS = "DemoRunner.entitlements";
 				COMBINE_HIDPI_IMAGES = YES;

examples/DemoRunner/Source/Demos/DemoPIPs1.cpp
@@ -42,7 +42,7 @@
 #include "../../../Audio/MPEDemo.h"
 #include "../../../Audio/PluckedStringsDemo.h"
 
-#if (defined (JUCE_STDLIB_HAS_STD_FUNCTION_SUPPORT)) && (defined (JUCE_HAS_CONSTEXPR))
+#if JUCE_HAS_CONSTEXPR
  #include "../../../Audio/SimpleFFTDemo.h"
  #include "../../../BLOCKS/BlocksDrawingDemo.h"
  #include "../../../BLOCKS/BlocksMonitorDemo.h"
@@ -71,9 +71,7 @@
 #include "../../../Utilities/LiveConstantDemo.h"
 #include "../../../Utilities/MultithreadingDemo.h"
 #include "../../../Utilities/NetworkingDemo.h"
-#if defined (JUCE_STDLIB_HAS_STD_FUNCTION_SUPPORT)
- #include "../../../Utilities/OSCDemo.h"
-#endif
+#include "../../../Utilities/OSCDemo.h"
 #include "../../../Utilities/SystemInfoDemo.h"
 #include "../../../Utilities/TimersAndEventsDemo.h"
 #include "../../../Utilities/UnitTestsDemo.h"
@@ -92,7 +90,7 @@ void registerDemos_One() noexcept
     REGISTER_DEMO (MPEDemo,                   Audio,     false)
     REGISTER_DEMO (PluckedStringsDemo,        Audio,     false)
 
-   #if (defined (JUCE_STDLIB_HAS_STD_FUNCTION_SUPPORT)) && (defined (JUCE_HAS_CONSTEXPR))
+   #if JUCE_HAS_CONSTEXPR
     REGISTER_DEMO (SimpleFFTDemo,             Audio,     false)
     REGISTER_DEMO (BlocksDrawingDemo,         BLOCKS,    false)
     REGISTER_DEMO (BlocksMonitorDemo,         BLOCKS,    false)
@@ -120,9 +118,7 @@ void registerDemos_One() noexcept
     REGISTER_DEMO (LiveConstantDemo,          Utilities, false)
     REGISTER_DEMO (MultithreadingDemo,        Utilities, false)
     REGISTER_DEMO (NetworkingDemo,            Utilities, false)
-   #if defined (JUCE_STDLIB_HAS_STD_FUNCTION_SUPPORT)
     REGISTER_DEMO (OSCDemo,                   Utilities, false)
-   #endif
     REGISTER_DEMO (SystemInfoDemo,            Utilities, false)
     REGISTER_DEMO (TimersAndEventsDemo,       Utilities, false)
     REGISTER_DEMO_WITH_FILENAME (UnitTestClasses::UnitTestsDemo, Utilities, UnitTestsDemo, false)

examples/DemoRunner/Source/Demos/DemoPIPs2.cpp
@@ -43,12 +43,10 @@
 #include "../../../GUI/ComponentDemo.h"
 #include "../../../GUI/ComponentTransformsDemo.h"
 #include "../../../GUI/DialogsDemo.h"
-#if JUCE_COMPILER_SUPPORTS_INITIALIZER_LISTS
- #include "../../../GUI/FlexBoxDemo.h"
-#endif
+#include "../../../GUI/FlexBoxDemo.h"
 #include "../../../GUI/FontsDemo.h"
 #include "../../../GUI/GraphicsDemo.h"
-#if JUCE_COMPILER_SUPPORTS_INITIALIZER_LISTS && JUCE_HAS_CONSTEXPR
+#if JUCE_HAS_CONSTEXPR
  #include "../../../GUI/GridDemo.h"
 #endif
 #include "../../../GUI/ImagesDemo.h"
@@ -84,12 +82,10 @@ void registerDemos_Two() noexcept
     REGISTER_DEMO (ComponentDemo,             GUI,       false)
     REGISTER_DEMO (ComponentTransformsDemo,   GUI,       false)
     REGISTER_DEMO (DialogsDemo,               GUI,       false)
-   #if JUCE_COMPILER_SUPPORTS_INITIALIZER_LISTS
     REGISTER_DEMO (FlexBoxDemo,               GUI,       false)
-   #endif
     REGISTER_DEMO (FontsDemo,                 GUI,       false)
     REGISTER_DEMO (GraphicsDemo,              GUI,       false)
-   #if JUCE_COMPILER_SUPPORTS_INITIALIZER_LISTS && JUCE_HAS_CONSTEXPR
+   #if JUCE_HAS_CONSTEXPR
     REGISTER_DEMO (GridDemo,                  GUI,       false)
    #endif
     REGISTER_DEMO (ImagesDemo,                GUI,       false)

extras/AudioPerformanceTest/Builds/MacOSX/AudioPerformanceTest.xcodeproj/project.pbxproj
@@ -125,6 +125,7 @@
 					1DFEAF972822E305E013CC06, ); name = Source; sourceTree = "<group>"; };
 		19B7C16D592FB25D09022191 = {isa = XCBuildConfiguration; buildSettings = {
 				CLANG_CXX_LANGUAGE_STANDARD = "c++14";
+				CLANG_CXX_LIBRARY = "libc++";
 				CLANG_LINK_OBJC_RUNTIME = NO;
 				COMBINE_HIDPI_IMAGES = YES;
 				CONFIGURATION_BUILD_DIR = "$(PROJECT_DIR)/build/$(CONFIGURATION)";
@@ -156,6 +157,7 @@
 				USE_HEADERMAP = NO; }; name = Debug; };
 		B7A6988E30C0A68B01EDC53B = {isa = XCBuildConfiguration; buildSettings = {
 				CLANG_CXX_LANGUAGE_STANDARD = "c++14";
+				CLANG_CXX_LIBRARY = "libc++";
 				CLANG_LINK_OBJC_RUNTIME = NO;
 				COMBINE_HIDPI_IMAGES = YES;
 				CONFIGURATION_BUILD_DIR = "$(PROJECT_DIR)/build/$(CONFIGURATION)";

extras/AudioPerformanceTest/Builds/iOS/AudioPerformanceTest.xcodeproj/project.pbxproj
@@ -136,6 +136,7 @@
 				ASSETCATALOG_COMPILER_APPICON_NAME = AppIcon;
 				ASSETCATALOG_COMPILER_LAUNCHIMAGE_NAME = LaunchImage;
 				CLANG_CXX_LANGUAGE_STANDARD = "c++14";
+				CLANG_CXX_LIBRARY = "libc++";
 				CLANG_LINK_OBJC_RUNTIME = NO;
 				COMBINE_HIDPI_IMAGES = YES;
 				CONFIGURATION_BUILD_DIR = "$(PROJECT_DIR)/build/$(CONFIGURATION)";
@@ -166,6 +167,7 @@
 				ASSETCATALOG_COMPILER_APPICON_NAME = AppIcon;
 				ASSETCATALOG_COMPILER_LAUNCHIMAGE_NAME = LaunchImage;
 				CLANG_CXX_LANGUAGE_STANDARD = "c++14";
+				CLANG_CXX_LIBRARY = "libc++";
 				CLANG_LINK_OBJC_RUNTIME = NO;
 				COMBINE_HIDPI_IMAGES = YES;
 				CONFIGURATION_BUILD_DIR = "$(PROJECT_DIR)/build/$(CONFIGURATION)";

extras/AudioPluginHost/Builds/MacOSX/AudioPluginHost.xcodeproj/project.pbxproj
@@ -197,6 +197,7 @@
 					D85C0D11EE4F6C73B9EB5BCD, ); name = Source; sourceTree = "<group>"; };
 		C8B793AC1BEFBE7A99BE8352 = {isa = XCBuildConfiguration; buildSettings = {
 				CLANG_CXX_LANGUAGE_STANDARD = "c++14";
+				CLANG_CXX_LIBRARY = "libc++";
 				CLANG_LINK_OBJC_RUNTIME = NO;
 				COMBINE_HIDPI_IMAGES = YES;
 				CONFIGURATION_BUILD_DIR = "$(PROJECT_DIR)/build/$(CONFIGURATION)";
@@ -229,6 +230,7 @@
 				USE_HEADERMAP = NO; }; name = Debug; };
 		49453CC5AD9F08D2738464AC = {isa = XCBuildConfiguration; buildSettings = {
 				CLANG_CXX_LANGUAGE_STANDARD = "c++14";
+				CLANG_CXX_LIBRARY = "libc++";
 				CLANG_LINK_OBJC_RUNTIME = NO;
 				COMBINE_HIDPI_IMAGES = YES;
 				CONFIGURATION_BUILD_DIR = "$(PROJECT_DIR)/build/$(CONFIGURATION)";

commit 55f7526c01d00609b1247b239e5b9b37f786410b

Maintain RTAS build broken by changes in c7c26fa9f033c196c48e743deb6ae16e524ff29a
(We still build the RTAS lib with a _libcpp suffix..)

extras/Projucer/Source/ProjectSaving/jucer_ProjectExport_Xcode.h
@@ -1602,7 +1602,7 @@ public:
                         .getChildFile ("MacBag").getChildFile ("Libs");
 
                 String libraryPath (config.isDebug() ? "Debug/libPluginLibrary" : "Release/libPluginLibrary");
-                libraryPath += (isUsingClangCppLibrary (config) ? "_libcpp.a" : ".a");
+                libraryPath += "_libcpp.a";
 
                 extraLibs.add (rtasFolder.getChildFile (libraryPath));
             }