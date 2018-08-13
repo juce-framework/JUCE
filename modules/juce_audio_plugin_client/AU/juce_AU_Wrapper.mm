49aecb6eb0f8508311ebcbf186b7d84872ab6671
Tom Poole <thomas.poole@roli.com>
2018-08-13 11:10:30 +0100

    AU:  Fixed a bug with JUCE_FORCE_LEGACY_PARAM_IDS enabled

--- modules/juce_audio_plugin_client/AU/juce_AU_Wrapper.mm
+++ modules/juce_audio_plugin_client/AU/juce_AU_Wrapper.mm
@@ -1938,7 +1938,9 @@ private:
 
     AudioProcessorParameter* getParameterForAUParameterID (AudioUnitParameterID address) const noexcept
     {
-        return paramMap[static_cast<int32> (address)];
+        auto index = static_cast<int32> (address);
+        return forceUseLegacyParamIDs ? juceParameters.getParamForIndex (index)
+                                      : paramMap[index];
     }
 
     //==============================================================================
