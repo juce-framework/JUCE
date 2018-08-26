c0a83ed1ea3821aa3c3084ee82b20e1037a73cb9
John Joseph <mynameisjohnj@gmail.com>
2018-08-26 19:36:16 -0400

    Updating the ordering of these flags so that ARA comes last - this properly groups the code and also mirrors the order they appear in the UI.

--- extras/Projucer/Source/ProjectSaving/jucer_ProjectSaver.cpp
+++ extras/Projucer/Source/ProjectSaving/jucer_ProjectSaver.cpp
@@ -116,19 +116,19 @@ void ProjectSaver::writePluginCharacteristicsFile()
     flags.set ("JucePlugin_RTASDisableBypass",           boolToString (project.isPluginRTASBypassDisabled()));
     flags.set ("JucePlugin_RTASDisableMultiMono",        boolToString (project.isPluginRTASMultiMonoDisabled()));
     flags.set ("JucePlugin_AAXIdentifier",               project.getAAXIdentifierString());
-    flags.set ("JucePlugin_ARAFactoryID",                toStringLiteral(project.getARAFactoryIDString()));
-    flags.set ("JucePlugin_ARAVST3FactoryUUID",          project.getARAVST3FactoryUUIDString());
-    flags.set ("JucePlugin_ARADocumentArchiveID",        toStringLiteral(project.getARADocumentArchiveIDString()));
     flags.set ("JucePlugin_AAXManufacturerCode",         "JucePlugin_ManufacturerCode");
     flags.set ("JucePlugin_AAXProductId",                "JucePlugin_PluginCode");
     flags.set ("JucePlugin_AAXCategory",                 String (project.getAAXCategory()));
-    flags.set ("JucePlugin_ARAContentTypes",             String (project.getARAContentTypes()));
-    flags.set ("JucePlugin_ARATransformationFlags",      String (project.getARATransformationFlags()));
     flags.set ("JucePlugin_AAXDisableBypass",            boolToString (project.isPluginAAXBypassDisabled()));
     flags.set ("JucePlugin_AAXDisableMultiMono",         boolToString (project.isPluginAAXMultiMonoDisabled()));
     flags.set ("JucePlugin_IAAType",                     toCharLiteral (project.getIAATypeCode()));
     flags.set ("JucePlugin_IAASubType",                  "JucePlugin_PluginCode");
     flags.set ("JucePlugin_IAAName",                     project.getIAAPluginName().quoted());
+    flags.set ("JucePlugin_ARAContentTypes",             String (project.getARAContentTypes()));
+    flags.set ("JucePlugin_ARATransformationFlags",      String (project.getARATransformationFlags()));
+    flags.set ("JucePlugin_ARAFactoryID",                toStringLiteral(project.getARAFactoryIDString()));
+    flags.set ("JucePlugin_ARADocumentArchiveID",        toStringLiteral(project.getARADocumentArchiveIDString()));
+    flags.set ("JucePlugin_ARAVST3FactoryUUID",          project.getARAVST3FactoryUUIDString());
 
     {
         String plugInChannelConfig = project.getPluginChannelConfigsString();
