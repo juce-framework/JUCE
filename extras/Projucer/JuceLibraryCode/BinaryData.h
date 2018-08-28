7657f1816afacc86ebbff7cb59a41c4ef4764145
John Joseph <mynameisjohnj@gmail.com>
2018-08-28 09:06:51 -0400

    Revert "Properly implementing [d158b3e] - creating two new template files for our JUCE ARA document controller and adding them to the .jucer file for the Projucer so that the binary data gets auto generated. I'm not enjoying the diff to BinaryData.cpp / h - should those be checked in using some sort of LFS?"
    This reverts commit c5276aa14ff24180a06b112eb1d0d55b8a41d259.

--- extras/Projucer/JuceLibraryCode/BinaryData.h
+++ extras/Projucer/JuceLibraryCode/BinaryData.h
@@ -173,9 +173,6 @@ namespace BinaryData
     extern const char*   jucer_PIPTemplate_h;
     const int            jucer_PIPTemplate_hSize = 517;
 
-    extern const char*   jucer_UnityPluginGUIScript_cs;
-    const int            jucer_UnityPluginGUIScript_csSize = 6426;
-
     extern const char*   colourscheme_dark_xml;
     const int            colourscheme_dark_xmlSize = 1050;
 
@@ -195,7 +192,7 @@ namespace BinaryData
     const int            RecentFilesMenuTemplate_nibSize = 2842;
 
     // Number of elements in the namedResourceList and originalFileNames arrays.
-    const int namedResourceListSize = 62;
+    const int namedResourceListSize = 61;
 
     // Points to the start of a list of resource names.
     extern const char* namedResourceList[];
