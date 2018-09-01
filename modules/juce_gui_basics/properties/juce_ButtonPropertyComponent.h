de4a70104286f2e50a8d49180723c5f63a81a1e9
Xav83 <x.jouvenot@expressivee.com>
2018-09-01 12:42:50 +0200

    Adds missing override keyword in ButtonPropertyComponent class.

--- modules/juce_gui_basics/properties/juce_ButtonPropertyComponent.h
+++ modules/juce_gui_basics/properties/juce_ButtonPropertyComponent.h
@@ -66,7 +66,7 @@ public:
 
     //==============================================================================
     /** @internal */
-    void refresh();
+    void refresh() override;
 
 private:
     TextButton button;
