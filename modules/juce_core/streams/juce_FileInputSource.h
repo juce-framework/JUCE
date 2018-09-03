ba85dd7a053a339c6987ee3f569f228de2a187a1
ed <eddavies95@gmail.com>
2018-09-03 10:54:16 +0100

    Added some missing override keywords

--- modules/juce_core/streams/juce_FileInputSource.h
+++ modules/juce_core/streams/juce_FileInputSource.h
@@ -45,9 +45,9 @@ public:
     /** Destructor. */
     ~FileInputSource();
 
-    InputStream* createInputStream();
-    InputStream* createInputStreamFor (const String& relatedItemPath);
-    int64 hashCode() const;
+    InputStream* createInputStream() override;
+    InputStream* createInputStreamFor (const String& relatedItemPath) override;
+    int64 hashCode() const override;
 
 private:
     //==============================================================================
