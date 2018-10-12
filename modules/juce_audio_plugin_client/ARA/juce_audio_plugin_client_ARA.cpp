9d65c9881ee2c821f4488aa2414bbd485d7e6b3f
John Joseph <mynameisjohnj@gmail.com>
2018-10-11 21:30:19 -0400

    Surrounding all the code in these files with the JucePlugin_Enable_ARA fence -  I don't think it's necessary since anyone including them is already checking this, but it can't hurt I suppose.

--- modules/juce_audio_plugin_client/ARA/juce_audio_plugin_client_ARA.cpp
+++ modules/juce_audio_plugin_client/ARA/juce_audio_plugin_client_ARA.cpp
@@ -1,30 +1,32 @@
-// TODO formalize ARA includes
+#if JucePlugin_Enable_ARA
 
-#include "juce_ARA_interface.h"
-
-#include <ARA_Library/PlugIn/ARAPlug.cpp>
-#include <ARA_Library/Debug/ARADebug.c>
-#include <ARA_Library/Dispatch/ARAPlugInDispatch.cpp>
-
-// Include these source files directly for now
-#include "juce_ARADocumentController.cpp"
-#include "juce_ARARegionSequence.cpp"
-#include "juce_ARAAudioSource.cpp"
-
-namespace juce
-{
-
-#if (JUCE_DEBUG && ! JUCE_DISABLE_ASSERTIONS) || JUCE_LOG_ASSERTIONS
-     JUCE_API void JUCE_CALLTYPE handleARAAssertion(const char* file, const int line, const char* diagnosis) noexcept
-     {
- #if (JUCE_DEBUG && ! JUCE_DISABLE_ASSERTIONS)
-         DBG(diagnosis);
+ #include "juce_ARA_interface.h"
+ 
+ #include <ARA_Library/PlugIn/ARAPlug.cpp>
+ #include <ARA_Library/Debug/ARADebug.c>
+ #include <ARA_Library/Dispatch/ARAPlugInDispatch.cpp>
+ 
+ // Include these source files directly for now
+ #include "juce_ARADocumentController.cpp"
+ #include "juce_ARARegionSequence.cpp"
+ #include "juce_ARAAudioSource.cpp"
+ 
+ namespace juce
+ {
+ 
+ #if (JUCE_DEBUG && ! JUCE_DISABLE_ASSERTIONS) || JUCE_LOG_ASSERTIONS
+      JUCE_API void JUCE_CALLTYPE handleARAAssertion(const char* file, const int line, const char* diagnosis) noexcept
+      {
+  #if (JUCE_DEBUG && ! JUCE_DISABLE_ASSERTIONS)
+          DBG(diagnosis);
+  #endif
+  
+          juce::logAssertion (file, line);
+  
+          jassertfalse;
+      }
  #endif
  
-         juce::logAssertion (file, line);
- 
-         jassertfalse;
-     }
-#endif
+ } // namespace juce
 
-} // namespace juce
+#endif JucePlugin_Enable_ARA
