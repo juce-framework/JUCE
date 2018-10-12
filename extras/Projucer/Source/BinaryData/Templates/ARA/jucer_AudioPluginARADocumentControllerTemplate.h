eb2b27913220388eae903083f32ff952908115e5
John Joseph <mynameisjohnj@gmail.com>
2018-10-11 21:22:12 -0400

    Extra semicolon.

--- extras/Projucer/Source/BinaryData/Templates/ARA/jucer_AudioPluginARADocumentControllerTemplate.h
+++ extras/Projucer/Source/BinaryData/Templates/ARA/jucer_AudioPluginARADocumentControllerTemplate.h
@@ -31,4 +31,4 @@ protected:
 private:
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (%%aradocumentcontroller_class_name%%)
-};;
+};
