5f7ec058472421461fe25d2dcc2c1d108221b4e6
John Joseph <mynameisjohnj@gmail.com>
2018-10-11 21:15:25 -0400

    Setting up ARA debug macros from juce_ARA_interface.h. We use the JUCE debug related defines to configure ARA debug settings as well as the assertion handler function. This juce::handleARAAssertion function ultimately jasserts false - I'm not sure of the best way to conform to the JUCE_LOG_ASSERTIONS define, but perhaps that should be the condition for us logging the ARA assertion diagnosis?

--- modules/juce_audio_plugin_client/ARA/juce_ARA_interface.h
+++ modules/juce_audio_plugin_client/ARA/juce_ARA_interface.h
@@ -3,5 +3,24 @@
 // Include juce preamble
 #include "JuceHeader.h"
 
+// Configure ARA debug support prior to including ARA headerse
+#if (JUCE_DEBUG && ! JUCE_DISABLE_ASSERTIONS) || JUCE_LOG_ASSERTIONS
+ // define a juce assert and define the ARA_INTERNAL_ASSERT macro to use the function below
+ namespace juce
+ {
+     extern JUCE_API void JUCE_CALLTYPE handleARAAssertion (const char* file, const int line, const char* diagnosis) noexcept;
+ }
+ // Enable ARA_INTERNAL_ASSERT and define it to be juce::handleARAAssertion
+ #define ARA_ENABLE_INTERNAL_ASSERTS 1
+ #define ARA_HANDLE_ASSERT(file, line, diagnosis)    juce::handleARAAssertion(file, line, diagnosis)
+ 
+ #if JUCE_LOG_ASSERTIONS
+  #define ARA_ENABLE_DEBUG_OUTPUT 1
+ #endif
+#else
+ // Otherwise disable ARA internal asserts
+ #define ARA_ENABLE_INTERNAL_ASSERTS 0
+#endif
+
 // Include ARA headers
 #include <ARA_Library/PlugIn/ARAPlug.h>
--- modules/juce_audio_plugin_client/ARA/juce_audio_plugin_client_ARA.cpp
+++ modules/juce_audio_plugin_client/ARA/juce_audio_plugin_client_ARA.cpp
@@ -10,3 +10,21 @@
 #include "juce_ARADocumentController.cpp"
 #include "juce_ARARegionSequence.cpp"
 #include "juce_ARAAudioSource.cpp"
+
+namespace juce
+{
+
+#if (JUCE_DEBUG && ! JUCE_DISABLE_ASSERTIONS) || JUCE_LOG_ASSERTIONS
+     JUCE_API void JUCE_CALLTYPE handleARAAssertion(const char* file, const int line, const char* diagnosis) noexcept
+     {
+ #if (JUCE_DEBUG && ! JUCE_DISABLE_ASSERTIONS)
+         DBG(diagnosis);
+ #endif
+ 
+         juce::logAssertion (file, line);
+ 
+         jassertfalse;
+     }
+#endif
+
+} // namespace juce
