1bafc5b0a9744fb8462c8eed621a52151c3fb510
Stefan Gretscher <ara@celemony.com>
2018-10-18 02:35:28 +0200

    using appropriate initializer for factory to avoid warnings, plus related cleanups

--- modules/juce_audio_plugin_client/ARA/juce_ARADocumentController.cpp
+++ modules/juce_audio_plugin_client/ARA/juce_ARADocumentController.cpp
@@ -11,40 +11,27 @@ const ARA::ARAFactory* ARA::PlugIn::DocumentController::getARAFactory ()
     static ARAFactory* factory = nullptr;
     if (factory == nullptr)
     {
-        factory = new SizedStruct<ARA_MEMBER_PTR_ARGS (ARAFactory, supportedPlaybackTransformationFlags)>;
+        factory = new SizedStruct<ARA_MEMBER_PTR_ARGS (ARAFactory, supportedPlaybackTransformationFlags)>(
+                                                        // Supported API generations
+                                                        kARAAPIGeneration_2_0_Draft, kARAAPIGeneration_2_0_Final,
+                                                        // Factory ID
+                                                        JucePlugin_ARAFactoryID,
+                                                        // ARA lifetime management functions
+                                                        ARAInitialize, ARAUninitialize,
+                                                        // Strings for user dialogs
+                                                        JucePlugin_Name, JucePlugin_Manufacturer,
+                                                        JucePlugin_ManufacturerWebsite, JucePlugin_VersionString,
+                                                        // DocumentController factory function
+                                                        ARACreateDocumentControllerWithDocumentInstance,
+                                                        // Document archive IDs
+                                                        // TODO add a way to update compatible archive IDs and count if needed!
+                                                        JucePlugin_ARADocumentArchiveID, 0U, nullptr,
+                                                        // Analyzeable content types - will be updated below
+                                                        0U, nullptr,
+                                                        // Playback transformation flags - will be updated below
+                                                        0);
 
-        // Supported API generations (forced, I guess...)
-        factory->lowestSupportedApiGeneration = kARAAPIGeneration_2_0_Draft;
-        factory->highestSupportedApiGeneration = kARAAPIGeneration_2_0_Final;
-
-        // Factory ID
-        factory->factoryID = JucePlugin_ARAFactoryID;
-
-        // ARA lifetime management functions
-        factory->initializeARAWithConfiguration = ARAInitialize;
-        factory->uninitializeARA = ARAUninitialize;
-
-        // Plugin Name
-        factory->plugInName = JucePlugin_Name;
-
-        // Manufacturer Name
-        factory->manufacturerName = JucePlugin_Manufacturer;
-
-        // Info URL
-        factory->informationURL = JucePlugin_ManufacturerWebsite;
-
-        // Version
-        factory->version = JucePlugin_VersionString;
-
-        // Document Controller factory function
-        factory->createDocumentControllerWithDocument = ARACreateDocumentControllerWithDocumentInstance;
-
-        // Document archive ID
-        factory->documentArchiveID = JucePlugin_ARADocumentArchiveID;
-
-        // TODO Compatible archive IDs and count
-
-        // Content types
+        // Update analyzeable content types
         static std::vector<ARAContentType> contentTypes;
         static ARAContentType araContentVars[]{
             kARAContentTypeNotes,
@@ -56,17 +43,16 @@ const ARA::ARAFactory* ARA::PlugIn::DocumentController::getARAFactory ()
             kARAContentTypeKeySignatures,
             kARAContentTypeSheetChords
         };
-        for (int i = 0; i < sizeof(araContentVars) / sizeof(ARAContentType); i++)
+        for (size_t i = 0; i < sizeof(araContentVars) / sizeof(ARAContentType); i++)
         {
             if (JucePlugin_ARAContentTypes & (1 << i))
                 contentTypes.push_back(araContentVars[i]);
         }
 
-        delete factory->analyzeableContentTypes;
-        factory->analyzeableContentTypesCount = (int) contentTypes.size();
+        factory->analyzeableContentTypesCount = (ARASize) contentTypes.size();
         factory->analyzeableContentTypes = contentTypes.data();
 
-        // Playback Transformation Flags
+        // Update playback transformation flags
         static ARAPlaybackTransformationFlags araPlaybackTransformations[]{
             kARAPlaybackTransformationTimestretch,
             kARAPlaybackTransformationTimestretchReflectingTempo,
@@ -75,13 +61,11 @@ const ARA::ARAFactory* ARA::PlugIn::DocumentController::getARAFactory ()
         };
 
         factory->supportedPlaybackTransformationFlags = 0;
-        for (int i = 0; i < sizeof(araPlaybackTransformations) / sizeof(ARAPlaybackTransformationFlags); i++)
+        for (size_t i = 0; i < sizeof(araPlaybackTransformations) / sizeof(ARAPlaybackTransformationFlags); i++)
         {
             if (JucePlugin_ARATransformationFlags & (1 << i))
                 factory->supportedPlaybackTransformationFlags |= araPlaybackTransformations[i];
         }
-
-        // TODO Algorithms
     }
 
     return factory;
