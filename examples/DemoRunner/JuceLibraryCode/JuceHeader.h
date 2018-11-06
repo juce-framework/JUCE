08928231af8f14e29ebd492c6ce8a8ec22c5b78e
ed <eddavies95@gmail.com>
2018-11-06 14:03:15 +0000

    Re-saved all projects

--- examples/DemoRunner/JuceLibraryCode/JuceHeader.h
+++ examples/DemoRunner/JuceLibraryCode/JuceHeader.h
@@ -47,7 +47,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "DemoRunner";
     const char* const  companyName    = "ROLI Ltd.";
-    const char* const  versionString  = "5.3.2";
-    const int          versionNumber  = 0x50302;
+    const char* const  versionString  = "5.4.0";
+    const int          versionNumber  = 0x50400;
 }
 #endif
