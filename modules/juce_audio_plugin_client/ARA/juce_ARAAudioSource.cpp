64891dc4b2219c0b2d71391551a31212720ee1c9
John Joseph <mynameisjohnj@gmail.com>
2018-11-13 17:32:46 -0500

    Making a new AudioFormatReader class called ARAAudioSourceReader that was very heavily copied from the exisitng ARAAudioSource::Reader class. The differences here are a) we use a lock per reader, allowing new readers to be constructed while existing readers are reading samples (and are read-locked) b) we no longer rely on the SafeRef type (though we do have to explicitly try/lock in readSamples)

--- modules/juce_audio_plugin_client/ARA/juce_ARAAudioSource.cpp
+++ modules/juce_audio_plugin_client/ARA/juce_ARAAudioSource.cpp
@@ -134,7 +134,7 @@ void ARAAudioSource::doUpdateAudioSourceContent (ARA::PlugIn::AudioSource* audio
 
 std::unique_ptr<BufferingAudioSource> ARAAudioSource::createBufferingAudioSource (TimeSliceThread& thread, int bufferSize)
 {
-    return std::unique_ptr<BufferingAudioSource> (new BufferingAudioSource (new AudioFormatReaderSource (newReader(), true), thread, true, bufferSize));
+    return std::unique_ptr<BufferingAudioSource> (new BufferingAudioSource (new AudioFormatReaderSource (new ARAAudioSourceReader (this), true), thread, true, bufferSize));
 }
 
 ARAAudioSource::Reader::Reader (ARAAudioSource* source)
@@ -206,4 +206,126 @@ bool ARAAudioSource::Reader::readSamples (
     return araHostReader->readAudioSamples (startSampleInFile, numSamples, tmpPtrs.data());
 }
 
+// =============================================================================================================
+
+ARAAudioSourceReader::ARAAudioSourceReader (ARA::PlugIn::AudioSource* source, bool use64BitSamples)
+: AudioFormatReader (nullptr, "ARAAudioSourceReader"),
+  ARAAudioSourceUpdateListener (source->getDocument ()->getDocumentController ())
+{
+    bitsPerSample = use64BitSamples ? 64 : 32;
+    usesFloatingPointData = true;
+    sampleRate = source->getSampleRate ();
+    numChannels = source->getChannelCount ();
+    lengthInSamples = source->getSampleCount ();
+    tmpPtrs.resize (numChannels);
+    audioSourceBeingRead = source;
+
+    if (audioSourceBeingRead->isSampleAccessEnabled ())
+        recreate ();
+}
+
+ARAAudioSourceReader::~ARAAudioSourceReader ()
+{
+    ScopedWriteLock l (lock);
+    invalidate ();
+}
+
+void ARAAudioSourceReader::willEnableAudioSourceSamplesAccess (ARA::PlugIn::AudioSource* audioSource, bool enable) noexcept
+{
+    if (audioSource != audioSourceBeingRead)
+        return;
+
+    // unlocked in didEnableAudioSourceSamplesAccess
+    lock.enterWrite ();
+
+    // invalidate our reader if sample access is disabled
+    if (enable == false)
+        invalidate ();
+}
+
+void ARAAudioSourceReader::didEnableAudioSourceSamplesAccess (ARA::PlugIn::AudioSource* audioSource, bool enable) noexcept
+{
+    // following the invalidation above we can recreate any readers
+    // we had before access was disabled
+    if (audioSource != audioSourceBeingRead)
+        return;
+
+    // recreate our reader if sample access is enabled
+    if (enable)
+        recreate ();
+
+    lock.exitWrite ();
+}
+
+void ARAAudioSourceReader::willDestroyAudioSource (ARA::PlugIn::AudioSource* audioSource) noexcept
+{
+    if (audioSource != audioSourceBeingRead)
+        return;
+
+    // TODO JUCE_ARA
+    // should this ever happen? ideally someone delete us instead...
+    // jassertfalse;
+    ScopedWriteLock scopedLock (lock);
+    invalidate ();
+    audioSourceBeingRead = nullptr;
+}
+
+void ARAAudioSourceReader::doUpdateAudioSourceContent (ARA::PlugIn::AudioSource* audioSource, const ARA::ARAContentTimeRange* range, ARA::ARAContentUpdateFlags flags) noexcept
+{
+    if (audioSource != audioSourceBeingRead)
+        return;
+
+    // don't invalidate if the audio signal is unchanged
+    if (flags & ARA::kARAContentUpdateSignalScopeRemainsUnchanged)
+        return;
+
+    ScopedWriteLock scopedLock (lock);
+    invalidate ();
+}
+
+void ARAAudioSourceReader::recreate ()
+{
+    // TODO JUCE_ARA
+    // it shouldnt' be possible for araHostReader to contain data at this point, 
+    // but should we assert that?
+    jassert (audioSourceBeingRead->isSampleAccessEnabled ());
+    araHostReader.reset (new ARA::PlugIn::HostAudioReader (audioSourceBeingRead));
+}
+
+void ARAAudioSourceReader::invalidate ()
+{
+    araHostReader.reset ();
+}
+
+bool ARAAudioSourceReader::readSamples (
+    int** destSamples,
+    int numDestChannels,
+    int startOffsetInDestBuffer,
+    int64 startSampleInFile,
+    int numSamples)
+{
+    // If we can't enter the lock or we don't have a reader, zero samples and return false
+    if (!lock.tryEnterRead () || araHostReader == nullptr)
+    {
+        for (int chan_i = 0; chan_i < (int) tmpPtrs.size (); ++chan_i)
+            FloatVectorOperations::clear ((float *) destSamples[chan_i], numSamples);
+        return false;
+    }
+
+    jassert (audioSourceBeingRead != nullptr);
+
+    for (int chan_i = 0; chan_i < (int) tmpPtrs.size (); ++chan_i)
+        if (chan_i < numDestChannels && destSamples[chan_i] != nullptr)
+            tmpPtrs[chan_i] = (void*) (destSamples[chan_i] + startOffsetInDestBuffer);
+        else
+        {
+            if (numSamples > (int) dummyBuffer.size ())
+                dummyBuffer.resize (numSamples);
+            tmpPtrs[chan_i] = (void*) dummyBuffer.data ();
+        }
+
+    bool success = araHostReader->readAudioSamples (startSampleInFile, numSamples, tmpPtrs.data ());
+    lock.exitRead ();
+    return success;
+}
 } // namespace juce
--- modules/juce_audio_plugin_client/ARA/juce_ARAAudioSource.h
+++ modules/juce_audio_plugin_client/ARA/juce_ARAAudioSource.h
@@ -40,4 +40,42 @@ private:
    #endif
 };
 
+class ARAAudioSourceReader : public AudioFormatReader,
+                             ARAAudioSourceUpdateListener
+{
+public:
+    ARAAudioSourceReader (ARA::PlugIn::AudioSource* audioSource, bool use64BitSamples = false);
+    ~ARAAudioSourceReader ();
+
+    void recreate ();
+    void invalidate ();
+
+    bool readSamples (
+        int** destSamples,
+        int numDestChannels,
+        int startOffsetInDestBuffer,
+        int64 startSampleInFile,
+        int numSamples) override;
+
+    // TODO JUCE_ARA
+    // do we need to handle property updates?
+    // any other invalidation hooks? 
+    void willEnableAudioSourceSamplesAccess (ARA::PlugIn::AudioSource* audioSource, bool enable) noexcept override;
+    void didEnableAudioSourceSamplesAccess (ARA::PlugIn::AudioSource* audioSource, bool enable) noexcept override;
+    void willDestroyAudioSource (ARA::PlugIn::AudioSource* audioSource) noexcept override;
+    void doUpdateAudioSourceContent (ARA::PlugIn::AudioSource* audioSource, const ARA::ARAContentTimeRange* range, ARA::ARAContentUpdateFlags flags) noexcept override;
+
+private:
+    std::vector<void*> tmpPtrs;
+
+    // per reader locks means we can create readers while others are reading
+    ReadWriteLock lock;
+
+    // When readSamples is not reading all channels,
+    // we still need to provide pointers to all channels to the ARA read call.
+    // So we'll read the other channels into this dummy buffer.
+    std::vector<float> dummyBuffer;
+    ARA::PlugIn::AudioSource* audioSourceBeingRead; // TODO JUCE_ARA make this const
+    std::unique_ptr<ARA::PlugIn::HostAudioReader> araHostReader;
+};
 } // namespace juce
