b8a895d83483a9950ebcdc27df99e9ac90e7f481
Stefan Gretscher <ara@celemony.com>
2018-11-17 11:46:31 +0100

    optimizing proposed notification pattern for safer listeners
    @jj please see todo, I think we should move this to ListenerList for now since it’s widely used.

--- modules/juce_audio_plugin_client/ARA/juce_ARAPlaybackRegion.cpp
+++ modules/juce_audio_plugin_client/ARA/juce_ARAPlaybackRegion.cpp
@@ -28,16 +28,32 @@ void ARAPlaybackRegion::willDestroyPlaybackRegion() noexcept
 // So we have to roll a custom version, which is not entirely possible due to the lock not being accesible.
 // Started a thread in the JUCE forum to figure out what to do:
 // https://forum.juce.com/t/listenerlist-issue-need-reliable-notifications-if-adding-removing-listeners-from-within-a-callback/30361
+// @jj for the time being, I suggest we edit the ListenerList class and add some
+//     callExpectingUnregistration () variant which we use in all the willDestroy... ()
+//     Unsure about the other calls, I tend to keeping call () there for performance reasons...?
 
-// This is not a generic implementation. It'll work well here due to the context,
-// but is not thread safe, and potentially no great performance either...
-//  auto lock (listeners.getLock());
-    auto listenersCopy (listeners.getListeners());
-    for (auto listener : listenersCopy)
-    {
-        if (listeners.contains (listener))
-            listener->willDestroyPlaybackRegion (this);
-    }
+    if (listeners.isEmpty() == 0)
+        return;
+
+    auto& listenersArray = listeners.getListeners();
+
+// not thread safe without the lock, but currently there's no outside acceess to it...!
+//  auto lock (listenersArray.getLock());
+
+	if (listenersArray.size() == 1)
+	{
+        // if there's but one listener, we can skip copying the array
+		listenersArray.getFirst()->willDestroyPlaybackRegion (this);
+	}
+	else
+	{
+		auto listenersCopy (listenersArray);
+		for (auto listener : listenersCopy)
+		{
+			if (listeners.contains (listener))
+				listener->willDestroyPlaybackRegion (this);
+		}
+	}
 }
 
 void ARAPlaybackRegion::addListener (Listener * l) 
