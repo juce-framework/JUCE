434a600bfbab964a935087d0df24cd8db6f4b8f6
Stefan Gretscher <ara@celemony.com>
2018-11-18 22:47:04 +0100

    removing ToDo that has already been solved in [d794399]

--- modules/juce_audio_plugin_client/ARA/juce_AudioProcessorEditorARAExtension.h
+++ modules/juce_audio_plugin_client/ARA/juce_AudioProcessorEditorARAExtension.h
@@ -15,8 +15,6 @@ public:
     AudioProcessorEditorARAExtension (AudioProcessor* audioProcessor);
     virtual ~AudioProcessorEditorARAExtension();
 
-    // TODO JUCE_ARA return proper JUCE class here once we've added it to the framework to handle selection -
-    // see static_cast<ARASampleProjectEditorView*> in ARASampleProjectAudioProcessorEditor
     ARAEditorView* getARAEditorView() const noexcept;
 
     bool isARAEditorView() const noexcept { return getARAEditorView() != nullptr; }
