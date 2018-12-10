203b50a1898928f7eb2b29b447eb3a4c9b843992
ed <eddavies95@gmail.com>
2018-12-10 09:26:04 +0000

    Projucer:  Use the legacy build system for Xcode projects to work around some issues with Xcode 10

--- extras/Projucer/Source/ProjectSaving/jucer_ProjectExport_Xcode.h
+++ extras/Projucer/Source/ProjectSaving/jucer_ProjectExport_Xcode.h
@@ -397,6 +397,10 @@ public:
 
         writeInfoPlistFiles();
 
+        // This forces the project to use the legacy build system to workaround Xcode 10 issues,
+        // hopefully these will be fixed in the future and this can be removed...
+        writeWorkspaceSettings();
+
         // Deleting the .rsrc files can be needed to force Xcode to update the version number.
         deleteRsrcFiles (getTargetFolder().getChildFile ("build"));
     }
@@ -2280,6 +2284,27 @@ private:
             iconFile = getTargetFolder().getChildFile ("Icon.icns");
             overwriteFileIfDifferentOrThrow (iconFile, mo);
         }
+    }
+
+    void writeWorkspaceSettings() const
+    {
+        auto settingsFile = getProjectBundle().getChildFile ("project.xcworkspace")
+                                              .getChildFile ("xcshareddata")
+                                              .getChildFile ("WorkspaceSettings.xcsettings");
+
+        MemoryOutputStream mo;
+        mo.setNewLineString ("\n");
+
+        mo << "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" << newLine
+           << "<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">" << newLine
+           << "<plist version=\"1.0\">"                    << newLine
+           << "<dict>"                                     << newLine
+           << "    <key>BuildSystemType</key>"             << newLine
+           << "    <string>Original</string>"              << newLine
+           << "</dict>"                                    << newLine
+           << "</plist>"                                   << newLine;
+
+        overwriteFileIfDifferentOrThrow (settingsFile, mo);
     }
 
     void writeInfoPlistFiles() const
