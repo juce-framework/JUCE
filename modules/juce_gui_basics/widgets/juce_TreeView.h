d86a672f7ecf3605cde461f78057484b67500293
bill-auger <mr.j.spam.me@gmail.com>
2019-01-06 19:25:45 -0500

    mention findItemFromIdentifierString() in TreeView::getUniqueName() doc

--- modules/juce_gui_basics/widgets/juce_TreeView.h
+++ modules/juce_gui_basics/widgets/juce_TreeView.h
@@ -238,13 +238,14 @@ public:
 
     /** Returns a string to uniquely identify this item.
 
-        If you're planning on using the TreeView::getOpennessState() method, then
-        these strings will be used to identify which nodes are open. The string
-        should be unique amongst the item's sibling items, but it's ok for there
-        to be duplicates at other levels of the tree.
+        If you're planning on using the TreeView::getOpennessState() or
+        TreeView::getItemIdentifierString() methods, then these strings will be used
+        to identify which nodes are open. The string should be unique amongst the item's
+        sibling items, but it's ok for there to be duplicates at other levels of the tree.
 
-        If you're not going to store the state, then it's ok not to bother implementing
-        this method.
+        If you're not going to store the openness state nor search for TreeViewItems with
+        the TreeView::findItemFromIdentifierString() method, then it's ok not to bother
+        implementing this method.
     */
     virtual String getUniqueName() const;
 
