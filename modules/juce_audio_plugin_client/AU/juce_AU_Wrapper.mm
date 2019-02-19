c9432e58972875bbc00b244b1ef8d3a997afad75
ed <eddavies95@gmail.com>
2019-02-19 11:07:50 +0000

    Fixed an uninitialised variable in the AU wrapper

--- modules/juce_audio_plugin_client/AU/juce_AU_Wrapper.mm
+++ modules/juce_audio_plugin_client/AU/juce_AU_Wrapper.mm
@@ -130,7 +130,6 @@ public:
           MusicDeviceBase (component,
                            (UInt32) AudioUnitHelpers::getBusCount (juceFilter.get(), true),
                            (UInt32) AudioUnitHelpers::getBusCount (juceFilter.get(), false)),
-          isBypassed (false),
           mapper (*juceFilter)
     {
         inParameterChangedCallback = false;
@@ -1720,7 +1719,7 @@ private:
     //==============================================================================
     AudioUnitHelpers::CoreAudioBufferList audioBuffer;
     MidiBuffer midiEvents, incomingEvents;
-    bool prepared, isBypassed;
+    bool prepared = false, isBypassed = false;
 
     //==============================================================================
    #if JUCE_FORCE_USE_LEGACY_PARAM_IDS
