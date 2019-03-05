dcd606531aaecaf80c328a4d1570f18cf75ece8d
ed <eddavies95@gmail.com>
2019-03-05 12:04:40 +0000

    Fixed a documentation typo

--- modules/juce_gui_basics/buttons/juce_Button.h
+++ modules/juce_gui_basics/buttons/juce_Button.h
@@ -107,10 +107,12 @@ public:
     */
     bool getToggleState() const noexcept                        { return isOn.getValue(); }
 
-    /** Returns the Value object that represents the botton's toggle state.
+    /** Returns the Value object that represents the button's toggle state.
+
         You can use this Value object to connect the button's state to external values or setters,
         either by taking a copy of the Value, or by using Value::referTo() to make it point to
         your own Value object.
+
         @see getToggleState, Value
     */
     Value& getToggleStateValue() noexcept                       { return isOn; }
