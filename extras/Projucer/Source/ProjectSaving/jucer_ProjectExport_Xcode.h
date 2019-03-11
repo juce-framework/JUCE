78aa1af18b209b3c920e24f3c0d83107da6fc22e
Tom Poole <thomas.poole@roli.com>
2019-03-11 11:23:19 +0000

    MacOS:  Removed the "update to latest version" Xcode warning

--- extras/Projucer/Source/ProjectSaving/jucer_ProjectExport_Xcode.h
+++ extras/Projucer/Source/ProjectSaving/jucer_ProjectExport_Xcode.h
@@ -3399,7 +3399,7 @@ private:
     {
         String attributes;
 
-        attributes << "{ LastUpgradeCheck = 0930; "
+        attributes << "{ LastUpgradeCheck = 1010; "
                    << "ORGANIZATIONNAME = " << getProject().getCompanyNameString().quoted()
                    <<"; ";
 
