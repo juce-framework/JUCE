juce-framework/JUCE

commit 1694d2730224462324c55c5f877216a60cf24b14

Fixed VST3 after merge (PreSonus GR with newer juce parameters api).

modules/juce_audio_plugin_client/VST3/juce_VST3_Wrapper.cpp
@@ -133,7 +133,7 @@ public:
     //==============================================================================
     static const FUID iid;
     Array<Vst::ParamID> vstParamIDs;
-    Array<int> metersParamIDs;
+    Array<Vst::ParamID> metersParamIDs;
     Vst::ParamID bypassParamID = 0;
     bool bypassIsRegularParameter = false;
 
@@ -285,7 +285,7 @@ public:
         TEST_FOR_COMMON_BASE_AND_RETURN_IF_VALID (targetIID, IDependent, Vst::IEditController)
         TEST_FOR_COMMON_BASE_AND_RETURN_IF_VALID (targetIID, FUnknown, Vst::IEditController)
 
-        if (metersParamIDs.size() > 0)
+        if (audioProcessor->metersParamIDs.size() > 0)
         {
             TEST_FOR_AND_RETURN_IF_VALID (targetIID, Presonus::IGainReductionInfo)
         }
@@ -332,13 +332,15 @@ public:
     {
         double gainReduction = 1.0;
         bool hasGRMeter = false;
-        for (int i = 0; i < metersParamIDs.size(); ++i)
+
+        for (int i = 0; i < audioProcessor->metersParamIDs.size(); ++i)
         {
             // sum gain reduction meters only
-            auto category = getPluginInstance()->getParameterCategory(i);
+            const auto param = audioProcessor->getParamForVSTParamID(audioProcessor->metersParamIDs[i]);
+            const auto category = param->getCategory();
             if (category == AudioProcessorParameter::Category::compressorLimiterGainReductionMeter || category == AudioProcessorParameter::Category::expanderGateGainReductionMeter)
             {
-                gainReduction *= getPluginInstance()->getParameter(metersParamIDs[i]);
+                gainReduction *= param->getValue();
                 hasGRMeter = true;
             }
         }
@@ -770,9 +772,9 @@ private:
                                                         (vstParamID == audioProcessor->bypassParamID), forceLegacyParamIDs));
 
                     // is this a meter?
-                    if (((pluginInstance->getParameterCategory(i) & 0xffff0000) >> 16) == 2)
+                    if (((juceParam->getCategory() & 0xffff0000) >> 16) == 2)
                     {
-                        metersParamIDs.add (i);
+                        audioProcessor->metersParamIDs.add (vstParamID);
                     }
                 }
 

commit 013c5fabf133846bc89d87e87723f5e21cdf4d86

[VST3, Bugfix] Fix crash in Cubase 10 introduced in 1694d27.
Was triggered in case we tried to check the number of audioProcessor parameters before it was created.

modules/juce_audio_plugin_client/VST3/juce_VST3_Wrapper.cpp
@@ -289,15 +289,19 @@ public:
         TEST_FOR_COMMON_BASE_AND_RETURN_IF_VALID (targetIID, IDependent, Vst::IEditController)
         TEST_FOR_COMMON_BASE_AND_RETURN_IF_VALID (targetIID, FUnknown, Vst::IEditController)
 
-        if (audioProcessor->metersParamIDs.size() > 0)
-        {
-            TEST_FOR_AND_RETURN_IF_VALID (targetIID, Presonus::IGainReductionInfo)
-        }
-
         if (doUIDsMatch (targetIID, JuceAudioProcessor::iid))
         {
             audioProcessor->addRef();
             *obj = audioProcessor;
+            return kResultOk;
+        }
+
+        if (doUIDsMatch (targetIID, Presonus::IGainReductionInfo::iid)
+            && audioProcessor != nullptr
+            && ! audioProcessor->metersParamIDs.isEmpty())
+        {
+            addRef();
+            *obj = dynamic_cast<Presonus::IGainReductionInfo*> (this);
             return kResultOk;
         }
 
@@ -337,6 +341,7 @@ public:
         double gainReduction = 1.0;
         bool hasGRMeter = false;
 
+        jassert (audioProcessor != nullptr);
         for (int id : audioProcessor->metersParamIDs)
         {
             // sum gain reduction meters only