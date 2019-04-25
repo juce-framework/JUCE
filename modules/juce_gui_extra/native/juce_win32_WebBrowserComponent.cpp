a82f8c6fc7a5e86662362c501df24889b1f5b8c3
Martin Kosdy <martinkosdy@gmail.com>
2019-04-25 12:46:43 +0200

    Windows: Fixed double delete in WebBrowserComponent::goToURL() when invoked with POST data

--- modules/juce_gui_extra/native/juce_win32_WebBrowserComponent.cpp
+++ modules/juce_gui_extra/native/juce_win32_WebBrowserComponent.cpp
@@ -87,8 +87,6 @@ public:
     {
         if (browser != nullptr)
         {
-            LPSAFEARRAY sa = nullptr;
-
             VARIANT headerFlags, frame, postDataVar, headersVar;  // (_variant_t isn't available in all compilers)
             VariantInit (&headerFlags);
             VariantInit (&frame);
@@ -103,6 +101,7 @@ public:
 
             if (postData != nullptr && postData->getSize() > 0)
             {
+                LPSAFEARRAY sa = nullptr;
                 sa = SafeArrayCreateVector (VT_UI1, 0, (ULONG) postData->getSize());
 
                 if (sa != nullptr)
@@ -121,7 +120,12 @@ public:
                         V_VT (&postDataVar2) = VT_ARRAY | VT_UI1;
                         V_ARRAY (&postDataVar2) = sa;
 
+                        sa = nullptr;
                         postDataVar = postDataVar2;
+                    }
+                    else // SafeArrayAccessData failed
+                    {
+                        SafeArrayDestroy (sa);
                     }
                 }
             }
@@ -129,9 +133,6 @@ public:
             auto urlBSTR = SysAllocString ((const OLECHAR*) url.toWideCharPointer());
             browser->Navigate (urlBSTR, &headerFlags, &frame, &postDataVar, &headersVar);
             SysFreeString (urlBSTR);
-
-            if (sa != nullptr)
-                SafeArrayDestroy (sa);
 
             VariantClear (&headerFlags);
             VariantClear (&frame);
