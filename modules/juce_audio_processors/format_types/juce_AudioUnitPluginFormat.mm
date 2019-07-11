20d8f4d5b4457804e2c9689e39ba7ae88d4893ab
Mathieu Garcia <mg@mgdev.ch>
2019-07-11 12:14:02 +0200

    Some AUv3 plugins seems to register but ignore the kAudioUnitProperty_BypassEffect AU parameter. To workaround this, added a return statement so processing doesn't occur.

--- modules/juce_audio_processors/format_types/juce_AudioUnitPluginFormat.mm
+++ modules/juce_audio_processors/format_types/juce_AudioUnitPluginFormat.mm
@@ -1010,6 +1010,10 @@ public:
         if (auSupportsBypass)
         {
             updateBypass (processBlockBypassedCalled);
+            if (processBlockBypassedCalled)
+            {
+                return;
+            }
         }
         else if (processBlockBypassedCalled)
         {
