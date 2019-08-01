9ed1ba03accfb02edcf23a576130a35bd18d0348
吉井 大輝 <0214.taiki@gmail.com>
2019-08-01 20:38:31 +0900

    fix infinite loop bug

--- modules/juce_core/juce_core.h
+++ modules/juce_core/juce_core.h
@@ -214,6 +214,7 @@ namespace juce
 
     extern JUCE_API bool JUCE_CALLTYPE juce_isRunningUnderDebugger() noexcept;
     extern JUCE_API void JUCE_CALLTYPE logAssertion (const char* file, int line) noexcept;
+    extern JUCE_API void JUCE_CALLTYPE logAssertion (const wchar_t* file, int line) noexcept;
 }
 
 #include "memory/juce_Memory.h"
