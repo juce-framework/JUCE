bc25355b71745a291ee944c98b065e14c5f0df94
Tom Poole <thomas.poole@roli.com>
2019-08-07 16:06:47 +0100

    Fixed a documentation typo

--- modules/juce_graphics/colour/juce_FillType.h
+++ modules/juce_graphics/colour/juce_FillType.h
@@ -131,7 +131,7 @@ public:
     Colour colour;
 
     /** Returns the gradient that should be used for filling.
-        This will be zero if the object is some other type of fill.
+        This will be nullptr if the object is some other type of fill.
         If a gradient is active, the overall opacity with which it should be applied
         is indicated by the alpha channel of the colour variable.
     */
