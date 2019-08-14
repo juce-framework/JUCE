juce-framework/JUCE

commit 2b1d1d5bea2a77ac43bfe60854d7120c64f30357

Using braced C++11 initialization for member variables and initializing RulersView::document in the constructor member initializer.

modules/juce_audio_plugin_client/ARA/juce_ARADocumentController.cpp
@@ -358,7 +358,6 @@ void ARADocumentController::timerCallback()
 
 ARADocumentController::ArchiveReader::ArchiveReader (ARA::PlugIn::HostArchiveReader* reader)
 : archiveReader (reader), 
-  position (0), 
   size (reader->getArchiveSize())
 {}
 
@@ -385,8 +384,6 @@ bool ARADocumentController::ArchiveReader::isExhausted()
 }
 
 ARADocumentController::ArchiveWriter::ArchiveWriter (ARA::PlugIn::HostArchiveWriter* writer)
-: archiveWriter (writer), 
-  position (0)
 {}
 
 bool ARADocumentController::ArchiveWriter::write (const void* dataToWrite, size_t numberOfBytes)

examples/ARA/ARASampleProject/Source/ARASampleProjectAudioProcessor.h
@@ -68,7 +68,7 @@ private:
     // temp buffers to use for summing signals if rendering multiple regions
     std::unique_ptr<AudioBuffer<float>> tempBuffer;
 
-    bool lastProcessBlockSucceeded = true;
+    bool lastProcessBlockSucceeded { true };
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ARASampleProjectAudioProcessor)
 };

examples/ARA/ARASampleProject/Source/DocumentView.h
@@ -257,15 +257,15 @@ private:
     AudioFormatManager audioFormatManger;
 
     // Component View States
-    bool scrollFollowsPlayHead = true;
-    bool showOnlySelectedRegionSequences = true;
+    bool scrollFollowsPlayHead { true };
+    bool showOnlySelectedRegionSequences { true };
 
-    double pixelsPerSecond;
-    double maxPixelsPerSecond, minPixelsPerSecond;
+    double pixelsPerSecond { 1.0 };
+    double maxPixelsPerSecond { 192000.0 }, minPixelsPerSecond { 1.0 };
 
-    int trackHeight = 80;
+    int trackHeight { 80 };
 
-    bool regionSequenceViewsAreInvalid = true;
+    bool regionSequenceViewsAreInvalid { true };
     Range<double> timeRange;
 
     juce::AudioPlayHead::CurrentPositionInfo lastReportedPosition;

examples/ARA/ARASampleProject/Source/PlaybackRegionView.h
@@ -50,8 +50,8 @@ private:
 private:
     DocumentView& documentView;
     ARAPlaybackRegion* playbackRegion;
-    ARAPlaybackRegionReader* playbackRegionReader = nullptr;  // careful: "weak" pointer, actual pointer is owned by our audioThumb
-    bool isSelected = false;
+    ARAPlaybackRegionReader* playbackRegionReader { nullptr };  // careful: "weak" pointer, actual pointer is owned by our audioThumb
+    bool isSelected { false };
 
     AudioThumbnailCache audioThumbCache;
     AudioThumbnail audioThumb;

examples/ARA/ARASampleProject/Source/RulersView.cpp
@@ -6,10 +6,9 @@
 //==============================================================================
 RulersView::RulersView (DocumentView& documentView)
     : documentView (documentView),
-      document (nullptr),
+      document (documentView.getARADocumentController()->getDocument<ARADocument>()),
       musicalContext (nullptr)
 {
-    document = documentView.getARADocumentController()->getDocument<ARADocument>();
     document->addListener (this);
     findMusicalContext();
     lastPaintedPosition.resetToDefault();

examples/ARA/ARASampleProject/Source/TrackHeaderView.h
@@ -30,7 +30,7 @@ private:
 private:
     ARAEditorView* editorView;
     ARARegionSequence* regionSequence;
-    bool isSelected = false;
+    bool isSelected { false };
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (TrackHeaderView)
 };

modules/juce_audio_plugin_client/ARA/juce_ARAAudioReaders.h
@@ -136,7 +136,7 @@ public:
     void willDestroyPlaybackRegion (ARAPlaybackRegion* playbackRegion) override;
 
     /** The starting point of the reader in playback samples */
-    int64 startInSamples = 0;
+    int64 startInSamples { 0 };
 
 protected:
     bool getCurrentPosition (CurrentPositionInfo& result) override;
@@ -144,7 +144,7 @@ protected:
 private:
     std::unique_ptr<AudioProcessor> audioProcessor;
     AudioProcessorARAExtension* audioProcessorAraExtension; // cache of dynamic_cast<AudioProcessorARAExtension*> (audioProcessor)
-    int64 renderPosition = 0;
+    int64 renderPosition { 0 };
     static MidiBuffer dummyMidiBuffer;
     ReadWriteLock lock;
 

modules/juce_audio_plugin_client/ARA/juce_ARADocumentController.h
@@ -184,7 +184,8 @@ protected:
 
     private:
         ARA::PlugIn::HostArchiveReader* archiveReader;
-        size_t position, size;
+        size_t position { 0 };
+        size_t size;
     };
 
     //==============================================================================
@@ -201,7 +202,7 @@ protected:
 
     private:
         ARA::PlugIn::HostArchiveWriter* archiveWriter;
-        size_t position;
+        size_t position { 0 };
     };
 
 private:

commit 75d313ba1b13bed86defdfc93ed09be2185f4e0f

Fixing a bug / crash I introduced in [2b1d1d5] by not assigning the archiveWriter pointer.

modules/juce_audio_plugin_client/ARA/juce_ARADocumentController.cpp
@@ -430,6 +430,7 @@ bool ARADocumentController::ArchiveReader::isExhausted()
 }
 
 ARADocumentController::ArchiveWriter::ArchiveWriter (ARA::PlugIn::HostArchiveWriter* writer)
+: archiveWriter (writer)
 {}
 
 bool ARADocumentController::ArchiveWriter::write (const void* dataToWrite, size_t numberOfBytes)