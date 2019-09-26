2d0eb9e0330bc0ee8c4f7f5219af20943b65c59f
Tom Poole <thomas.poole@roli.com>
2019-09-26 15:46:33 +0100

    Made the interpolation classes moveable

--- modules/juce_audio_basics/utilities/juce_LagrangeInterpolator.h
+++ modules/juce_audio_basics/utilities/juce_LagrangeInterpolator.h
@@ -42,6 +42,9 @@ public:
     LagrangeInterpolator() noexcept;
     ~LagrangeInterpolator() noexcept;
 
+    LagrangeInterpolator (LagrangeInterpolator&&) noexcept = default;
+    LagrangeInterpolator& operator= (LagrangeInterpolator&&) noexcept = default;
+
     /** Resets the state of the interpolator.
         Call this when there's a break in the continuity of the input data stream.
     */
