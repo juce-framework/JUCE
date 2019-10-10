juce-framework/JUCE

commit b857f965ce85aa682f3a3475341026b1b25a1fb2

Replaced select() calls with poll()

modules/juce_core/network/juce_Socket.cpp
@@ -36,6 +36,7 @@ namespace juce
  using juce_socklen_t       = int;
  using juce_recvsend_size_t = int;
  using SocketHandle         = SOCKET;
+ using pollfd               = WSAPOLLFD;
  static const SocketHandle invalidSocket = INVALID_SOCKET;
 #elif JUCE_ANDROID
  using juce_socklen_t       = socklen_t;
@@ -283,50 +284,35 @@ namespace SocketHelpers
         if (! lock.isLocked())
             return -1;
 
-        int h = handle.load();
+        auto h = handle.load();
 
-        struct timeval timeout;
-        struct timeval* timeoutp;
+        short eventsFlag = (forReading ? POLLIN : POLLOUT);
+        pollfd pfd { (SocketHandle) h, eventsFlag, 0 };
 
-        if (timeoutMsecs >= 0)
+        int result = 0;
+
+        for (;;)
         {
-            timeout.tv_sec = timeoutMsecs / 1000;
-            timeout.tv_usec = (timeoutMsecs % 1000) * 1000;
-            timeoutp = &timeout;
-        }
-        else
-        {
-            timeoutp = nullptr;
-        }
+           #if JUCE_WINDOWS
+            result = WSAPoll (&pfd, 1, timeoutMsecs);
+           #else
+            result = poll (&pfd, 1, timeoutMsecs);
+           #endif
 
-        fd_set rset, wset;
-        FD_ZERO (&rset);
-        FD_SET (h, &rset);
-        FD_ZERO (&wset);
-        FD_SET (h, &wset);
-
-        fd_set* const prset = forReading ? &rset : nullptr;
-        fd_set* const pwset = forReading ? nullptr : &wset;
-
-       #if JUCE_WINDOWS
-        if (select ((int) h + 1, prset, pwset, 0, timeoutp) < 0)
-            return -1;
-       #else
-        {
-            int result = 0;
-
-            for (;;)
+            if (result >= 0
+               #if JUCE_WINDOWS
+                || result == SOCKET_ERROR
+               #else
+                || errno != EINTR
+               #endif
+                )
             {
-                result = select (h + 1, prset, pwset, nullptr, timeoutp);
-
-                if (result >= 0 || errno != EINTR)
-                    break;
+                break;
             }
-
-            if (result < 0)
-                return -1;
         }
-       #endif
+
+        if (result < 0)
+            return -1;
 
         // we are closing
         if (handle.load() < 0)
@@ -341,7 +327,7 @@ namespace SocketHelpers
                 return -1;
         }
 
-        return FD_ISSET (h, forReading ? &rset : &wset) ? 1 : 0;
+        return (pfd.revents & eventsFlag) != 0;
     }
 
     static addrinfo* getAddressInfo (bool isDatagram, const String& hostName, int portNumber)

modules/juce_core/native/juce_BasicNativeHeaders.h
@@ -104,6 +104,7 @@
  #include <objc/runtime.h>
  #include <objc/objc.h>
  #include <objc/message.h>
+ #include <poll.h>
 
 //==============================================================================
 #elif JUCE_WINDOWS
@@ -241,6 +242,7 @@
  #include <sys/vfs.h>
  #include <sys/wait.h>
  #include <utime.h>
+ #include <poll.h>
 
 //==============================================================================
 #elif JUCE_BSD
@@ -269,6 +271,7 @@
  #include <sys/types.h>
  #include <sys/wait.h>
  #include <utime.h>
+ #include <poll.h>
 
 //==============================================================================
 #elif JUCE_ANDROID
@@ -290,6 +293,7 @@
  #include <fnmatch.h>
  #include <sys/wait.h>
  #include <android/api-level.h>
+ #include <poll.h>
 
  // If you are getting include errors here, then you to re-build the Projucer
  // and re-save your .jucer file.

modules/juce_core/native/juce_linux_Network.cpp
@@ -205,16 +205,10 @@ public:
                 bytesToRead = static_cast<int> (chunkEnd - position);
         }
 
-        fd_set readbits;
-        FD_ZERO (&readbits);
-        FD_SET (socketHandle, &readbits);
+        pollfd pfd { socketHandle, POLLIN, 0 };
 
-        struct timeval tv;
-        tv.tv_sec = jmax (1, timeOutMs / 1000);
-        tv.tv_usec = 0;
-
-        if (select (socketHandle + 1, &readbits, 0, 0, &tv) <= 0)
-            return 0;   // (timeout)
+        if (poll (&pfd, 1, timeOutMs) <= 0)
+            return 0; // (timeout)
 
         auto bytesRead = jmax (0, (int) recv (socketHandle, buffer, (size_t) bytesToRead, MSG_WAITALL));
 

modules/juce_core/native/juce_posix_NamedPipe.cpp
@@ -172,15 +172,8 @@ private:
 
     static void waitForInput (int handle, int timeoutMsecs) noexcept
     {
-        struct timeval timeout;
-        timeout.tv_sec = timeoutMsecs / 1000;
-        timeout.tv_usec = (timeoutMsecs % 1000) * 1000;
-
-        fd_set rset;
-        FD_ZERO (&rset);
-        FD_SET (handle, &rset);
-
-        select (handle + 1, &rset, nullptr, nullptr, &timeout);
+        pollfd pfd { handle, POLLIN, 0 };
+        poll (&pfd, 1, timeoutMsecs);
     }
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Pimpl)

modules/juce_events/native/juce_linux_Messaging.cpp
@@ -20,8 +20,6 @@
   ==============================================================================
 */
 
-#include <poll.h>
-
 namespace juce
 {
 

modules/juce_gui_extra/native/juce_linux_X11_WebBrowserComponent.cpp
@@ -457,6 +457,10 @@ public:
         }
 
         receiver.reset (new CommandReceiver (this, inChannel));
+
+        pfds.push_back ({ threadControl[0],  POLLIN, 0 });
+        pfds.push_back ({ receiver->getFd(), POLLIN, 0 });
+
         startThread();
 
         xembed.reset (new XEmbedComponent (windowHandle));
@@ -602,17 +606,10 @@ private:
 
             receiver->tryNextRead();
 
-            fd_set set;
-            FD_ZERO (&set);
-            FD_SET (threadControl[0],  &set);
-            FD_SET (receiver->getFd(), &set);
-
-            int max_fd = jmax (threadControl[0], receiver->getFd());
-
             int result = 0;
 
             while (result == 0 || (result < 0 && errno == EINTR))
-                result = select (max_fd + 1, &set, nullptr, nullptr, nullptr);
+                result = poll (&pfds.front(), static_cast<nfds_t> (pfds.size()), 0);
 
             if (result < 0)
                 break;
@@ -702,6 +699,7 @@ private:
     int threadControl[2];
     std::unique_ptr<XEmbedComponent> xembed;
     WaitableEvent threadBlocker;
+    std::vector<pollfd> pfds;
 };
 
 //==============================================================================

commit 46ba5087a8ca0506092455f807c81ab6549cc03c

Fix compiling juce_core/network/juce_Socket.cpp with MinGW for real
Compiling juce_core/network/juce_Socket.cpp with MinGW is broken since
b857f965c. A fix was attempted in 170ec47a5, but it was incomplete.

modules/juce_core/network/juce_Socket.cpp
@@ -339,10 +339,10 @@ namespace SocketHelpers
                 break;
             }
         }
-       #endif
 
         if (result < 0)
             return -1;
+       #endif
 
         // we are closing
         if (handle.load() < 0)