b824c07ae45d11e7a5ad9a7d3c1841a4b92b9337
ed <eddavies95@gmail.com>
2019-10-16 10:08:56 +0100

    Re-saved all projects

--- extras/AudioPluginHost/JuceLibraryCode/AppConfig.h
+++ extras/AudioPluginHost/JuceLibraryCode/AppConfig.h
@@ -49,7 +49,7 @@
 
 #define JUCE_USE_DARK_SPLASH_SCREEN 1
 
-#define JUCE_PROJUCER_VERSION 0x50404
+#define JUCE_PROJUCER_VERSION 0x50405
 
 //==============================================================================
 #define JUCE_MODULE_AVAILABLE_juce_audio_basics          1
