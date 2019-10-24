65be5bc9c53bfcd386615e85a02b68bf344e6cd8
ed <eddavies95@gmail.com>
2019-10-24 17:20:59 +0100

    macOS:  Removed an SDK version guard for setting transparent window colours in the native peer code

--- modules/juce_gui_basics/native/juce_mac_NSViewComponentPeer.mm
+++ modules/juce_gui_basics/native/juce_mac_NSViewComponentPeer.mm
@@ -112,13 +112,11 @@ public:
 
             [window setOpaque: component.isOpaque()];
 
-          #if defined (MAC_OS_X_VERSION_10_14) && (MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_14)
             if (! [window isOpaque])
                 [window setBackgroundColor: [NSColor clearColor]];
 
-            #if defined (MAC_OS_X_VERSION_10_9) && (MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_9)
-             [view setAppearance: [NSAppearance appearanceNamed: NSAppearanceNameAqua]];
-            #endif
+           #if defined (MAC_OS_X_VERSION_10_9) && (MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_9)
+            [view setAppearance: [NSAppearance appearanceNamed: NSAppearanceNameAqua]];
            #endif
 
             [window setHasShadow: ((windowStyleFlags & windowHasDropShadow) != 0)];
