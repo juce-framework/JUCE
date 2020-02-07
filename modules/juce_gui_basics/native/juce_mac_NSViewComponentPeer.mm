juce-framework/JUCE

commit 8ce79e20e74fa13da4ac6a75d243c3a95ea79634

macOS:  Fixed a bug causing hidden NSWindows to become unhidden when the app regains focus

modules/juce_gui_basics/native/juce_mac_NSViewComponentPeer.mm
@@ -1015,12 +1015,12 @@ public:
 
     bool canBecomeKeyWindow()
     {
-        return (getStyleFlags() & juce::ComponentPeer::windowIgnoresKeyPresses) == 0;
+        return component.isVisible() && (getStyleFlags() & juce::ComponentPeer::windowIgnoresKeyPresses) == 0;
     }
 
     bool canBecomeMainWindow()
     {
-        return dynamic_cast<ResizableWindow*> (&component) != nullptr;
+        return component.isVisible() && dynamic_cast<ResizableWindow*> (&component) != nullptr;
     }
 
     bool worksWhenModal() const
@@ -1881,9 +1881,15 @@ private:
     static BOOL becomeFirstResponder (id self, SEL)
     {
         if (auto* owner = getOwner (self))
-            owner->viewFocusGain();
+        {
+            if (owner->canBecomeKeyWindow())
+            {
+                owner->viewFocusGain();
+                return YES;
+            }
+        }
 
-        return YES;
+        return NO;
     }
 
     static BOOL resignFirstResponder (id self, SEL)
@@ -1999,7 +2005,17 @@ private:
         sendSuperclassMessage (self, @selector (becomeKeyWindow));
 
         if (auto* owner = getOwner (self))
-            owner->becomeKeyWindow();
+        {
+            if (owner->canBecomeKeyWindow())
+            {
+                owner->becomeKeyWindow();
+                return;
+            }
+
+            // this fixes a bug causing hidden windows to sometimes become visible when the app regains focus
+            if (! owner->getComponent().isVisible())
+                [(NSWindow*) self orderOut: nil];
+        }
     }
 
     static BOOL windowShouldClose (id self, SEL, id /*window*/)

commit e4d10979aeb359b056dbcd515e1d6c01b2a2f1ac

macOS:  Fixed an NSWindow focus bug introduced in 8ce79e2

modules/juce_gui_basics/native/juce_mac_NSViewComponentPeer.mm
@@ -1887,15 +1887,9 @@ private:
     static BOOL becomeFirstResponder (id self, SEL)
     {
         if (auto* owner = getOwner (self))
-        {
-            if (owner->canBecomeKeyWindow())
-            {
-                owner->viewFocusGain();
-                return YES;
-            }
-        }
+            owner->viewFocusGain();
 
-        return NO;
+        return YES;
     }
 
     static BOOL resignFirstResponder (id self, SEL)