f91daa046ec49954e1a7cfe0b254b36c8754a7fa
Tobias Ludwig <tobias.ludwig@inf.fu-berlin.de>
2020-04-19 21:58:52 +0200

    juce_MidiMessage : fixed const correctness : remove c-style const cast, added non-cast overload

--- modules/juce_audio_basics/midi/juce_MidiMessage.h
+++ modules/juce_audio_basics/midi/juce_MidiMessage.h
@@ -945,7 +945,8 @@ private:
    #endif
 
     inline bool isHeapAllocated() const noexcept  { return size > (int) sizeof (packedData); }
-    inline uint8* getData() const noexcept        { return isHeapAllocated() ? packedData.allocatedData : (uint8*) packedData.asBytes; }
+    inline const uint8* getData() const noexcept  { return isHeapAllocated() ? packedData.allocatedData : packedData.asBytes; }
+    inline uint8* getData() noexcept              { return isHeapAllocated() ? packedData.allocatedData : packedData.asBytes; }
     uint8* allocateSpace (int);
 };
 
