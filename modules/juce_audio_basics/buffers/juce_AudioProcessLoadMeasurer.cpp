2d16374b144e45e01de15cbcdb2396af17d986e1
Tom Poole <tom@juce.com>
2020-04-22 15:04:59 +0100

    Updated all license headers

--- modules/juce_audio_basics/buffers/juce_AudioProcessLoadMeasurer.cpp
+++ modules/juce_audio_basics/buffers/juce_AudioProcessLoadMeasurer.cpp
@@ -2,7 +2,7 @@
   ==============================================================================
 
    This file is part of the JUCE library.
-   Copyright (c) 2017 - ROLI Ltd.
+   Copyright (c) 2020 - Raw Material Software Limited
 
    JUCE is an open source library subject to commercial or open-source
    licensing.
--- modules/juce_video/native/juce_android_Video.h
+++ modules/juce_video/native/juce_android_Video.h
@@ -2,14 +2,14 @@
   ==============================================================================
 
    This file is part of the JUCE library.
-   Copyright (c) 2018 - ROLI Ltd.
+   Copyright (c) 2020 - Raw Material Software Limited
 
    JUCE is an open source library subject to commercial or open-source
    licensing.
 
    By using JUCE, you agree to the terms of both the JUCE 5 End-User License
    Agreement and JUCE 5 Privacy Policy (both updated and effective as of the
-   27th April 2018).
+   22nd April 2020).
 
    End User License Agreement: www.juce.com/juce-5-licence
    Privacy Policy: www.juce.com/juce-5-privacy-policy
