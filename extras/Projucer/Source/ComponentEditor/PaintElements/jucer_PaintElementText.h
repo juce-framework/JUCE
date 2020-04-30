f60ca1fb8d03e0c5c144d98802b070721f16a75b
ed <eddavies95@gmail.com>
2020-04-30 13:01:30 +0100

    Whitespace

--- extras/Projucer/Source/ComponentEditor/PaintElements/jucer_PaintElementText.h
+++ extras/Projucer/Source/ComponentEditor/PaintElements/jucer_PaintElementText.h
@@ -63,7 +63,7 @@ public:
 
         if (multipleSelected)
             return;
-        
+
         props.add (new TextProperty (this));
         props.add (new FontNameProperty (this));
         props.add (new FontStyleProperty (this));
@@ -92,7 +92,7 @@ public:
               << "    g.drawText (text, x, y, width, height,\n"
               << "                " << CodeHelpers::justificationToCode (justification) << ", true);\n"
               << "}\n\n";
-            
+
             paintMethodCode += r;
         }
     }
@@ -100,7 +100,7 @@ public:
     void applyCustomPaintSnippets (StringArray& snippets) override
     {
         customPaintCode.clear();
-        
+
         if (! snippets.isEmpty() && ! fillType.isInvisible())
         {
             customPaintCode = snippets[0];
@@ -382,7 +382,7 @@ private:
     String typefaceName;
     Justification justification;
     String customPaintCode;
-    
+
     Array <Justification> justificationTypes;
 
     //==============================================================================
