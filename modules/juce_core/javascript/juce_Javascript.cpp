ad60bfa5a9bdfe7afcd96e56c087c7b2aa3971e4
Ben Kuper <bkuperberg@gmail.com>
2020-05-11 15:13:34 +0200

    fix parseInt bug on 08 / 09

--- modules/juce_core/javascript/juce_Javascript.cpp
+++ modules/juce_core/javascript/juce_Javascript.cpp
@@ -1802,8 +1802,7 @@ struct JavascriptEngine::RootObject   : public DynamicObject
         {
             auto s = getString (a, 0).trim();
 
-            return s[0] == '0' ? (s[1] == 'x' ? s.substring(2).getHexValue64() : getOctalValue (s))
-                               : s.getLargeIntValue();
+            return s.startsWith("0x") ? s.substring(2).getHexValue64() : s.getLargeIntValue();
         }
     };
 
