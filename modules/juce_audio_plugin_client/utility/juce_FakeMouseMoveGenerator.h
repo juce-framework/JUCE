40ab91dea436d16ca49eb989e33201af55d6003e
ed <eddavies95@gmail.com>
2020-05-29 10:19:06 +0100

    macOS:  Fixed an issue in FakeMouseMoveGenerator when a global scale factor is applied

--- modules/juce_audio_plugin_client/utility/juce_FakeMouseMoveGenerator.h
+++ modules/juce_audio_plugin_client/utility/juce_FakeMouseMoveGenerator.h
@@ -67,8 +67,12 @@ public:
                         {
                             if (! peer->isFocused())
                             {
-                                peer->handleMouseEvent (MouseInputSource::InputSourceType::mouse, peer->globalToLocal (screenPos), mods,
-                                                        MouseInputSource::invalidPressure, MouseInputSource::invalidOrientation, Time::currentTimeMillis());
+                                peer->handleMouseEvent (MouseInputSource::InputSourceType::mouse,
+                                                        peer->globalToLocal (Desktop::getInstance().getMainMouseSource().getRawScreenPosition()),
+                                                        mods,
+                                                        MouseInputSource::invalidPressure,
+                                                        MouseInputSource::invalidOrientation,
+                                                        Time::currentTimeMillis());
                             }
                         }
 
@@ -80,8 +84,12 @@ public:
                 {
                     if (auto* peer = safeOldComponent->getPeer())
                     {
-                        peer->handleMouseEvent (MouseInputSource::InputSourceType::mouse, MouseInputSource::offscreenMousePos, mods,
-                                                MouseInputSource::invalidPressure, MouseInputSource::invalidOrientation, Time::currentTimeMillis());
+                        peer->handleMouseEvent (MouseInputSource::InputSourceType::mouse,
+                                                MouseInputSource::offscreenMousePos,
+                                                mods,
+                                                MouseInputSource::invalidPressure,
+                                                MouseInputSource::invalidOrientation,
+                                                Time::currentTimeMillis());
                     }
                 }
 
