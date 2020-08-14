b196855b37941b50b9052c73a3a1f154bfcf1500
ed <eddavies95@gmail.com>
2020-08-14 11:50:08 +0100

    Projucer:  Fixed some warnings in the latest Xcode

--- extras/Projucer/Source/ProjectSaving/jucer_ProjectExport_Xcode.h
+++ extras/Projucer/Source/ProjectSaving/jucer_ProjectExport_Xcode.h
@@ -2285,14 +2285,16 @@ private:
         {
             mo.setNewLineString ("\n");
 
-            mo << "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" << newLine
+            mo << "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"              << newLine
                << "<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">" << newLine
-               << "<plist version=\"1.0\">"                    << newLine
-               << "<dict>"                                     << newLine
-               << "\t" << "<key>BuildSystemType</key>"         << newLine
-               << "\t" << "<string>Original</string>"          << newLine
-               << "</dict>"                                    << newLine
-               << "</plist>"                                   << newLine;
+               << "<plist version=\"1.0\">"                                 << newLine
+               << "<dict>"                                                  << newLine
+               << "\t" << "<key>BuildSystemType</key>"                      << newLine
+               << "\t" << "<string>Original</string>"                       << newLine
+               << "\t" << "<key>DisableBuildSystemDeprecationWarning</key>" << newLine
+               << "\t" << "<true/>"                                         << newLine
+               << "</dict>"                                                 << newLine
+               << "</plist>"                                                << newLine;
         });
     }
 
@@ -3180,7 +3182,7 @@ private:
     {
         String attributes;
 
-        attributes << "{ LastUpgradeCheck = 1100; "
+        attributes << "{ LastUpgradeCheck = 1200; "
                    << "ORGANIZATIONNAME = " << getProject().getCompanyNameString().quoted()
                    <<"; ";
 
