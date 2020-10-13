juce-framework/JUCE

commit 333f98d2046c171d7d02cfdfb1be4e27c035a99c

Squashed some compiler warnings with -Wmissing-prototypes enabled

modules/juce_audio_plugin_client/juce_audio_plugin_client.h
@@ -127,4 +127,5 @@
 #endif
 
 #include "utility/juce_PluginHostType.h"
+#include "utility/juce_CreatePluginFilter.h"
 #include "VST/juce_VSTCallbackHandler.h"

examples/DemoRunner/Source/Demos/JUCEDemos.cpp
@@ -169,10 +169,6 @@ AudioDeviceManager& getSharedAudioDeviceManager (int numInputChannels, int numOu
 }
 
 //==============================================================================
-// need to split this into two files otherwise VS will fall over
-void registerDemos_One() noexcept;
-void registerDemos_Two() noexcept;
-
 void registerAllDemos() noexcept
 {
     registerDemos_One();

examples/DemoRunner/Source/Demos/JUCEDemos.h
@@ -57,6 +57,9 @@ struct JUCEDemos
     static File findExamplesDirectoryFromExecutable (File exec);
 };
 
+void registerDemos_One() noexcept;
+void registerDemos_Two() noexcept;
+
 //==============================================================================
 // used by child-process demo
 bool invokeChildProcessDemo (const String& commandLine);

extras/Build/juce_build_tools/utils/juce_Icons.cpp
@@ -78,7 +78,7 @@ namespace build_tools
         }
     } // namespace mac
 
-    void writeMacIcon (const Icons& icons, OutputStream& out)
+    static void writeMacIcon (const Icons& icons, OutputStream& out)
     {
         MemoryOutputStream data;
         auto smallest = std::numeric_limits<int>::max();
@@ -269,7 +269,7 @@ namespace build_tools
         }
     } // namespace win
 
-    void writeWinIcon (const Icons& icons, OutputStream& os)
+    static void writeWinIcon (const Icons& icons, OutputStream& os)
     {
         Array<Image> images;
         int sizes[] = { 16, 32, 48, 256 };

extras/Projucer/Source/Application/jucer_Application.cpp
@@ -1452,7 +1452,7 @@ void ProjucerApplication::initCommandManager()
     registerGUIEditorCommands();
 }
 
-void rescanModules (AvailableModulesList& list, const Array<File>& paths, bool async)
+static void rescanModules (AvailableModulesList& list, const Array<File>& paths, bool async)
 {
     if (async)
         list.scanPathsAsync (paths);
@@ -1556,13 +1556,13 @@ void ProjucerApplication::setEditorColourScheme (int index, bool saveSetting)
     getCommandManager().commandStatusChanged();
 }
 
-bool isEditorColourSchemeADefaultScheme (const StringArray& schemes, int editorColourSchemeIndex)
+static bool isEditorColourSchemeADefaultScheme (const StringArray& schemes, int editorColourSchemeIndex)
 {
     auto& schemeName = schemes[editorColourSchemeIndex];
     return (schemeName == "Default (Dark)" || schemeName == "Default (Light)");
 }
 
-int getEditorColourSchemeForGUIColourScheme (const StringArray& schemes, int guiColourSchemeIndex)
+static int getEditorColourSchemeForGUIColourScheme (const StringArray& schemes, int guiColourSchemeIndex)
 {
     auto defaultDarkEditorIndex  = schemes.indexOf ("Default (Dark)");
     auto defaultLightEditorIndex = schemes.indexOf ("Default (Light)");

extras/Projucer/Source/Application/jucer_AutoUpdater.cpp
@@ -499,7 +499,7 @@ private:
     std::function<void()> completionCallback;
 };
 
-void restartProcess (const File& targetFolder)
+static void restartProcess (const File& targetFolder)
 {
    #if JUCE_MAC || JUCE_LINUX
     #if JUCE_MAC

extras/Projucer/Source/ComponentEditor/UI/jucer_JucerDocumentEditor.cpp
@@ -1217,6 +1217,7 @@ Image JucerDocumentEditor::createComponentLayerSnapshot() const
 const int gridSnapMenuItemBase = 0x8723620;
 const int snapSizes[] = { 2, 3, 4, 5, 6, 8, 10, 12, 16, 20, 24, 32 };
 
+PopupMenu createGUIEditorMenu();
 PopupMenu createGUIEditorMenu()
 {
     PopupMenu menu;
@@ -1293,6 +1294,7 @@ PopupMenu createGUIEditorMenu()
 
         menu.addSubMenu ("Component Overlay", overlays, holder != nullptr);
     }
+
     return menu;
 }
 

extras/Projucer/Source/Utility/Helpers/jucer_MiscUtilities.cpp
@@ -366,7 +366,7 @@ bool isJUCEModulesFolder (const File& f)
 }
 
 //==============================================================================
-bool isDivider (const String& line)
+static bool isDivider (const String& line)
 {
     auto afterIndent = line.trim();
 
@@ -385,7 +385,7 @@ bool isDivider (const String& line)
     return false;
 }
 
-int getIndexOfCommentBlockStart (const StringArray& lines, int endIndex)
+static int getIndexOfCommentBlockStart (const StringArray& lines, int endIndex)
 {
     auto endLine = lines[endIndex];
 
@@ -431,7 +431,7 @@ int findBestLineToScrollToForClass (StringArray lines, const String& className, 
 }
 
 //==============================================================================
-var parseJUCEHeaderMetadata (const StringArray& lines)
+static var parseJUCEHeaderMetadata (const StringArray& lines)
 {
     auto* o = new DynamicObject();
     var result (o);
@@ -454,7 +454,7 @@ var parseJUCEHeaderMetadata (const StringArray& lines)
     return result;
 }
 
-String parseMetadataItem (const StringArray& lines, int& index)
+static String parseMetadataItem (const StringArray& lines, int& index)
 {
     String result = lines[index++];
 

commit 2bef52e857c9e8c5ec8a523718cc6d59bb95e2d5

Fix compilation of ARA example broken by 333f98d2046c
That commit added an include of juce_CreatePluginFilter.h before inclusion of ARA headers.

Tested that this now build with ARA SDK 1.9.13.001 as well as 1.9.14.002.

modules/juce_audio_plugin_client/juce_audio_plugin_client.h
@@ -126,10 +126,6 @@
  #define JUCE_STANDALONE_FILTER_WINDOW_USE_KIOSK_MODE (JUCE_IOS || JUCE_ANDROID)
 #endif
 
-#include "utility/juce_PluginHostType.h"
-#include "utility/juce_CreatePluginFilter.h"
-#include "VST/juce_VSTCallbackHandler.h"
-
 #if JucePlugin_Enable_ARA
  #include "ARA/juce_ARAModelObjects.h"
  #include "ARA/juce_ARADocumentController.h"
@@ -137,3 +133,7 @@
  #include "ARA/juce_ARAAudioReaders.h"
  #include "ARA/juce_ARAPlugInInstanceRoles.h"
 #endif
+
+#include "utility/juce_PluginHostType.h"
+#include "utility/juce_CreatePluginFilter.h"
+#include "VST/juce_VSTCallbackHandler.h"