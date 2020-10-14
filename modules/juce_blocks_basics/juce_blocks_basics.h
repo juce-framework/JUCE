c35bb61c8de4fafcd6083d03597b0965c5231bf7
Tom Poole <tom@juce.com>
2020-10-14 11:40:30 +0100

    Bump version number to 6.0.4

--- modules/juce_blocks_basics/juce_blocks_basics.h
+++ modules/juce_blocks_basics/juce_blocks_basics.h
@@ -32,7 +32,7 @@
 
   ID:                 juce_blocks_basics
   vendor:             juce
-  version:            6.0.3
+  version:            6.0.4
   name:               Provides low-level control over ROLI BLOCKS devices
   description:        JUCE wrapper for low-level control over ROLI BLOCKS devices.
   website:            http://developer.roli.com
