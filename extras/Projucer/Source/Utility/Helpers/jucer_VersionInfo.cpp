d69bee594cba8b48e0016003ecbf7c53012894b2
jerryuhoo <yyflongjuan@126.com>
2021-01-16 18:43:08 +0800

    Remove redundant condition

--- extras/Projucer/Source/Utility/Helpers/jucer_VersionInfo.cpp
+++ extras/Projucer/Source/Utility/Helpers/jucer_VersionInfo.cpp
@@ -60,7 +60,7 @@ bool VersionInfo::isNewerVersionThanCurrent()
     auto currentTokens = StringArray::fromTokens (ProjectInfo::versionString, ".", {});
     auto thisTokens    = StringArray::fromTokens (versionString, ".", {});
 
-    jassert (thisTokens.size() == 3 && thisTokens.size() == 3);
+    jassert (thisTokens.size() == 3);
 
     if (currentTokens[0].getIntValue() == thisTokens[0].getIntValue())
     {
