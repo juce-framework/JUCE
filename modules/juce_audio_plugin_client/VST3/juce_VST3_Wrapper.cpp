9cc7e40c8604b6b5965dcb8b6f06d9f11ea40e59
Tom Poole <tom@juce.com>
2021-01-22 16:53:54 +0000

    Updated some documentation

--- modules/juce_audio_plugin_client/VST3/juce_VST3_Wrapper.cpp
+++ modules/juce_audio_plugin_client/VST3/juce_VST3_Wrapper.cpp
@@ -241,13 +241,13 @@ public:
         if (group == nullptr || group->getParent() == nullptr)
             return Vst::kRootUnitId;
 
-        // From the VST3 docs:
+        // From the VST3 docs (also applicable to unit IDs!):
         // Up to 2^31 parameters can be exported with id range [0, 2147483648]
         // (the range [2147483649, 429496729] is reserved for host application).
         auto unitID = group->getID().hashCode() & 0x7fffffff;
 
         // If you hit this assertion then your group ID is hashing to a value
-        // reserved by the VST3 SDK. Use a different group ID.
+        // reserved by the VST3 SDK. Please use a different group ID.
         jassert (unitID != Vst::kRootUnitId);
 
         return unitID;
