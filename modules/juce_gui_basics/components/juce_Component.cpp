891d86fe6b769cbdc7827981b87a23a4eb87fbf4
ed <eddavies95@gmail.com>
2021-01-27 16:34:43 +0000

    Don't trigger a fake mouse move from Component::sendFakeMouseMove() if the component passes through all mouse events

--- modules/juce_gui_basics/components/juce_Component.cpp
+++ modules/juce_gui_basics/components/juce_Component.cpp
@@ -2595,6 +2595,9 @@ void Component::internalMagnifyGesture (MouseInputSource source, Point<float> re
 
 void Component::sendFakeMouseMove() const
 {
+    if (flags.ignoresMouseClicksFlag && ! flags.allowChildMouseClicksFlag)
+        return;
+
     auto mainMouse = Desktop::getInstance().getMainMouseSource();
 
     if (! mainMouse.isDragging())
