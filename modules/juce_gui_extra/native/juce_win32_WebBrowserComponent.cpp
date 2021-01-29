juce-framework/JUCE

commit 48fcea0c4438022ef126373d55263293a885ec3f

Windows:  Removed WinRTWrapper dependency from WebView2 implementation

modules/juce_gui_extra/native/juce_win32_WebBrowserComponent.cpp
@@ -335,9 +335,6 @@ public:
          : ComponentMovementWatcher (&o),
            owner (o)
     {
-        if (! WinRTWrapper::getInstance()->isInitialised())
-            throw std::runtime_error ("Failed to initialise the WinRT wrapper");
-
         if (! createWebViewEnvironment (dllLocation, userDataFolder))
             throw std::runtime_error ("Failed to create the CoreWebView2Environemnt");
 
@@ -531,7 +528,7 @@ private:
                     if (urlRequest.url.isEmpty())
                         return S_OK;
 
-                    WinRTWrapper::ComPtr<ICoreWebView2WebResourceRequest> request;
+                    ComSmartPtr<ICoreWebView2WebResourceRequest> request;
                     args->get_Request (request.resetAndGetPointerAddress());
 
                     auto uriString = getUriStringFromArgs (request.get());
@@ -545,14 +542,14 @@ private:
                         {
                             method = "POST";
 
-                            WinRTWrapper::ComPtr<IStream> content (SHCreateMemStream ((BYTE*) urlRequest.postData.getData(),
+                            ComSmartPtr<IStream> content (SHCreateMemStream ((BYTE*) urlRequest.postData.getData(),
                                                                                       (UINT) urlRequest.postData.getSize()));
                             request->put_Content (content.get());
                         }
 
                         if (! urlRequest.headers.isEmpty())
                         {
-                            WinRTWrapper::ComPtr<ICoreWebView2HttpRequestHeaders> headers;
+                            ComSmartPtr<ICoreWebView2HttpRequestHeaders> headers;
                             request->get_Headers (headers.resetAndGetPointerAddress());
 
                             for (auto& header : urlRequest.headers)
@@ -711,9 +708,9 @@ private:
 
     HMODULE webView2LoaderHandle = nullptr;
 
-    WinRTWrapper::ComPtr<ICoreWebView2Environment> webViewEnvironment;
-    WinRTWrapper::ComPtr<ICoreWebView2Controller> webViewController;
-    WinRTWrapper::ComPtr<ICoreWebView2> webView;
+    ComSmartPtr<ICoreWebView2Environment> webViewEnvironment;
+    ComSmartPtr<ICoreWebView2Controller> webViewController;
+    ComSmartPtr<ICoreWebView2> webView;
 
     EventRegistrationToken navigationStartingToken   { 0 },
                            newWindowRequestedToken   { 0 },

modules/juce_events/juce_events.cpp
@@ -35,7 +35,7 @@
 #define JUCE_CORE_INCLUDE_COM_SMART_PTR 1
 #define JUCE_EVENTS_INCLUDE_WIN32_MESSAGE_WINDOW 1
 
-#if JUCE_USE_WINRT_MIDI || JUCE_USE_WIN_WEBVIEW2
+#if JUCE_USE_WINRT_MIDI
  #define JUCE_EVENTS_INCLUDE_WINRT_WRAPPER 1
 #endif
 

modules/juce_gui_extra/juce_gui_extra.cpp
@@ -40,10 +40,6 @@
 #define JUCE_GRAPHICS_INCLUDE_COREGRAPHICS_HELPERS 1
 #define JUCE_GUI_BASICS_INCLUDE_XHEADERS 1
 
-#if JUCE_USE_WIN_WEBVIEW2
- #define JUCE_EVENTS_INCLUDE_WINRT_WRAPPER 1
-#endif
-
 #ifndef JUCE_PUSH_NOTIFICATIONS
  #define JUCE_PUSH_NOTIFICATIONS 0
 #endif

commit 6cc67f5ac59c8e0ed87d963664cc4f3378825caf

Windows:  Fixed some build errors in the WebView2 code introduced in 48fcea0c

modules/juce_gui_extra/native/juce_win32_WebBrowserComponent.cpp
@@ -531,7 +531,7 @@ private:
                     ComSmartPtr<ICoreWebView2WebResourceRequest> request;
                     args->get_Request (request.resetAndGetPointerAddress());
 
-                    auto uriString = getUriStringFromArgs (request.get());
+                    auto uriString = getUriStringFromArgs<ICoreWebView2WebResourceRequest> (request);
 
                     if (uriString == urlRequest.url
                         || (uriString.endsWith ("/") && uriString.upToLastOccurrenceOf ("/", false, false) == urlRequest.url))
@@ -544,7 +544,7 @@ private:
 
                             ComSmartPtr<IStream> content (SHCreateMemStream ((BYTE*) urlRequest.postData.getData(),
                                                                                       (UINT) urlRequest.postData.getSize()));
-                            request->put_Content (content.get());
+                            request->put_Content (content);
                         }
 
                         if (! urlRequest.headers.isEmpty())
@@ -672,7 +672,7 @@ private:
 
     void closeWebView()
     {
-        if (webViewController.get() != nullptr)
+        if (webViewController != nullptr)
         {
             webViewController->Close();
             webViewController = nullptr;