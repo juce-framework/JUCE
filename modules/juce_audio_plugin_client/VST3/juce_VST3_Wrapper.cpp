213d7973ce9c53de1564a8033ff4401f2516fb3a
sgretscher <41306803+sgretscher@users.noreply.github.com>
2021-03-19 12:18:13 +0100

    Enforcing that processing latency is 0 if ARA is enabled.

--- modules/juce_audio_plugin_client/VST3/juce_VST3_Wrapper.cpp
+++ modules/juce_audio_plugin_client/VST3/juce_VST3_Wrapper.cpp
@@ -732,8 +732,7 @@ public:
     Steinberg::TBool PLUGIN_API isViewEmbeddingSupported() override
     {
         if (auto* pluginInstance = getPluginInstance())
-            if (auto* araExtension = dynamic_cast<AudioProcessorARAExtension*> (pluginInstance))
-                return (Steinberg::TBool) araExtension->isEditorView();
+            return (Steinberg::TBool) dynamic_cast<AudioProcessorARAExtension*> (pluginInstance)->isEditorView();
         return (Steinberg::TBool) false;
     }
 
@@ -1018,6 +1017,9 @@ public:
             }
 
             auto latencySamples = pluginInstance->getLatencySamples();
+           #if JucePlugin_Enable_ARA
+            jassert (latencySamples == 0 || ! dynamic_cast<AudioProcessorARAExtension*> (pluginInstance)->isBoundToARA());
+           #endif
 
             if (latencySamples != lastLatencySamples)
             {
