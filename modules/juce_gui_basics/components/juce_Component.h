9e647365190bae1425723b2d1de97b15ae066034
ed <eddavies95@gmail.com>
2021-03-26 16:24:35 +0000

    Docs:  Updated the documentation for Component::modifierKeysChanged()

--- modules/juce_gui_basics/components/juce_Component.h
+++ modules/juce_gui_basics/components/juce_Component.h
@@ -1743,9 +1743,12 @@ public:
     /** Called when a modifier key is pressed or released.
 
         Whenever the shift, control, alt or command keys are pressed or released,
-        this method will be called on the component that currently has the keyboard focus.
-        Remember that a component will only be given the focus if its setWantsKeyboardFocus()
-        method has been used to enable this.
+        this method will be called.
+
+        The component that is currently under the main mouse pointer will be tried first and,
+        if there is no component currently under the pointer, the component that currently
+        has the keyboard focus will have this method called. Remember that a component will
+        only be given the focus if its setWantsKeyboardFocus() method has been used to enable this.
 
         The default implementation of this method actually calls its parent's modifierKeysChanged
         method, so that focused components which aren't interested in this will give their
