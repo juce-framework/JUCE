36ca0e0fc1ac42cb6457b5419585073613beb58f
reuk <reuk@users.noreply.github.com>
2021-06-16 15:00:07 +0100

    DSP:  Avoid forward declaration of IIR::Coefficients

--- modules/juce_dsp/processors/juce_IIRFilter.h
+++ modules/juce_dsp/processors/juce_IIRFilter.h
@@ -33,124 +33,6 @@ namespace dsp
 */
 namespace IIR
 {
-    template <typename NumericType>
-    struct Coefficients;
-
-    /**
-        A processing class that can perform IIR filtering on an audio signal, using
-        the Transposed Direct Form II digital structure.
-
-        If you need a lowpass, bandpass or highpass filter with fast modulation of
-        its cutoff frequency, you might use the class StateVariableFilter instead,
-        which is designed to prevent artefacts at parameter changes, instead of the
-        class Filter.
-
-        @see Filter::Coefficients, FilterAudioSource, StateVariableFilter
-
-        @tags{DSP}
-    */
-    template <typename SampleType>
-    class Filter
-    {
-    public:
-        /** The NumericType is the underlying primitive type used by the SampleType (which
-            could be either a primitive or vector)
-        */
-        using NumericType = typename SampleTypeHelpers::ElementType<SampleType>::Type;
-
-        /** A typedef for a ref-counted pointer to the coefficients object */
-        using CoefficientsPtr = typename Coefficients<NumericType>::Ptr;
-
-        //==============================================================================
-        /** Creates a filter.
-
-            Initially the filter is inactive, so will have no effect on samples that
-            you process with it. You can modify the coefficients member to turn it into
-            the type of filter needed.
-        */
-        Filter();
-
-        /** Creates a filter with a given set of coefficients. */
-        Filter (CoefficientsPtr coefficientsToUse);
-
-        Filter (const Filter&) = default;
-        Filter (Filter&&) = default;
-        Filter& operator= (const Filter&) = default;
-        Filter& operator= (Filter&&) = default;
-
-        //==============================================================================
-        /** The coefficients of the IIR filter. It's up to the caller to ensure that
-            these coefficients are modified in a thread-safe way.
-
-            If you change the order of the coefficients then you must call reset after
-            modifying them.
-        */
-        CoefficientsPtr coefficients;
-
-        //==============================================================================
-        /** Resets the filter's processing pipeline, ready to start a new stream of data.
-
-            Note that this clears the processing state, but the type of filter and
-            its coefficients aren't changed.
-        */
-        void reset()    { reset (SampleType {0}); }
-
-        /** Resets the filter's processing pipeline to a specific value.
-            @see reset
-        */
-        void reset (SampleType resetToValue);
-
-        //==============================================================================
-        /** Called before processing starts. */
-        void prepare (const ProcessSpec&) noexcept;
-
-        /** Processes a block of samples */
-        template <typename ProcessContext>
-        void process (const ProcessContext& context) noexcept
-        {
-            if (context.isBypassed)
-                processInternal<ProcessContext, true> (context);
-            else
-                processInternal<ProcessContext, false> (context);
-
-           #if JUCE_DSP_ENABLE_SNAP_TO_ZERO
-            snapToZero();
-           #endif
-        }
-
-        /** Processes a single sample, without any locking.
-
-            Use this if you need processing of a single value.
-
-            Moreover, you might need the function snapToZero after a few calls to avoid
-            potential denormalisation issues.
-        */
-        SampleType JUCE_VECTOR_CALLTYPE processSample (SampleType sample) noexcept;
-
-        /** Ensure that the state variables are rounded to zero if the state
-            variables are denormals. This is only needed if you are doing
-            sample by sample processing.
-        */
-        void snapToZero() noexcept;
-
-    private:
-        //==============================================================================
-        void check();
-
-        /** Processes a block of samples */
-        template <typename ProcessContext, bool isBypassed>
-        void processInternal (const ProcessContext& context) noexcept;
-
-        //==============================================================================
-        HeapBlock<SampleType> memory;
-        SampleType* state = nullptr;
-        size_t order = 0;
-
-        JUCE_LEAK_DETECTOR (Filter)
-    };
-
-
-    //==============================================================================
     /** A set of coefficients for use in an Filter object.
         @see IIR::Filter
 
@@ -302,6 +184,119 @@ namespace IIR
         static constexpr NumericType inverseRootTwo = static_cast<NumericType> (0.70710678118654752440L);
     };
 
+    //==============================================================================
+    /**
+        A processing class that can perform IIR filtering on an audio signal, using
+        the Transposed Direct Form II digital structure.
+
+        If you need a lowpass, bandpass or highpass filter with fast modulation of
+        its cutoff frequency, you might use the class StateVariableFilter instead,
+        which is designed to prevent artefacts at parameter changes, instead of the
+        class Filter.
+
+        @see Filter::Coefficients, FilterAudioSource, StateVariableFilter
+
+        @tags{DSP}
+    */
+    template <typename SampleType>
+    class Filter
+    {
+    public:
+        /** The NumericType is the underlying primitive type used by the SampleType (which
+            could be either a primitive or vector)
+        */
+        using NumericType = typename SampleTypeHelpers::ElementType<SampleType>::Type;
+
+        /** A typedef for a ref-counted pointer to the coefficients object */
+        using CoefficientsPtr = typename Coefficients<NumericType>::Ptr;
+
+        //==============================================================================
+        /** Creates a filter.
+
+            Initially the filter is inactive, so will have no effect on samples that
+            you process with it. You can modify the coefficients member to turn it into
+            the type of filter needed.
+        */
+        Filter();
+
+        /** Creates a filter with a given set of coefficients. */
+        Filter (CoefficientsPtr coefficientsToUse);
+
+        Filter (const Filter&) = default;
+        Filter (Filter&&) = default;
+        Filter& operator= (const Filter&) = default;
+        Filter& operator= (Filter&&) = default;
+
+        //==============================================================================
+        /** The coefficients of the IIR filter. It's up to the caller to ensure that
+            these coefficients are modified in a thread-safe way.
+
+            If you change the order of the coefficients then you must call reset after
+            modifying them.
+        */
+        CoefficientsPtr coefficients;
+
+        //==============================================================================
+        /** Resets the filter's processing pipeline, ready to start a new stream of data.
+
+            Note that this clears the processing state, but the type of filter and
+            its coefficients aren't changed.
+        */
+        void reset()    { reset (SampleType {0}); }
+
+        /** Resets the filter's processing pipeline to a specific value.
+            @see reset
+        */
+        void reset (SampleType resetToValue);
+
+        //==============================================================================
+        /** Called before processing starts. */
+        void prepare (const ProcessSpec&) noexcept;
+
+        /** Processes a block of samples */
+        template <typename ProcessContext>
+        void process (const ProcessContext& context) noexcept
+        {
+            if (context.isBypassed)
+                processInternal<ProcessContext, true> (context);
+            else
+                processInternal<ProcessContext, false> (context);
+
+           #if JUCE_DSP_ENABLE_SNAP_TO_ZERO
+            snapToZero();
+           #endif
+        }
+
+        /** Processes a single sample, without any locking.
+
+            Use this if you need processing of a single value.
+
+            Moreover, you might need the function snapToZero after a few calls to avoid
+            potential denormalisation issues.
+        */
+        SampleType JUCE_VECTOR_CALLTYPE processSample (SampleType sample) noexcept;
+
+        /** Ensure that the state variables are rounded to zero if the state
+            variables are denormals. This is only needed if you are doing
+            sample by sample processing.
+        */
+        void snapToZero() noexcept;
+
+    private:
+        //==============================================================================
+        void check();
+
+        /** Processes a block of samples */
+        template <typename ProcessContext, bool isBypassed>
+        void processInternal (const ProcessContext& context) noexcept;
+
+        //==============================================================================
+        HeapBlock<SampleType> memory;
+        SampleType* state = nullptr;
+        size_t order = 0;
+
+        JUCE_LEAK_DETECTOR (Filter)
+    };
 } // namespace IIR
 } // namespace dsp
 } // namespace juce
