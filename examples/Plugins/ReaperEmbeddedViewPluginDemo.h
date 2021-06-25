d3b279d0e895ec5da7c4279add8b08159eee8992
Tom Poole <tom@juce.com>
2021-06-25 09:07:33 +0100

    Silence some compiler warnings in third party headers

--- examples/Plugins/ReaperEmbeddedViewPluginDemo.h
+++ examples/Plugins/ReaperEmbeddedViewPluginDemo.h
@@ -61,14 +61,20 @@
 
 #pragma once
 
+JUCE_BEGIN_IGNORE_WARNINGS_GCC_LIKE ("-Wshadow-field-in-constructor",
+                                     "-Wnon-virtual-dtor")
+
 #include <pluginterfaces/base/ftypes.h>
 #include <pluginterfaces/base/funknown.h>
 #include <pluginterfaces/vst/ivsthostapplication.h>
+
+JUCE_END_IGNORE_WARNINGS_GCC_LIKE
 
 namespace reaper
 {
     JUCE_BEGIN_IGNORE_WARNINGS_GCC_LIKE ("-Wzero-as-null-pointer-constant",
                                          "-Wunused-parameter")
+    JUCE_BEGIN_IGNORE_WARNINGS_MSVC (4100)
 
     using namespace Steinberg;
     using INT_PTR = pointer_sized_int;
@@ -85,6 +91,7 @@ namespace reaper
     DEF_CLASS_IID (IReaperHostApplication)
     DEF_CLASS_IID (IReaperUIEmbedInterface)
 
+    JUCE_END_IGNORE_WARNINGS_MSVC
     JUCE_END_IGNORE_WARNINGS_GCC_LIKE
 }
 
