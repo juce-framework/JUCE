e467473157f6051b8aac19da3626e189de53eb4b
ed <eddavies95@gmail.com>
2021-07-15 12:57:03 +0100

    Standalone:  Fixed a crash due to a dangling input muted value listener

--- modules/juce_audio_plugin_client/Standalone/juce_StandaloneFilterWindow.h
+++ modules/juce_audio_plugin_client/Standalone/juce_StandaloneFilterWindow.h
@@ -857,7 +857,7 @@ private:
               editor (owner.getAudioProcessor()->hasEditor() ? owner.getAudioProcessor()->createEditorIfNeeded()
                                                              : new GenericAudioProcessorEditor (*owner.getAudioProcessor()))
         {
-            Value& inputMutedValue = owner.pluginHolder->getMuteInputValue();
+            inputMutedValue.referTo (owner.pluginHolder->getMuteInputValue());
 
             if (editor != nullptr)
             {
@@ -1014,6 +1014,7 @@ private:
         StandaloneFilterWindow& owner;
         NotificationArea notification;
         std::unique_ptr<AudioProcessorEditor> editor;
+        Value inputMutedValue;
         bool shouldShowNotification = false;
 
         JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainContentComponent)
