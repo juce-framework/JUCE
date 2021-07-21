a2f7aaab2fc7e94b867ca9270e030942a3f66226
ed <eddavies95@gmail.com>
2021-07-21 17:12:45 +0100

    Accessibility:  Don't create AccessibilityHandler for Components without a native window handle

--- modules/juce_gui_basics/components/juce_Component.cpp
+++ modules/juce_gui_basics/components/juce_Component.cpp
@@ -3164,7 +3164,7 @@ void Component::invalidateAccessibilityHandler()
 
 AccessibilityHandler* Component::getAccessibilityHandler()
 {
-    if (flags.accessibilityIgnoredFlag)
+    if (flags.accessibilityIgnoredFlag || getWindowHandle() == nullptr)
         return nullptr;
 
     if (accessibilityHandler == nullptr
