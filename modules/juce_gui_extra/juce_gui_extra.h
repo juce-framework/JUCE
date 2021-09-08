aefc582b71038c48d6bbbf9505bd855ee6c9ffc2
reuk <reuk@users.noreply.github.com>
2021-09-08 16:53:30 +0100

    ScopedDPIAwarenessDisabler:  Move to juce_gui_basics

--- modules/juce_gui_extra/juce_gui_extra.h
+++ modules/juce_gui_extra/juce_gui_extra.h
@@ -106,7 +106,6 @@
 #include "embedding/juce_NSViewComponent.h"
 #include "embedding/juce_UIViewComponent.h"
 #include "embedding/juce_XEmbedComponent.h"
-#include "embedding/juce_ScopedDPIAwarenessDisabler.h"
 #include "embedding/juce_HWNDComponent.h"
 #include "misc/juce_AppleRemote.h"
 #include "misc/juce_BubbleMessageComponent.h"
