juce-framework/JUCE

commit 5f7ad995af67cb9bbf3b5ce1cb4d591559f9c116

Projucer:  Escape special characters in MSVC project files

extras/Projucer/Source/ProjectSaving/jucer_ProjectExport_MSVC.h
@@ -25,6 +25,14 @@
 
 #pragma once
 
+inline String msBuildEscape (String str)
+{
+    // see https://docs.microsoft.com/en-us/visualstudio/msbuild/msbuild-special-characters?view=vs-2019
+    for (const auto& special : { "%", "$", "@", "'", ";", "?", "\""})
+        str = str.replace (special, "%" + String::toHexString (*special));
+
+    return str;
+}
 
 //==============================================================================
 class MSVCProjectExporterBase   : public ProjectExporter
@@ -502,7 +510,7 @@ public:
                     {
                         auto* targetName = props->createNewChildElement ("TargetName");
                         setConditionAttribute (*targetName, config);
-                        targetName->addTextElement (config.getOutputFilename ("", false, type == UnityPlugIn));
+                        targetName->addTextElement (msBuildEscape (config.getOutputFilename ("", false, type == UnityPlugIn)));
                     }
 
                     {
@@ -597,7 +605,7 @@ public:
 
                 auto externalLibraries = getExternalLibraries (config, getOwner().getExternalLibrariesString());
                 auto additionalDependencies = type != SharedCodeTarget && externalLibraries.isNotEmpty()
-                                                        ? getOwner().replacePreprocessorTokens (config, externalLibraries).trim() + ";%(AdditionalDependencies)"
+                                                        ? msBuildEscape (getOwner().replacePreprocessorTokens (config, externalLibraries).trim()) + ";%(AdditionalDependencies)"
                                                         : String();
 
                 auto librarySearchPaths = config.getLibrarySearchPaths();

commit 0feda541e7503f435b8410be4666a03dd22f6a0f

Projucer:  Escape external library paths correctly
5f7ad995af67cb9bbf3b5ce1cb4d591559f9c116 introduced an issue where the
string of external libraries would be escaped in one go. This is
incorrect, because only the individual paths should be escaped. The
semicolons separating each path should not be escaped.

extras/Projucer/Source/ProjectSaving/jucer_ProjectExport_MSVC.h
@@ -606,9 +606,9 @@ public:
                                                                                                     : "NDEBUG;%(PreprocessorDefinitions)");
                 }
 
-                auto externalLibraries = getExternalLibraries (config, getOwner().getExternalLibrariesString());
-                auto additionalDependencies = type != SharedCodeTarget && externalLibraries.isNotEmpty()
-                                                        ? msBuildEscape (getOwner().replacePreprocessorTokens (config, externalLibraries).trim()) + ";%(AdditionalDependencies)"
+                auto externalLibraries = getExternalLibraries (config, getOwner().getExternalLibrariesStringArray());
+                auto additionalDependencies = type != SharedCodeTarget && ! externalLibraries.isEmpty()
+                                                        ? externalLibraries.joinIntoString (";") + ";%(AdditionalDependencies)"
                                                         : String();
 
                 auto librarySearchPaths = config.getLibrarySearchPaths();
@@ -1332,22 +1332,25 @@ public:
             return librarySearchPaths;
         }
 
-        String getExternalLibraries (const MSVCBuildConfiguration& config, const String& otherLibs) const
+        StringArray getExternalLibraries (const MSVCBuildConfiguration& config, const StringArray& otherLibs) const
         {
-            StringArray libraries;
+            const auto sharedCodeLib = [&]() -> StringArray
+            {
+                if (type != SharedCodeTarget)
+                    if (auto* shared = getOwner().getSharedCodeTarget())
+                        return { shared->getBinaryNameWithSuffix (config, false) };
 
-            if (otherLibs.isNotEmpty())
-                libraries.add (otherLibs);
+                return {};
+            }();
 
-            auto moduleLibs = getOwner().getModuleLibs();
-            if (! moduleLibs.isEmpty())
-                libraries.addArray (moduleLibs);
+            auto result = otherLibs;
+            result.addArray (getOwner().getModuleLibs());
+            result.addArray (sharedCodeLib);
 
-            if (type != SharedCodeTarget)
-                if (auto* shared = getOwner().getSharedCodeTarget())
-                    libraries.add (shared->getBinaryNameWithSuffix (config, false));
+            for (auto& i : result)
+                i = msBuildEscape (getOwner().replacePreprocessorTokens (config, i).trim());
 
-            return libraries.joinIntoString (";");
+            return result;
         }
 
         String getDelayLoadedDLLs() const

extras/Projucer/Source/ProjectSaving/jucer_ProjectExporter.h
@@ -144,7 +144,8 @@ public:
     String getExtraCompilerFlagsString() const            { return extraCompilerFlagsValue.get().toString().replaceCharacters ("\r\n", "  "); }
     String getExtraLinkerFlagsString() const              { return extraLinkerFlagsValue.get().toString().replaceCharacters ("\r\n", "  "); }
 
-    String getExternalLibrariesString() const             { return getSearchPathsFromString (externalLibrariesValue.get().toString()).joinIntoString (";"); }
+    StringArray getExternalLibrariesStringArray() const   { return getSearchPathsFromString (externalLibrariesValue.get().toString()); }
+    String getExternalLibrariesString() const             { return getExternalLibrariesStringArray().joinIntoString (";"); }
 
     bool shouldUseGNUExtensions() const                   { return gnuExtensionsValue.get(); }
 