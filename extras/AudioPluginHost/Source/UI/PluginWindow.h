36e35aef3b51172c03d071c956150d2c23a6dd5a
reuk <reuk@users.noreply.github.com>
2021-09-29 12:42:22 +0100

    AudioPluginHost:  Display format of loaded plugins in graph view

--- extras/AudioPluginHost/Source/UI/PluginWindow.h
+++ extras/AudioPluginHost/Source/UI/PluginWindow.h
@@ -27,6 +27,19 @@
 
 #include "../Plugins/IOConfigurationWindow.h"
 
+inline String getFormatSuffix (const AudioProcessor* plugin)
+{
+    const auto format = [plugin]()
+    {
+        if (auto* instance = dynamic_cast<const AudioPluginInstance*> (plugin))
+            return instance->getPluginDescription().pluginFormatName;
+
+        return String();
+    }();
+
+    return format.isNotEmpty() ? (" (" + format + ")") : format;
+}
+
 class PluginGraph;
 
 /**
@@ -140,7 +153,7 @@ public:
     };
 
     PluginWindow (AudioProcessorGraph::Node* n, Type t, OwnedArray<PluginWindow>& windowList)
-       : DocumentWindow (n->getProcessor()->getName(),
+       : DocumentWindow (n->getProcessor()->getName() + getFormatSuffix (n->getProcessor()),
                          LookAndFeel::getDefaultLookAndFeel().findColour (ResizableWindow::backgroundColourId),
                          DocumentWindow::minimiseButton | DocumentWindow::closeButton),
          activeWindowList (windowList),
