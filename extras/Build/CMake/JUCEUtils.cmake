68a727fa19182fdc7f3daf836e69eb90d46cc08b
reuk <reuk@users.noreply.github.com>
2021-10-07 14:21:37 +0100

    CMake:  Factor source-grouping fixup into a separate function

--- extras/Build/CMake/JUCEUtils.cmake
+++ extras/Build/CMake/JUCEUtils.cmake
@@ -1611,18 +1611,7 @@ function(_juce_initialise_target target)
 
     _juce_write_generate_time_info(${target})
     _juce_link_optional_libraries(${target})
-
-    if(JUCE_ENABLE_MODULE_SOURCE_GROUPS)
-        get_property(all_modules GLOBAL PROPERTY _juce_module_names)
-
-        foreach(module_name IN LISTS all_modules)
-            get_target_property(path ${module_name} INTERFACE_JUCE_MODULE_PATH)
-            get_target_property(header_files ${module_name} INTERFACE_JUCE_MODULE_HEADERS)
-            get_target_property(source_files ${module_name} INTERFACE_JUCE_MODULE_SOURCES)
-            source_group(TREE ${path} PREFIX "JUCE Modules" FILES ${header_files} ${source_files})
-            set_source_files_properties(${header_files} PROPERTIES HEADER_FILE_ONLY TRUE)
-        endforeach()
-    endif()
+    _juce_fixup_module_source_groups()
 endfunction()
 
 # ==================================================================================================
