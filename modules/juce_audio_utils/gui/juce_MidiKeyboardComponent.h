7a6e346b0df04846a0df751ff2c97d936ff8b789
reuk <reuk@users.noreply.github.com>
2021-10-25 18:44:25 +0100

    MidiKeyboardComponent:  Ensure note is not highlighted after mouse leaves component

--- modules/juce_audio_utils/gui/juce_MidiKeyboardComponent.h
+++ modules/juce_audio_utils/gui/juce_MidiKeyboardComponent.h
@@ -399,6 +399,7 @@ protected:
 private:
     //==============================================================================
     struct UpDownButton;
+    struct NoteAndVelocity { int note; float velocity; };
 
     MidiKeyboardState& state;
     float blackNoteLengthRatio = 0.7f;
@@ -425,8 +426,8 @@ private:
     int keyMappingOctave = 6, octaveNumForMiddleC = 3;
 
     Range<float> getKeyPos (int midiNoteNumber) const;
-    int xyToNote (Point<float>, float& mousePositionVelocity);
-    int remappedXYToNote (Point<float>, float& mousePositionVelocity) const;
+    NoteAndVelocity xyToNote (Point<float>);
+    NoteAndVelocity remappedXYToNote (Point<float>) const;
     void resetAnyKeysInUse();
     void updateNoteUnderMouse (Point<float>, bool isDown, int fingerNum);
     void updateNoteUnderMouse (const MouseEvent&, bool isDown);
