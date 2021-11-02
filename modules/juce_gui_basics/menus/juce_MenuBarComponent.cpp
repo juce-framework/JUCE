ffee9b19de53b2658d1fd4a8d4699a1cd51fbd02
reuk <reuk@users.noreply.github.com>
2021-11-02 10:13:20 +0000

    MenuBarComponent:  Avoid dereferencing dangling pointer when dismissing menus

--- modules/juce_gui_basics/menus/juce_MenuBarComponent.cpp
+++ modules/juce_gui_basics/menus/juce_MenuBarComponent.cpp
@@ -241,10 +241,16 @@ void MenuBarComponent::showMenu (int index)
 
             auto itemBounds = itemComponent->getBounds();
 
+            const auto callback = [ref = SafePointer<MenuBarComponent> (this), index] (int result)
+            {
+                if (ref != nullptr)
+                    ref->menuDismissed (index, result);
+            };
+
             m.showMenuAsync (PopupMenu::Options().withTargetComponent (this)
                                                  .withTargetScreenArea (localAreaToGlobal (itemBounds))
                                                  .withMinimumWidth (itemBounds.getWidth()),
-                             [this, index] (int result) { menuDismissed (index, result); });
+                             callback);
         }
     }
 }
