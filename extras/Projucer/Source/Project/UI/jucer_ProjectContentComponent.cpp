f06d8098b004b3e9008304b4fdb6b4b4b32e7d1d
reuk <reuk@users.noreply.github.com>
2021-12-14 11:59:20 +0000

    Revert "Projucer:  Only resave project if necessary"
    This reverts commit 809252736b41abc6036f317f25e1e9539300aacb.

--- extras/Projucer/Source/Project/UI/jucer_ProjectContentComponent.cpp
+++ extras/Projucer/Source/Project/UI/jucer_ProjectContentComponent.cpp
@@ -512,15 +512,13 @@ void ProjectContentComponent::openInSelectedIDE (bool saveFirst)
                 return;
             }
 
-            if (project->hasChangedSinceSaved())
-            {
-                project->saveAsync (true, true, [safeThis = SafePointer<ProjectContentComponent> { this }] (Project::SaveResult r)
-                {
-                    if (safeThis != nullptr && r == Project::SaveResult::savedOk)
-                        safeThis->openInSelectedIDE (false);
-                });
-                return;
-            }
+            SafePointer<ProjectContentComponent> safeThis { this };
+            project->saveAsync (true, true, [safeThis] (Project::SaveResult r)
+                                {
+                                    if (safeThis != nullptr && r == Project::SaveResult::savedOk)
+                                        safeThis->openInSelectedIDE (false);
+                                });
+            return;
         }
 
         project->openProjectInIDE (*selectedExporter);
