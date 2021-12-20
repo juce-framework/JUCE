8a6e1980d0ec58fba70a76562c38366c0721ed92
Tom Poole <tom@juce.com>
2021-12-20 11:06:15 +0000

    Bump version number to 6.1.4

--- modules/juce_analytics/juce_analytics.h
+++ modules/juce_analytics/juce_analytics.h
@@ -35,7 +35,7 @@
 
   ID:                 juce_analytics
   vendor:             juce
-  version:            6.1.3
+  version:            6.1.4
   name:               JUCE analytics classes
   description:        Classes to collect analytics and send to destinations
   website:            http://www.juce.com/juce
