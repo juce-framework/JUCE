566be80d9e8fee60df63969e3a713637f9e12d47
Yair Chuchem <yairchu@gmail.com>
2022-02-06 15:50:40 +0200

    Avoid using C++17 feature

--- modules/juce_core/system/juce_SystemStats.cpp
+++ modules/juce_core/system/juce_SystemStats.cpp
@@ -183,7 +183,8 @@ String SystemStats::getStackBacktrace()
 
     for (int i = 0; i < frames; ++i)
     {
-        if (Dl_info info; dladdr (stack[i], &info))
+        Dl_info info;
+        if (dladdr (stack[i], &info))
         {
             int status;
             std::unique_ptr<char, decltype (::free)*> demangled (abi::__cxa_demangle (info.dli_sname, nullptr, 0, &status), ::free);
