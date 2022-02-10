5eb82463cf338ef21505f60f3df486a2bb0705d1
akiva <akiva.bamberger@gmail.com>
2022-02-10 13:08:14 -0800

    Revert "Use VoiceProcessingIO for apple (#2)" (#3)
    This reverts commit d5cb65d447f9ddc9c168341e73e770e22d70214f.

--- modules/juce_audio_devices/native/juce_ios_Audio.cpp
+++ modules/juce_audio_devices/native/juce_ios_Audio.cpp
@@ -962,7 +962,7 @@ struct iOSAudioIODevice::Pimpl      : public AudioPlayHead,
 
         AudioComponentDescription desc;
         desc.componentType = kAudioUnitType_Output;
-        desc.componentSubType = kAudioUnitSubType_VoiceProcessingIO;
+        desc.componentSubType = kAudioUnitSubType_RemoteIO;
         desc.componentManufacturer = kAudioUnitManufacturer_Apple;
         desc.componentFlags = 0;
         desc.componentFlagsMask = 0;
