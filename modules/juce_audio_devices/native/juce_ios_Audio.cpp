41d1a8098377f42a92559b882a24e0ddc1bff9d6
akiva <akiva.bamberger@gmail.com>
2022-02-18 11:21:31 -0800

    bump-size (#5)

--- modules/juce_audio_devices/native/juce_ios_Audio.cpp
+++ modules/juce_audio_devices/native/juce_ios_Audio.cpp
@@ -1342,7 +1342,7 @@ struct iOSAudioIODevice::Pimpl      : public AudioPlayHead,
    #if TARGET_IPHONE_SIMULATOR
     static constexpr int defaultBufferSize = 512;
    #else
-    static constexpr int defaultBufferSize = 256;
+    static constexpr int defaultBufferSize = 1024;
    #endif
     int targetBufferSize = defaultBufferSize, bufferSize = targetBufferSize;
 
