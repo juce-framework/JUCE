5f73e38e036f847c4687bef8ef74f48321cbdf74
Yair Chuchem <yairchu@gmail.com>
2022-02-21 15:41:33 +0200

    Revert "Fixed binding vertex even when using previous OpenGL API introduced in commit - 90ad1877aff6fa2e23523a93131183db156f1d2d"
    IIUC this should be solved in JUCE by d3c4017685852544fe778e1ab1d37dd9bd623a13
    
    This reverts commit ba136a92c7ac71cccc82ffe3756704d291254fe7.

--- modules/juce_opengl/opengl/juce_OpenGLContext.cpp
+++ modules/juce_opengl/opengl/juce_OpenGLContext.cpp
@@ -414,7 +414,7 @@ public:
 
     void bindVertexArray() noexcept
     {
-        if (context.versionRequired >= juce::OpenGLContext::OpenGLVersion::openGL3_2)
+        if (openGLVersion.major >= 3)
             if (vertexArrayObject != 0)
                 context.extensions.glBindVertexArray (vertexArrayObject);
     }
