juce-framework/JUCE

commit fc378aaf9ac9d9168172114e4fad39cf1d485fcc

AU Client:  Avoid AudioBuffer assertion when plugin has no audio channels

modules/juce_audio_processors/format_types/juce_AU_Shared.h
@@ -162,7 +162,9 @@ struct AudioUnitHelpers
                 jassert (channels[(size_t) i] != nullptr);
            #endif
 
-            mutableBuffer.setDataToReferTo (channels.data(), (int) channels.size(), static_cast<int> (frames));
+            if (! channels.empty())
+                mutableBuffer.setDataToReferTo (channels.data(), (int) channels.size(), static_cast<int> (frames));
+
             return mutableBuffer;
         }
 

commit 5b3aa7fc2ddeed50898f5c6b87db5fabf9031bcc

AU Client:  Ensure processBlock receives AudioBuffer of correct size
fc378aaf9ac9d9168172114e4fad39cf1d485fcc introduced a regression where
plugins with no audio channels (such as MIDI FX plugins) would receive
an audio buffer with a length-in-samples of '0', rather than the actual
block length.

modules/juce_audio_processors/format_types/juce_AU_Shared.h
@@ -157,13 +157,10 @@ struct AudioUnitHelpers
 
         AudioBuffer<float>& getBuffer (UInt32 frames) noexcept
         {
-           #if JUCE_DEBUG
-            for (int i = 0; i < (int) channels.size(); ++i)
-                jassert (channels[(size_t) i] != nullptr);
-           #endif
+            jassert (std::none_of (channels.begin(), channels.end(), [] (auto* x) { return x == nullptr; }));
 
-            if (! channels.empty())
-                mutableBuffer.setDataToReferTo (channels.data(), (int) channels.size(), static_cast<int> (frames));
+            const auto channelPtr = channels.empty() ? scratch.getArrayOfWritePointers() : channels.data();
+            mutableBuffer.setDataToReferTo (channelPtr, (int) channels.size(), static_cast<int> (frames));
 
             return mutableBuffer;
         }