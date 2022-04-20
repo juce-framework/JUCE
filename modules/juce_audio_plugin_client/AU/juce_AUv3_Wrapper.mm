3b6c05ec10db633b566ffa2665e08145a40e411e
Tom Poole <tom@juce.com>
2022-04-20 09:44:53 +0100

    iOS: Fix a compiler warning

--- modules/juce_audio_plugin_client/AU/juce_AUv3_Wrapper.mm
+++ modules/juce_audio_plugin_client/AU/juce_AUv3_Wrapper.mm
@@ -976,7 +976,7 @@ public:
             {
                 const auto value = (newValue != nullptr ? *newValue : juceParam->getValue()) * getMaximumParameterValue (juceParam);
 
-                if (@available (macOS 10.12, *))
+                if (@available (macOS 10.12, iOS 10.0, *))
                 {
                     [param setValue: value
                          originator: editorObserverToken
