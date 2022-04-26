b0a8bc0ce2a9241c05f47c3a2b9b6b37e8f70513
Tom Poole <tom@juce.com>
2022-04-26 11:38:19 +0100

    VST3: Fix a compiler warning in the VST3 SDK

--- modules/juce_audio_processors/format_types/juce_VST3Headers.h
+++ modules/juce_audio_processors/format_types/juce_VST3Headers.h
@@ -32,7 +32,8 @@
 // Wow, those Steinberg guys really don't worry too much about compiler warnings.
 JUCE_BEGIN_IGNORE_WARNINGS_LEVEL_MSVC (0, 4505 4702 6011 6031 6221 6386 6387 6330 6001 28199)
 
-JUCE_BEGIN_IGNORE_WARNINGS_GCC_LIKE ("-Wnon-virtual-dtor",
+JUCE_BEGIN_IGNORE_WARNINGS_GCC_LIKE ("-Wdeprecated-copy-dtor",
+                                     "-Wnon-virtual-dtor",
                                      "-Wreorder",
                                      "-Wunsequenced",
                                      "-Wint-to-pointer-cast",
