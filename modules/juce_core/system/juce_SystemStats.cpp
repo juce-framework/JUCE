addd2dacfdbe73243b458feaa5ffdf43dc52cc5b
Dan Raviv <dan@soundradix.com>
2022-05-22 16:58:57 -0700

    Fix -Wzero-as-null-pointer-constant warning

--- modules/juce_core/system/juce_SystemStats.cpp
+++ modules/juce_core/system/juce_SystemStats.cpp
@@ -187,7 +187,7 @@ String SystemStats::getStackBacktrace()
         if (dladdr (stack[i], &info))
         {
             int status;
-            std::unique_ptr<char, decltype (::free)*> demangled (abi::__cxa_demangle (info.dli_sname, nullptr, 0, &status), ::free);
+            std::unique_ptr<char, decltype (::free)*> demangled (abi::__cxa_demangle (info.dli_sname, nullptr, nullptr, &status), ::free);
             if (status == 0)
             {
                 result
