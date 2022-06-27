bcad7f1456a7011477f4610d8641ebdd0fc2d3cd
reuk <reuk@users.noreply.github.com>
2022-06-27 16:11:40 +0100

    AudioPlayHead:  Set playing/recording/looping flags in CurrentPositionInfo

--- modules/juce_audio_basics/audio_play_head/juce_AudioPlayHead.h
+++ modules/juce_audio_basics/audio_play_head/juce_AudioPlayHead.h
@@ -552,6 +552,10 @@ public:
             if (const auto timeInSamples = pos->getTimeInSamples())
                 result.timeInSamples = *timeInSamples;
 
+            result.isPlaying    = pos->getIsPlaying();
+            result.isRecording  = pos->getIsRecording();
+            result.isLooping    = pos->getIsLooping();
+
             return true;
         }
 
