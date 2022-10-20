f3b7b4ca2e29369220259797d008dd56434f4972
reuk <reuk@users.noreply.github.com>
2022-10-20 18:12:09 +0100

    TextInputTarget:  Improve input handling on macOS and iOS

--- modules/juce_gui_basics/native/juce_ios_Windowing.mm
+++ modules/juce_gui_basics/native/juce_ios_Windowing.mm
@@ -480,7 +480,7 @@ public:
         : callback (std::move (cb)),
           shouldDeleteThis (deleteOnCompletion)
     {
-        if (currentlyFocusedPeer != nullptr)
+        if (iOSGlobals::currentlyFocusedPeer != nullptr)
         {
             UIAlertController* alert = [UIAlertController alertControllerWithTitle: juceStringToNS (opts.getTitle())
                                                                            message: juceStringToNS (opts.getMessage())
@@ -490,9 +490,9 @@ public:
             addButton (alert, opts.getButtonText (1));
             addButton (alert, opts.getButtonText (2));
 
-            [currentlyFocusedPeer->controller presentViewController: alert
-                                                           animated: YES
-                                                         completion: nil];
+            [iOSGlobals::currentlyFocusedPeer->controller presentViewController: alert
+                                                                       animated: YES
+                                                                     completion: nil];
         }
         else
         {
