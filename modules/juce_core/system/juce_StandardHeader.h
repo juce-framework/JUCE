78f1baae08202b37b0775952069e73b8b2e7280e
Tom Poole <tom@juce.com>
2023-01-25 11:37:31 +0000

    Bump version number to 7.0.5

--- modules/juce_core/system/juce_StandardHeader.h
+++ modules/juce_core/system/juce_StandardHeader.h
@@ -29,7 +29,7 @@
 */
 #define JUCE_MAJOR_VERSION      7
 #define JUCE_MINOR_VERSION      0
-#define JUCE_BUILDNUMBER        4
+#define JUCE_BUILDNUMBER        5
 
 /** Current JUCE version number.
 
