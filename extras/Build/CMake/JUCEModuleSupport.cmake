962796ac52ae6029e6dbd09240e2d21c339a39ca
Mathieu Demange <contact@mathieudemange.fr>
2023-02-27 11:28:50 +0000

    CMake: Disable AAX builds with MinGW

--- extras/Build/CMake/JUCEModuleSupport.cmake
+++ extras/Build/CMake/JUCEModuleSupport.cmake
@@ -257,7 +257,7 @@ function(_juce_get_platform_plugin_kinds out)
         endif()
     endif()
 
-    if(CMAKE_SYSTEM_NAME STREQUAL "Darwin" OR CMAKE_SYSTEM_NAME STREQUAL "Windows")
+    if((CMAKE_SYSTEM_NAME STREQUAL "Darwin" OR CMAKE_SYSTEM_NAME STREQUAL "Windows") AND NOT (CMAKE_CXX_COMPILER_ID STREQUAL "GNU"))
         list(APPEND result AAX)
     endif()
 
