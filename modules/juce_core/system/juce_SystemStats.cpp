c596bb61992e2ef62c2b7db7a584e712b8a7ac91
Violet Purcell <vimproved@inventati.org>
2023-06-15 18:39:11 +0000

    SystemStats: Do not use execinfo.h on JUCE_MUSL

--- modules/juce_core/system/juce_SystemStats.cpp
+++ modules/juce_core/system/juce_SystemStats.cpp
@@ -178,7 +178,7 @@ String SystemStats::getStackBacktrace()
 {
     String result;
 
-   #if JUCE_ANDROID || JUCE_MINGW || JUCE_WASM
+   #if JUCE_ANDROID || JUCE_MINGW || JUCE_WASM || JUCE_MUSL
     jassertfalse; // sorry, not implemented yet!
 
    #elif JUCE_WINDOWS
