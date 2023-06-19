364976b1798c4646d9354682e370978b1ca43262
reuk <reuk@users.noreply.github.com>
2023-06-19 17:42:34 +0100

    AudioProcessor: Tidy up bus assertions

--- modules/juce_audio_processors/processors/juce_AudioProcessor.cpp
+++ modules/juce_audio_processors/processors/juce_AudioProcessor.cpp
@@ -1154,7 +1154,7 @@ void AudioProcessor::Bus::updateChannelCount() noexcept
 void AudioProcessor::BusesProperties::addBus (bool isInput, const String& name,
                                               const AudioChannelSet& dfltLayout, bool isActivatedByDefault)
 {
-    jassert (dfltLayout.size() != 0);
+    jassert (! dfltLayout.isDisabled());
 
     BusProperties props;
 
