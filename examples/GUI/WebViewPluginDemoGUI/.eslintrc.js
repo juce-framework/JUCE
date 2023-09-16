ee0aac2c75c39d2efc0905f49f5022bfa35a02d9
attila <attila@juce.com>
2023-09-17 00:04:29 +0200

    WebViewPluginDemo: Add empty React GUI project
    This commit contains an unmodified React app template. The interesting
    bit is the following commit that shows how to modify this template to
    interoperate with the JUCE framework.

---
+++ examples/GUI/WebViewPluginDemoGUI/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "no-extra-semi": "error",
+  },
+};
