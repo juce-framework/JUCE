e80fd215d5c8310c864af3e1dfff62ce2e7d310b
Tom Poole <tom@juce.com>
2023-10-19 09:16:54 +0100

    Resave all projects

--- extras/Projucer/JuceLibraryCode/JuceHeader.h
+++ extras/Projucer/JuceLibraryCode/JuceHeader.h
@@ -44,7 +44,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "Projucer";
     const char* const  companyName    = "Raw Material Software Limited";
-    const char* const  versionString  = "7.0.7";
-    const int          versionNumber  = 0x70007;
+    const char* const  versionString  = "7.0.8";
+    const int          versionNumber  = 0x70008;
 }
 #endif
