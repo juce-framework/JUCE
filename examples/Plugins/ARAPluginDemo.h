e6509fcb51eaa92b1c7a2c7918caced2eefd00b4
attila <attila@juce.com>
2023-12-08 16:52:55 +0100

    ARAPluginDemo: Fix potential crash during RegionSequence removal

--- examples/Plugins/ARAPluginDemo.h
+++ examples/Plugins/ARAPluginDemo.h
@@ -515,7 +515,9 @@ public:
 
     void willRemoveRegionSequence (ARA::PlugIn::RegionSequence* rs) noexcept override
     {
-        regionSequences.erase (static_cast<ARARegionSequence*> (rs));
+        auto* rsToRemove = static_cast<ARARegionSequence*> (rs);
+        rsToRemove->removeListener (this);
+        regionSequences.erase (rsToRemove);
     }
 
     void didAddPlaybackRegion (ARA::PlugIn::PlaybackRegion*) noexcept override
