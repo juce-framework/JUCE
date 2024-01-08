b2ab0973bac7e4c9330eb894a29c3bdaa48a32db
Jackson Kaplan <JwyattK@gmail.com>
2024-01-08 11:24:45 -0800

    juce networking fix issue #1738

--- modules/juce_core/native/juce_mac_Network.mm
+++ modules/juce_core/native/juce_mac_Network.mm
@@ -1141,7 +1141,8 @@ private:
                     [req HTTPBody];
 
                     if (@available (macOS 10.10, *))
-                        connection = std::make_unique<URLConnectionState> (req, numRedirectsToFollow);
+                        // connection = std::make_unique<URLConnectionState> (req, numRedirectsToFollow);
+                        connection = std::make_unique<URLConnectionStatePreYosemite> (req, numRedirectsToFollow);
                    #if JUCE_MAC
                     else
                         connection = std::make_unique<URLConnectionStatePreYosemite> (req, numRedirectsToFollow);
