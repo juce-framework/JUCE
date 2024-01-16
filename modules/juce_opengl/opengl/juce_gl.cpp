76b9b286588803cf0f9d548e791b7a20a0683d11
reuk <reuk@users.noreply.github.com>
2024-01-16 15:34:08 +0000

    OpenGL: Update compatibility headers
    This also adds missing KHRONOS_APIENTRY qualifications to the debug
    callback type, which fixes potential crashes when running debug Win32
    builds.

--- modules/juce_opengl/opengl/juce_gl.cpp
+++ modules/juce_opengl/opengl/juce_gl.cpp
@@ -1140,7 +1140,7 @@
     X (void        , glDebugMessageEnableAMD, (GLenum category, GLenum severity, GLsizei count, const GLuint *ids, GLboolean enabled)) \
     X (void        , glDebugMessageInsertAMD, (GLenum category, GLenum severity, GLuint id, GLsizei length, const GLchar *buf)) \
     X (void        , glDebugMessageCallbackAMD, (GLDEBUGPROCAMD callback, void *userParam)) \
-    X (GLuint      , glGetDebugMessageLogAMD, (GLuint count, GLsizei bufSize, GLenum *categories, GLuint *severities, GLuint *ids, GLsizei *lengths, GLchar *message))
+    X (GLuint      , glGetDebugMessageLogAMD, (GLuint count, GLsizei bufSize, GLenum *categories, GLenum *severities, GLuint *ids, GLsizei *lengths, GLchar *message))
 
 #define JUCE_GL_FUNCTIONS_GL_AMD_draw_buffers_blend \
     X (void        , glBlendFuncIndexedAMD, (GLuint buf, GLenum src, GLenum dst)) \
@@ -2189,6 +2189,10 @@
 #define JUCE_GL_FUNCTIONS_GL_EXT_framebuffer_blit \
     X (void        , glBlitFramebufferEXT, (GLint srcX0, GLint srcY0, GLint srcX1, GLint srcY1, GLint dstX0, GLint dstY0, GLint dstX1, GLint dstY1, GLbitfield mask, GLenum filter))
 
+#define JUCE_GL_FUNCTIONS_GL_EXT_framebuffer_blit_layers \
+    X (void        , glBlitFramebufferLayersEXT, (GLint srcX0, GLint srcY0, GLint srcX1, GLint srcY1, GLint dstX0, GLint dstY0, GLint dstX1, GLint dstY1, GLbitfield mask, GLenum filter)) \
+    X (void        , glBlitFramebufferLayerEXT, (GLint srcX0, GLint srcY0, GLint srcX1, GLint srcY1, GLint srcLayer, GLint dstX0, GLint dstY0, GLint dstX1, GLint dstY1, GLint dstLayer, GLbitfield mask, GLenum filter))
+
 #define JUCE_GL_FUNCTIONS_GL_EXT_framebuffer_multisample \
     X (void        , glRenderbufferStorageMultisampleEXT, (GLenum target, GLsizei samples, GLenum internalformat, GLsizei width, GLsizei height))
 
@@ -2384,7 +2388,7 @@
     X (GLuint      , glCreateShaderProgramEXT, (GLenum type, const GLchar *string)) \
     X (void        , glActiveShaderProgramEXT, (GLuint pipeline, GLuint program)) \
     X (void        , glBindProgramPipelineEXT, (GLuint pipeline)) \
-    X (GLuint      , glCreateShaderProgramvEXT, (GLenum type, GLsizei count, const GLchar **strings)) \
+    X (GLuint      , glCreateShaderProgramvEXT, (GLenum type, GLsizei count, const GLchar *const*strings)) \
     X (void        , glDeleteProgramPipelinesEXT, (GLsizei n, const GLuint *pipelines)) \
     X (void        , glGenProgramPipelinesEXT, (GLsizei n, GLuint *pipelines)) \
     X (void        , glGetProgramPipelineInfoLogEXT, (GLuint pipeline, GLsizei bufSize, GLsizei *length, GLchar *infoLog)) \
@@ -2438,6 +2442,11 @@
 
 #define JUCE_GL_FUNCTIONS_GL_EXT_texture_perturb_normal \
     X (void        , glTextureNormalEXT, (GLenum mode))
+
+#define JUCE_GL_FUNCTIONS_GL_EXT_texture_storage \
+    X (void        , glTexStorage1DEXT, (GLenum target, GLsizei levels, GLenum internalformat, GLsizei width)) \
+    X (void        , glTexStorage2DEXT, (GLenum target, GLsizei levels, GLenum internalformat, GLsizei width, GLsizei height)) \
+    X (void        , glTexStorage3DEXT, (GLenum target, GLsizei levels, GLenum internalformat, GLsizei width, GLsizei height, GLsizei depth))
 
 #define JUCE_GL_FUNCTIONS_GL_NV_timeline_semaphore \
     X (void        , glCreateSemaphoresNV, (GLsizei n, GLuint *semaphores)) \
@@ -2850,12 +2859,6 @@
     X (void        , glMultiTexCoord3hvNV, (GLenum target, const GLhalfNV *v)) \
     X (void        , glMultiTexCoord4hNV, (GLenum target, GLhalfNV s, GLhalfNV t, GLhalfNV r, GLhalfNV q)) \
     X (void        , glMultiTexCoord4hvNV, (GLenum target, const GLhalfNV *v)) \
-    X (void        , glFogCoordhNV, (GLhalfNV fog)) \
-    X (void        , glFogCoordhvNV, (const GLhalfNV *fog)) \
-    X (void        , glSecondaryColor3hNV, (GLhalfNV red, GLhalfNV green, GLhalfNV blue)) \
-    X (void        , glSecondaryColor3hvNV, (const GLhalfNV *v)) \
-    X (void        , glVertexWeighthNV, (GLhalfNV weight)) \
-    X (void        , glVertexWeighthvNV, (const GLhalfNV *weight)) \
     X (void        , glVertexAttrib1hNV, (GLuint index, GLhalfNV x)) \
     X (void        , glVertexAttrib1hvNV, (GLuint index, const GLhalfNV *v)) \
     X (void        , glVertexAttrib2hNV, (GLuint index, GLhalfNV x, GLhalfNV y)) \
@@ -2867,7 +2870,13 @@
     X (void        , glVertexAttribs1hvNV, (GLuint index, GLsizei n, const GLhalfNV *v)) \
     X (void        , glVertexAttribs2hvNV, (GLuint index, GLsizei n, const GLhalfNV *v)) \
     X (void        , glVertexAttribs3hvNV, (GLuint index, GLsizei n, const GLhalfNV *v)) \
-    X (void        , glVertexAttribs4hvNV, (GLuint index, GLsizei n, const GLhalfNV *v))
+    X (void        , glVertexAttribs4hvNV, (GLuint index, GLsizei n, const GLhalfNV *v)) \
+    X (void        , glFogCoordhNV, (GLhalfNV fog)) \
+    X (void        , glFogCoordhvNV, (const GLhalfNV *fog)) \
+    X (void        , glSecondaryColor3hNV, (GLhalfNV red, GLhalfNV green, GLhalfNV blue)) \
+    X (void        , glSecondaryColor3hvNV, (const GLhalfNV *v)) \
+    X (void        , glVertexWeighthNV, (GLhalfNV weight)) \
+    X (void        , glVertexWeighthvNV, (const GLhalfNV *weight))
 
 #define JUCE_GL_FUNCTIONS_GL_NV_internalformat_sample_query \
     X (void        , glGetInternalformatSampleivNV, (GLenum target, GLenum internalformat, GLsizei samples, GLenum pname, GLsizei count, GLint *params))
@@ -3880,6 +3889,7 @@
     JUCE_GL_FUNCTIONS_GL_EXT_external_buffer \
     JUCE_GL_FUNCTIONS_GL_EXT_fog_coord \
     JUCE_GL_FUNCTIONS_GL_EXT_framebuffer_blit \
+    JUCE_GL_FUNCTIONS_GL_EXT_framebuffer_blit_layers \
     JUCE_GL_FUNCTIONS_GL_EXT_framebuffer_multisample \
     JUCE_GL_FUNCTIONS_GL_EXT_framebuffer_object \
     JUCE_GL_FUNCTIONS_GL_EXT_geometry_shader4 \
@@ -3917,6 +3927,7 @@
     JUCE_GL_FUNCTIONS_GL_EXT_texture_integer \
     JUCE_GL_FUNCTIONS_GL_EXT_texture_object \
     JUCE_GL_FUNCTIONS_GL_EXT_texture_perturb_normal \
+    JUCE_GL_FUNCTIONS_GL_EXT_texture_storage \
     JUCE_GL_FUNCTIONS_GL_NV_timeline_semaphore \
     JUCE_GL_FUNCTIONS_GL_EXT_timer_query \
     JUCE_GL_FUNCTIONS_GL_EXT_transform_feedback \
--- modules/juce_opengl/opengl/juce_gles2.cpp
+++ modules/juce_opengl/opengl/juce_gles2.cpp
@@ -460,6 +460,9 @@
     X (void        , glGetInteger64vAPPLE, (GLenum pname, GLint64 *params)) \
     X (void        , glGetSyncivAPPLE, (GLsync sync, GLenum pname, GLsizei count, GLsizei *length, GLint *values))
 
+#define JUCE_GL_FUNCTIONS_GL_ARM_shader_core_properties \
+    X (void        , glMaxActiveShaderCoresARM, (GLuint count))
+
 #define JUCE_GL_FUNCTIONS_GL_EXT_EGL_image_storage \
     X (void        , glEGLImageTargetTexStorageEXT, (GLenum target, GLeglImageOES image, const GLint* attrib_list)) \
     X (void        , glEGLImageTargetTextureStorageEXT, (GLuint texture, GLeglImageOES image, const GLint* attrib_list))
@@ -547,6 +550,16 @@
 #define JUCE_GL_FUNCTIONS_GL_EXT_external_buffer \
     X (void        , glBufferStorageExternalEXT, (GLenum target, GLintptr offset, GLsizeiptr size, GLeglClientBufferEXT clientBuffer, GLbitfield flags)) \
     X (void        , glNamedBufferStorageExternalEXT, (GLuint buffer, GLintptr offset, GLsizeiptr size, GLeglClientBufferEXT clientBuffer, GLbitfield flags))
+
+#define JUCE_GL_FUNCTIONS_GL_EXT_fragment_shading_rate \
+    X (void        , glGetFragmentShadingRatesEXT, (GLsizei samples, GLsizei maxCount, GLsizei *count, GLenum *shadingRates)) \
+    X (void        , glShadingRateEXT, (GLenum rate)) \
+    X (void        , glShadingRateCombinerOpsEXT, (GLenum combinerOp0, GLenum combinerOp1)) \
+    X (void        , glFramebufferShadingRateEXT, (GLenum target, GLenum attachment, GLuint texture, GLint baseLayer, GLsizei numLayers, GLsizei texelWidth, GLsizei texelHeight))
+
+#define JUCE_GL_FUNCTIONS_GL_EXT_framebuffer_blit_layers \
+    X (void        , glBlitFramebufferLayersEXT, (GLint srcX0, GLint srcY0, GLint srcX1, GLint srcY1, GLint dstX0, GLint dstY0, GLint dstX1, GLint dstY1, GLbitfield mask, GLenum filter)) \
+    X (void        , glBlitFramebufferLayerEXT, (GLint srcX0, GLint srcY0, GLint srcX1, GLint srcY1, GLint srcLayer, GLint dstX0, GLint dstY0, GLint dstX1, GLint dstY1, GLint dstLayer, GLbitfield mask, GLenum filter))
 
 #define JUCE_GL_FUNCTIONS_GL_EXT_geometry_shader \
     X (void        , glFramebufferTextureEXT, (GLenum target, GLenum attachment, GLuint texture, GLint level))
@@ -640,7 +653,7 @@
     X (GLuint      , glCreateShaderProgramEXT, (GLenum type, const GLchar *string)) \
     X (void        , glActiveShaderProgramEXT, (GLuint pipeline, GLuint program)) \
     X (void        , glBindProgramPipelineEXT, (GLuint pipeline)) \
-    X (GLuint      , glCreateShaderProgramvEXT, (GLenum type, GLsizei count, const GLchar **strings)) \
+    X (GLuint      , glCreateShaderProgramvEXT, (GLenum type, GLsizei count, const GLchar *const*strings)) \
     X (void        , glDeleteProgramPipelinesEXT, (GLsizei n, const GLuint *pipelines)) \
     X (void        , glGenProgramPipelinesEXT, (GLsizei n, GLuint *pipelines)) \
     X (void        , glGetProgramPipelineInfoLogEXT, (GLuint pipeline, GLsizei bufSize, GLsizei *length, GLchar *infoLog)) \
@@ -718,6 +731,10 @@
     X (void        , glTextureStorage1DEXT, (GLuint texture, GLenum target, GLsizei levels, GLenum internalformat, GLsizei width)) \
     X (void        , glTextureStorage2DEXT, (GLuint texture, GLenum target, GLsizei levels, GLenum internalformat, GLsizei width, GLsizei height)) \
     X (void        , glTextureStorage3DEXT, (GLuint texture, GLenum target, GLsizei levels, GLenum internalformat, GLsizei width, GLsizei height, GLsizei depth))
+
+#define JUCE_GL_FUNCTIONS_GL_EXT_texture_storage_compression \
+    X (void        , glTexStorageAttribs2DEXT, (GLenum target, GLsizei levels, GLenum internalformat, GLsizei width, GLsizei height, const GLint* attrib_list)) \
+    X (void        , glTexStorageAttribs3DEXT, (GLenum target, GLsizei levels, GLenum internalformat, GLsizei width, GLsizei height, GLsizei depth, const GLint* attrib_list))
 
 #define JUCE_GL_FUNCTIONS_GL_EXT_texture_view \
     X (void        , glTextureViewEXT, (GLuint texture, GLenum target, GLuint origtexture, GLenum internalformat, GLuint minlevel, GLuint numlevels, GLuint minlayer, GLuint numlayers))
@@ -1265,6 +1282,7 @@
     JUCE_GL_FUNCTIONS_GL_APPLE_copy_texture_levels \
     JUCE_GL_FUNCTIONS_GL_APPLE_framebuffer_multisample \
     JUCE_GL_FUNCTIONS_GL_APPLE_sync \
+    JUCE_GL_FUNCTIONS_GL_ARM_shader_core_properties \
     JUCE_GL_FUNCTIONS_GL_EXT_EGL_image_storage \
     JUCE_GL_FUNCTIONS_GL_EXT_base_instance \
     JUCE_GL_FUNCTIONS_GL_EXT_blend_func_extended \
@@ -1283,6 +1301,8 @@
     JUCE_GL_FUNCTIONS_GL_EXT_draw_instanced \
     JUCE_GL_FUNCTIONS_GL_EXT_draw_transform_feedback \
     JUCE_GL_FUNCTIONS_GL_EXT_external_buffer \
+    JUCE_GL_FUNCTIONS_GL_EXT_fragment_shading_rate \
+    JUCE_GL_FUNCTIONS_GL_EXT_framebuffer_blit_layers \
     JUCE_GL_FUNCTIONS_GL_EXT_geometry_shader \
     JUCE_GL_FUNCTIONS_GL_EXT_instanced_arrays \
     JUCE_GL_FUNCTIONS_GL_EXT_map_buffer_range \
@@ -1308,6 +1328,7 @@
     JUCE_GL_FUNCTIONS_GL_EXT_texture_border_clamp \
     JUCE_GL_FUNCTIONS_GL_EXT_texture_buffer \
     JUCE_GL_FUNCTIONS_GL_EXT_texture_storage \
+    JUCE_GL_FUNCTIONS_GL_EXT_texture_storage_compression \
     JUCE_GL_FUNCTIONS_GL_EXT_texture_view \
     JUCE_GL_FUNCTIONS_GL_NV_timeline_semaphore \
     JUCE_GL_FUNCTIONS_GL_EXT_win32_keyed_mutex \
