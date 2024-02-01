juce-framework/JUCE

commit fb14118771eeb34f51146e23def0a1f94a49fbad

MacOS: Disable window controls for windows created by JUCE when a Component is modal
The change does not affect plugin windows, which are created by the
host.

modules/juce_gui_basics/native/juce_NSViewComponentPeer_mac.mm
@@ -1712,7 +1712,7 @@ public:
     NSWindow* window = nil;
     NSView* view = nil;
     WeakReference<Component> safeComponent;
-    bool isSharedWindow = false;
+    const bool isSharedWindow = false;
    #if USE_COREGRAPHICS_RENDERING
     bool usingCoreGraphics = true;
    #else
@@ -1998,9 +1998,38 @@ private:
        #endif
     }
 
+    void modalComponentManagerChanged()
+    {
+        if (isSharedWindow)
+            return;
+
+        auto style = [window styleMask];
+
+        if (ModalComponentManager::getInstance()->getNumModalComponents() > 0)
+        {
+            style &= ~NSWindowStyleMaskMiniaturizable;
+            style &= ~NSWindowStyleMaskClosable;
+        }
+        else
+        {
+            const auto flags = getStyleFlags();
+
+            if ((flags & windowHasMinimiseButton) != 0)  style |= NSWindowStyleMaskMiniaturizable;
+            if ((flags & windowHasCloseButton) != 0)     style |= NSWindowStyleMaskClosable;
+        }
+
+        [window setStyleMask: style];
+    }
+
     //==============================================================================
     std::vector<ScopedNotificationCenterObserver> scopedObservers;
     std::vector<ScopedNotificationCenterObserver> windowObservers;
+
+    ErasedScopeGuard modalChangeListenerScope =
+        detail::ComponentHelpers::ModalComponentManagerChangeNotifier::getInstance().addListener ([this]
+                                                                                                  {
+                                                                                                      modalComponentManagerChanged();
+                                                                                                  });
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (NSViewComponentPeer)
 };

modules/juce_gui_basics/components/juce_ModalComponentManager.cpp
@@ -106,7 +106,10 @@ JUCE_IMPLEMENT_SINGLETON (ModalComponentManager)
 void ModalComponentManager::startModal (Component* component, bool autoDelete)
 {
     if (component != nullptr)
+    {
         stack.add (new ModalItem (component, autoDelete));
+        detail::ComponentHelpers::ModalComponentManagerChangeNotifier::getInstance().modalComponentManagerChanged();
+    }
 }
 
 void ModalComponentManager::attachCallback (Component* component, Callback* callback)
@@ -210,6 +213,8 @@ void ModalComponentManager::handleAsyncUpdate()
                 item->callbacks.getUnchecked (j)->modalStateFinished (item->returnValue);
 
             compToDelete.deleteAndZero();
+
+            detail::ComponentHelpers::ModalComponentManagerChangeNotifier::getInstance().modalComponentManagerChanged();
         }
     }
 }

modules/juce_gui_basics/detail/juce_ComponentHelpers.h
@@ -250,6 +250,31 @@ struct ComponentHelpers
                 if (modalWouldBlockComponent (*c, &modal))
                     (c->*function) (ms, SH::screenPosToLocalPos (*c, ms.getScreenPosition()), Time::getCurrentTime());
     }
+
+    class ModalComponentManagerChangeNotifier
+    {
+    public:
+        static auto& getInstance()
+        {
+            static ModalComponentManagerChangeNotifier instance;
+            return instance;
+        }
+
+        ErasedScopeGuard addListener (std::function<void()> l)
+        {
+            return listeners.addListener (std::move (l));
+        }
+
+        void modalComponentManagerChanged()
+        {
+            listeners.call();
+        }
+
+    private:
+        ModalComponentManagerChangeNotifier() = default;
+
+        detail::CallbackListenerList<> listeners;
+    };
 };
 
 } // namespace juce::detail

commit 6056c686b8264d00022825005262618e0f0ade00

MacOS: Fix invisible PopupMenu in Logic
We avoid modifying window controls in modal situations if possible.
Doing as much as [window setStyleMask: [window styleMask]]; for a
PopupMenu window will make it invisible on MacOS when the plugin is
hosted in an external subprocess.

This fixes a regression caused by
fb14118771eeb34f51146e23def0a1f94a49fbad.

modules/juce_gui_basics/native/juce_NSViewComponentPeer_mac.mm
@@ -1998,33 +1998,83 @@ private:
        #endif
     }
 
+    /*  Used to store and restore the values of the NSWindowStyleMaskClosable and
+        NSWindowStyleMaskMiniaturizable flags.
+    */
+    struct StoredStyleFlags
+    {
+        StoredStyleFlags (NSWindowStyleMask m)
+            : stored { m }
+        {}
+
+        static auto getStoredFlags()
+        {
+            return std::array<NSWindowStyleMask, 2> { NSWindowStyleMaskClosable,
+                                                      NSWindowStyleMaskMiniaturizable };
+        }
+
+        auto withFlagsRestored (NSWindowStyleMask m) const
+        {
+            for (const auto& f : getStoredFlags())
+                m = withFlagFromStored (m, f);
+
+            return m;
+        }
+
+    private:
+        NSWindowStyleMask withFlagFromStored (NSWindowStyleMask m, NSWindowStyleMask flag) const
+        {
+            return (m & ~flag) | (stored & flag);
+        }
+
+        NSWindowStyleMask stored;
+    };
+
     void modalComponentManagerChanged()
     {
-        if (isSharedWindow)
+        // We are only changing the style flags if we absolutely have to. Plugin windows generally
+        // don't like to be modified. Windows created under plugin hosts running in an external
+        // subprocess are particularly touchy, and may make the window invisible even if we call
+        // [window setStyleMask [window setStyleMask]].
+        if (isSharedWindow || ! hasNativeTitleBar())
             return;
 
-        auto style = [window styleMask];
-
-        if (ModalComponentManager::getInstance()->getNumModalComponents() > 0)
+        const auto newStyleMask = [&]() -> std::optional<NSWindowStyleMask>
         {
-            style &= ~NSWindowStyleMaskMiniaturizable;
-            style &= ~NSWindowStyleMaskClosable;
-        }
-        else
-        {
-            const auto flags = getStyleFlags();
+            const auto currentStyleMask = [window styleMask];
 
-            if ((flags & windowHasMinimiseButton) != 0)  style |= NSWindowStyleMaskMiniaturizable;
-            if ((flags & windowHasCloseButton) != 0)     style |= NSWindowStyleMaskClosable;
-        }
+            if (ModalComponentManager::getInstance()->getNumModalComponents() > 0)
+            {
+                if (! storedFlags)
+                    storedFlags.emplace (currentStyleMask);
 
-        [window setStyleMask: style];
+                auto updatedMask = (storedFlags->withFlagsRestored (currentStyleMask)) & ~NSWindowStyleMaskMiniaturizable;
+
+                if (component.isCurrentlyBlockedByAnotherModalComponent())
+                    updatedMask &= ~NSWindowStyleMaskClosable;
+
+                return updatedMask;
+            }
+
+            if (storedFlags)
+            {
+                const auto flagsToApply = storedFlags->withFlagsRestored (currentStyleMask);
+                storedFlags.reset();
+                return flagsToApply;
+            }
+
+            return {};
+        }();
+
+        if (newStyleMask && *newStyleMask != [window styleMask])
+            [window setStyleMask: *newStyleMask];
     }
 
     //==============================================================================
     std::vector<ScopedNotificationCenterObserver> scopedObservers;
     std::vector<ScopedNotificationCenterObserver> windowObservers;
 
+    std::optional<StoredStyleFlags> storedFlags;
     ErasedScopeGuard modalChangeListenerScope =
         detail::ComponentHelpers::ModalComponentManagerChangeNotifier::getInstance().addListener ([this]
                                                                                                   {