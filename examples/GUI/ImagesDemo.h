dc38616f783322ac0812eaf613b6f475812e8490
MajorMurphy <major@murphyindustries.net>
2024-01-31 20:07:59 -0500

    fixed Android build errors

--- examples/GUI/ImagesDemo.h
+++ examples/GUI/ImagesDemo.h
@@ -108,7 +108,7 @@ public:
     }
 
 private:
-    WildcardFileFilter imagesWildcardFilter  { "*.jpeg;*.jpg;*.png;*.gif", "*", "Image File Filter"};
+    WildcardFileFilter imagesWildcardFilter  { "*.jpeg;*.jpg;*.png;*.gif;*.webp", "*", "Image File Filter"};
     TimeSliceThread directoryThread          { "Image File Scanner Thread" };
     DirectoryContentsList imageList          { &imagesWildcardFilter, directoryThread };
     FileTreeComponent fileTree               { imageList };
