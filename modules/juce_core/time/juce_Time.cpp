3988d492acc3bb796738d57bca436dbc74835a2d
Oliver James <oli@juce.com>
2024-06-06 14:13:02 +0100

    Core: Move __DATE__ and __TIME__ into a dedicated TU

--- modules/juce_core/time/juce_Time.cpp
+++ modules/juce_core/time/juce_Time.cpp
@@ -602,14 +602,18 @@ static int getMonthNumberForCompileDate (const String& m)
     return 0;
 }
 
+// Implemented in juce_core_CompilationTime.cpp
+extern const char* juce_compilationDate;
+extern const char* juce_compilationTime;
+
 Time Time::getCompilationDate()
 {
     StringArray dateTokens, timeTokens;
 
-    dateTokens.addTokens (__DATE__, true);
+    dateTokens.addTokens (juce_compilationDate, true);
     dateTokens.removeEmptyStrings (true);
 
-    timeTokens.addTokens (__TIME__, ":", StringRef());
+    timeTokens.addTokens (juce_compilationTime, ":", StringRef());
 
     return Time (dateTokens[2].getIntValue(),
                  getMonthNumberForCompileDate (dateTokens[0]),
