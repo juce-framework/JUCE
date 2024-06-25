9dda9d65a73f5a4131d326bd7f03204d9008d004
reuk <reuk@users.noreply.github.com>
2024-06-25 15:19:44 +0100

    RenderingHelpers: Allow software renderer to replace content of filled rects

--- modules/juce_graphics/native/juce_RenderingHelpers.h
+++ modules/juce_graphics/native/juce_RenderingHelpers.h
@@ -2191,8 +2191,10 @@ public:
             }
             else if (! transform.isRotated)
             {
-                jassert (! replaceContents); // not implemented
-                fillTargetRect (transform.boundsAfterTransform (r.toFloat()));
+                if (replaceContents)
+                    fillTargetRect (transform.boundsAfterTransform (r.toFloat()).toNearestInt(), true);
+                else
+                    fillTargetRect (transform.boundsAfterTransform (r.toFloat()));
             }
             else
             {
