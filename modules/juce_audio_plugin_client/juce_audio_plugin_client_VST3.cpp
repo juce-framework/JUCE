juce-framework/JUCE

commit 335f6e9591f6f09f74e13769c74b2d345e8336c9

VST3: Fix context menu refcounts
When the host creates an IContextMenu, the initial refcount should be 1 rather than zero.

modules/juce_audio_plugin_client/juce_audio_plugin_client_VST3.cpp
@@ -1721,7 +1721,7 @@ private:
                 return {};
 
             const auto idToUse = parameter != nullptr ? processor.getVSTParamIDForIndex (parameter->getParameterIndex()) : 0;
-            const auto menu = VSTComSmartPtr<Steinberg::Vst::IContextMenu> (handler->createContextMenu (view, &idToUse));
+            const auto menu = VSTComSmartPtr<Steinberg::Vst::IContextMenu> (handler->createContextMenu (view, &idToUse), false);
             return std::make_unique<EditorContextMenu> (editor, menu);
         }
 

modules/juce_audio_processors/format_types/juce_VST3PluginFormat.cpp
@@ -563,10 +563,12 @@ struct VST3HostContext  : public Vst::IComponentHandler,  // From VST V3.0.0
 
     Vst::IContextMenu* PLUGIN_API createContextMenu (IPlugView*, const Vst::ParamID*) override
     {
-        if (plugin != nullptr)
-            return new ContextMenu (*plugin);
+        if (plugin == nullptr)
+            return nullptr;
 
-        return nullptr;
+        auto* result = new ContextMenu (*plugin);
+        result->addRef();
+        return result;
     }
 
     tresult PLUGIN_API executeMenuItem (Steinberg::int32) override

commit 66aa42c9c0e272b505813216167ccb8f401a470c

VST3 Client: Fix parameter context menu in FL Studio
The change to the VST3 wrapper in
335f6e9591f6f09f74e13769c74b2d345e8336c9 broke context menu items in FL
Studio, so that clicking on a menu item had no effect. This could be
seen in the DSPModulePluginDemo example.

I'm not sure exactly what caused the breakage. Before the breaking
change, the menu remained alive until the point where the menu item was
triggered as it was leaked. After the breaking change, the IContextMenu
could be freed before a particular IContextMenuTarget was executed. My
guess is that FL Studio requires the menu to be alive when triggering a
particular menu item.

This change captures the IContextMenu inside the popup menu callback to
ensure that it remains alive until the PopupMenu is destroyed.

modules/juce_audio_plugin_client/juce_audio_plugin_client_VST3.cpp
@@ -1735,12 +1735,19 @@ private:
                 }
                 else
                 {
-                    const auto ownedTarget = addVSTComSmartPtrOwner (target);
-                    const auto tag = item.tag;
+                    const auto callback = [menu = contextMenu, i]
+                    {
+                        MenuItem localItem{};
+                        MenuTarget* localTarget = nullptr;
+
+                        if (menu->getItem (i, localItem, &localTarget) == kResultOk && localTarget != nullptr)
+                            localTarget->executeMenuItem (localItem.tag);
+                    };
+
                     menuStack.back().menu.addItem (toString (item.name),
                                                    (item.flags & MenuItem::kIsDisabled) == 0,
                                                    (item.flags & MenuItem::kIsChecked) != 0,
-                                                   [ownedTarget, tag] { ownedTarget->executeMenuItem (tag); });
+                                                   callback);
                 }
             }
 