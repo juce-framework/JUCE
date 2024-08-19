d7d8155da9e1d5ac053c3c76dd32f7f6e6d22b06
attila <attila@juce.com>
2024-08-19 12:34:06 +0200

    Fix some doxygen issues

--- modules/juce_gui_basics/widgets/juce_TextEditor.h
+++ modules/juce_gui_basics/widgets/juce_TextEditor.h
@@ -632,7 +632,7 @@ public:
         virtual ~InputFilter() = default;
 
         /** This method is called whenever text is entered into the editor.
-            An implementation of this class should should check the input string,
+            An implementation of this class should check the input string,
             and return an edited version of it that should be used.
         */
         virtual String filterNewText (TextEditor&, const String& newInput) = 0;
--- modules/juce_gui_basics/windows/juce_VBlankAttachment.h
+++ modules/juce_gui_basics/windows/juce_VBlankAttachment.h
@@ -39,7 +39,7 @@ namespace juce
     Helper class to synchronise Component updates to the vertical blank event of the display that
     the Component is presented on. This is useful when animating the Component's contents.
 
-    @tags{GUI, Animations}
+    @tags{Animations}
 */
 class JUCE_API  VBlankAttachment final  : public ComponentPeer::VBlankListener,
                                           public ComponentListener
