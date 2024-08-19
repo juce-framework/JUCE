juce-framework/JUCE

commit 5ce2fc388e25cdf85b9fec5360800a513d49ae1f

Fix C++23 compilation

modules/juce_gui_extra/juce_gui_extra.cpp
@@ -148,6 +148,16 @@
 #include "misc/juce_PushNotifications.cpp"
 #include "misc/juce_RecentlyOpenedFilesList.cpp"
 #include "misc/juce_SplashScreen.cpp"
+
+#if JUCE_MAC
+ #include "native/juce_SystemTrayIcon_mac.cpp"
+#elif JUCE_WINDOWS
+ #include "native/juce_ActiveXComponent_windows.cpp"
+ #include "native/juce_SystemTrayIcon_windows.cpp"
+#elif JUCE_LINUX
+ #include "native/juce_SystemTrayIcon_linux.cpp"
+#endif
+
 #include "misc/juce_SystemTrayIconComponent.cpp"
 #include "misc/juce_LiveConstantEditor.cpp"
 #include "misc/juce_AnimatedAppComponent.cpp"
@@ -161,7 +171,6 @@
   #include "native/juce_NSViewFrameWatcher_mac.h"
   #include "native/juce_NSViewComponent_mac.mm"
   #include "native/juce_AppleRemote_mac.mm"
-  #include "native/juce_SystemTrayIcon_mac.cpp"
  #endif
 
  #if JUCE_IOS
@@ -174,12 +183,10 @@
 
 //==============================================================================
 #elif JUCE_WINDOWS
- #include "native/juce_ActiveXComponent_windows.cpp"
  #include "native/juce_HWNDComponent_windows.cpp"
  #if JUCE_WEB_BROWSER
   #include "native/juce_WebBrowserComponent_windows.cpp"
  #endif
- #include "native/juce_SystemTrayIcon_windows.cpp"
 
 //==============================================================================
 #elif JUCE_LINUX || JUCE_BSD
@@ -197,8 +204,6 @@
  #endif
 
  JUCE_END_IGNORE_WARNINGS_GCC_LIKE
-
- #include "native/juce_SystemTrayIcon_linux.cpp"
 
 //==============================================================================
 #elif JUCE_ANDROID

extras/Projucer/Source/Application/jucer_AutoUpdater.cpp
@@ -36,6 +36,158 @@
 #include "jucer_Application.h"
 #include "jucer_AutoUpdater.h"
 
+class DownloadAndInstallThread final : private ThreadWithProgressWindow
+{
+public:
+    DownloadAndInstallThread  (const VersionInfo::Asset& a, const File& t, std::function<void (Result)>&& cb)
+        : ThreadWithProgressWindow ("Downloading New Version", true, true),
+          asset (a), targetFolder (t), completionCallback (std::move (cb))
+    {
+        launchThread (Priority::low);
+    }
+
+private:
+    void run() override
+    {
+        setProgress (-1.0);
+
+        MemoryBlock zipData;
+        auto result = download (zipData);
+
+        if (result.wasOk() && ! threadShouldExit())
+            result = install (zipData);
+
+        MessageManager::callAsync ([result, callback = completionCallback]
+        {
+            callback (result);
+        });
+    }
+
+    Result download (MemoryBlock& dest)
+    {
+        setStatusMessage ("Downloading...");
+
+        int statusCode = 0;
+        auto inStream = VersionInfo::createInputStreamForAsset (asset, statusCode);
+
+        if (inStream != nullptr && statusCode == 200)
+        {
+            int64 total = 0;
+            MemoryOutputStream mo (dest, true);
+
+            for (;;)
+            {
+                if (threadShouldExit())
+                    return Result::fail ("Cancelled");
+
+                auto written = mo.writeFromInputStream (*inStream, 8192);
+
+                if (written == 0)
+                    break;
+
+                total += written;
+
+                setStatusMessage ("Downloading... " + File::descriptionOfSizeInBytes (total));
+            }
+
+            return Result::ok();
+        }
+
+        return Result::fail ("Failed to download from: " + asset.url);
+    }
+
+    Result install (const MemoryBlock& data)
+    {
+        setStatusMessage ("Installing...");
+
+        MemoryInputStream input (data, false);
+        ZipFile zip (input);
+
+        if (zip.getNumEntries() == 0)
+            return Result::fail ("The downloaded file was not a valid JUCE file!");
+
+        struct ScopedDownloadFolder
+        {
+            explicit ScopedDownloadFolder (const File& installTargetFolder)
+            {
+                folder = installTargetFolder.getSiblingFile (installTargetFolder.getFileNameWithoutExtension() + "_download").getNonexistentSibling();
+                jassert (folder.createDirectory());
+            }
+
+            ~ScopedDownloadFolder()   { folder.deleteRecursively(); }
+
+            File folder;
+        };
+
+        ScopedDownloadFolder unzipTarget (targetFolder);
+
+        if (! unzipTarget.folder.isDirectory())
+            return Result::fail ("Couldn't create a temporary folder to unzip the new version!");
+
+        auto r = zip.uncompressTo (unzipTarget.folder);
+
+        if (r.failed())
+            return r;
+
+        if (threadShouldExit())
+            return Result::fail ("Cancelled");
+
+       #if JUCE_LINUX || JUCE_BSD || JUCE_MAC
+        r = setFilePermissions (unzipTarget.folder, zip);
+
+        if (r.failed())
+            return r;
+
+        if (threadShouldExit())
+            return Result::fail ("Cancelled");
+       #endif
+
+        if (targetFolder.exists())
+        {
+            auto oldFolder = targetFolder.getSiblingFile (targetFolder.getFileNameWithoutExtension() + "_old").getNonexistentSibling();
+
+            if (! targetFolder.moveFileTo (oldFolder))
+                return Result::fail ("Could not remove the existing folder!\n\n"
+                                     "This may happen if you are trying to download into a directory that requires administrator privileges to modify.\n"
+                                     "Please select a folder that is writable by the current user.");
+        }
+
+        if (! unzipTarget.folder.getChildFile ("JUCE").moveFileTo (targetFolder))
+            return Result::fail ("Could not overwrite the existing folder!\n\n"
+                                 "This may happen if you are trying to download into a directory that requires administrator privileges to modify.\n"
+                                 "Please select a folder that is writable by the current user.");
+
+        return Result::ok();
+    }
+
+    Result setFilePermissions (const File& root, const ZipFile& zip)
+    {
+        constexpr uint32 executableFlag = (1 << 22);
+
+        for (int i = 0; i < zip.getNumEntries(); ++i)
+        {
+            auto* entry = zip.getEntry (i);
+
+            if ((entry->externalFileAttributes & executableFlag) != 0 && entry->filename.getLastCharacter() != '/')
+            {
+                auto exeFile = root.getChildFile (entry->filename);
+
+                if (! exeFile.exists())
+                    return Result::fail ("Failed to find executable file when setting permissions " + exeFile.getFileName());
+
+                if (! exeFile.setExecutePermission (true))
+                    return Result::fail ("Failed to set executable file permission for " + exeFile.getFileName());
+            }
+        }
+
+        return Result::ok();
+    }
+
+    VersionInfo::Asset asset;
+    File targetFolder;
+    std::function<void (Result)> completionCallback;
+};
+
 //==============================================================================
 LatestVersionCheckerAndUpdater::LatestVersionCheckerAndUpdater()
     : Thread ("VersionChecker")
@@ -394,158 +546,6 @@ void LatestVersionCheckerAndUpdater::addNotificationToOpenProjects (const Versio
 }
 
 //==============================================================================
-class DownloadAndInstallThread final : private ThreadWithProgressWindow
-{
-public:
-    DownloadAndInstallThread  (const VersionInfo::Asset& a, const File& t, std::function<void (Result)>&& cb)
-        : ThreadWithProgressWindow ("Downloading New Version", true, true),
-          asset (a), targetFolder (t), completionCallback (std::move (cb))
-    {
-        launchThread (Priority::low);
-    }
-
-private:
-    void run() override
-    {
-        setProgress (-1.0);
-
-        MemoryBlock zipData;
-        auto result = download (zipData);
-
-        if (result.wasOk() && ! threadShouldExit())
-            result = install (zipData);
-
-        MessageManager::callAsync ([result, callback = completionCallback]
-        {
-            callback (result);
-        });
-    }
-
-    Result download (MemoryBlock& dest)
-    {
-        setStatusMessage ("Downloading...");
-
-        int statusCode = 0;
-        auto inStream = VersionInfo::createInputStreamForAsset (asset, statusCode);
-
-        if (inStream != nullptr && statusCode == 200)
-        {
-            int64 total = 0;
-            MemoryOutputStream mo (dest, true);
-
-            for (;;)
-            {
-                if (threadShouldExit())
-                    return Result::fail ("Cancelled");
-
-                auto written = mo.writeFromInputStream (*inStream, 8192);
-
-                if (written == 0)
-                    break;
-
-                total += written;
-
-                setStatusMessage ("Downloading... " + File::descriptionOfSizeInBytes (total));
-            }
-
-            return Result::ok();
-        }
-
-        return Result::fail ("Failed to download from: " + asset.url);
-    }
-
-    Result install (const MemoryBlock& data)
-    {
-        setStatusMessage ("Installing...");
-
-        MemoryInputStream input (data, false);
-        ZipFile zip (input);
-
-        if (zip.getNumEntries() == 0)
-            return Result::fail ("The downloaded file was not a valid JUCE file!");
-
-        struct ScopedDownloadFolder
-        {
-            explicit ScopedDownloadFolder (const File& installTargetFolder)
-            {
-                folder = installTargetFolder.getSiblingFile (installTargetFolder.getFileNameWithoutExtension() + "_download").getNonexistentSibling();
-                jassert (folder.createDirectory());
-            }
-
-            ~ScopedDownloadFolder()   { folder.deleteRecursively(); }
-
-            File folder;
-        };
-
-        ScopedDownloadFolder unzipTarget (targetFolder);
-
-        if (! unzipTarget.folder.isDirectory())
-            return Result::fail ("Couldn't create a temporary folder to unzip the new version!");
-
-        auto r = zip.uncompressTo (unzipTarget.folder);
-
-        if (r.failed())
-            return r;
-
-        if (threadShouldExit())
-            return Result::fail ("Cancelled");
-
-       #if JUCE_LINUX || JUCE_BSD || JUCE_MAC
-        r = setFilePermissions (unzipTarget.folder, zip);
-
-        if (r.failed())
-            return r;
-
-        if (threadShouldExit())
-            return Result::fail ("Cancelled");
-       #endif
-
-        if (targetFolder.exists())
-        {
-            auto oldFolder = targetFolder.getSiblingFile (targetFolder.getFileNameWithoutExtension() + "_old").getNonexistentSibling();
-
-            if (! targetFolder.moveFileTo (oldFolder))
-                return Result::fail ("Could not remove the existing folder!\n\n"
-                                     "This may happen if you are trying to download into a directory that requires administrator privileges to modify.\n"
-                                     "Please select a folder that is writable by the current user.");
-        }
-
-        if (! unzipTarget.folder.getChildFile ("JUCE").moveFileTo (targetFolder))
-            return Result::fail ("Could not overwrite the existing folder!\n\n"
-                                 "This may happen if you are trying to download into a directory that requires administrator privileges to modify.\n"
-                                 "Please select a folder that is writable by the current user.");
-
-        return Result::ok();
-    }
-
-    Result setFilePermissions (const File& root, const ZipFile& zip)
-    {
-        constexpr uint32 executableFlag = (1 << 22);
-
-        for (int i = 0; i < zip.getNumEntries(); ++i)
-        {
-            auto* entry = zip.getEntry (i);
-
-            if ((entry->externalFileAttributes & executableFlag) != 0 && entry->filename.getLastCharacter() != '/')
-            {
-                auto exeFile = root.getChildFile (entry->filename);
-
-                if (! exeFile.exists())
-                    return Result::fail ("Failed to find executable file when setting permissions " + exeFile.getFileName());
-
-                if (! exeFile.setExecutePermission (true))
-                    return Result::fail ("Failed to set executable file permission for " + exeFile.getFileName());
-            }
-        }
-
-        return Result::ok();
-    }
-
-    VersionInfo::Asset asset;
-    File targetFolder;
-    std::function<void (Result)> completionCallback;
-};
-
 static void restartProcess (const File& targetFolder)
 {
    #if JUCE_MAC || JUCE_LINUX || JUCE_BSD

extras/Projucer/Source/CodeEditor/jucer_SourceCodeEditor.cpp
@@ -275,6 +275,89 @@ void SourceCodeEditor::valueTreeRedirected (ValueTree&)                         
 void SourceCodeEditor::codeDocumentTextInserted (const String&, int)              { checkSaveState(); }
 void SourceCodeEditor::codeDocumentTextDeleted (int, int)                         { checkSaveState(); }
 
+class GenericCodeEditorComponent::FindPanel final : public Component
+{
+public:
+    FindPanel()
+    {
+        editor.setColour (CaretComponent::caretColourId, Colours::black);
+
+        addAndMakeVisible (editor);
+        label.setColour (Label::textColourId, Colours::white);
+        label.attachToComponent (&editor, false);
+
+        addAndMakeVisible (caseButton);
+        caseButton.setColour (ToggleButton::textColourId, Colours::white);
+        caseButton.setToggleState (isCaseSensitiveSearch(), dontSendNotification);
+        caseButton.onClick = [this] { setCaseSensitiveSearch (caseButton.getToggleState()); };
+
+        findPrev.setConnectedEdges (Button::ConnectedOnRight);
+        findNext.setConnectedEdges (Button::ConnectedOnLeft);
+        addAndMakeVisible (findPrev);
+        addAndMakeVisible (findNext);
+
+        setWantsKeyboardFocus (false);
+        setFocusContainerType (FocusContainerType::keyboardFocusContainer);
+        findPrev.setWantsKeyboardFocus (false);
+        findNext.setWantsKeyboardFocus (false);
+
+        editor.setText (getSearchString());
+        editor.onTextChange = [this] { changeSearchString(); };
+        editor.onReturnKey  = [] { ProjucerApplication::getCommandManager().invokeDirectly (CommandIDs::findNext, true); };
+        editor.onEscapeKey  = [this]
+        {
+            if (auto* ed = getOwner())
+                ed->hideFindPanel();
+        };
+    }
+
+    void setCommandManager (ApplicationCommandManager* cm)
+    {
+        findPrev.setCommandToTrigger (cm, CommandIDs::findPrevious, true);
+        findNext.setCommandToTrigger (cm, CommandIDs::findNext, true);
+    }
+
+    void paint (Graphics& g) override
+    {
+        Path outline;
+        outline.addRoundedRectangle (1.0f, 1.0f, (float) getWidth() - 2.0f, (float) getHeight() - 2.0f, 8.0f);
+
+        g.setColour (Colours::black.withAlpha (0.6f));
+        g.fillPath (outline);
+        g.setColour (Colours::white.withAlpha (0.8f));
+        g.strokePath (outline, PathStrokeType (1.0f));
+    }
+
+    void resized() override
+    {
+        int y = 30;
+        editor.setBounds (10, y, getWidth() - 20, 24);
+        y += 30;
+        caseButton.setBounds (10, y, getWidth() / 2 - 10, 22);
+        findNext.setBounds (getWidth() - 40, y, 30, 22);
+        findPrev.setBounds (getWidth() - 70, y, 30, 22);
+    }
+
+    void changeSearchString()
+    {
+        setSearchString (editor.getText());
+
+        if (auto* ed = getOwner())
+            ed->findNext (true, false);
+    }
+
+    GenericCodeEditorComponent* getOwner() const
+    {
+        return findParentComponentOfClass <GenericCodeEditorComponent>();
+    }
+
+    TextEditor editor;
+    Label label  { {}, "Find:" };
+    ToggleButton caseButton  { "Case-sensitive" };
+    TextButton findPrev  { "<" },
+               findNext  { ">" };
+};
+
 //==============================================================================
 GenericCodeEditorComponent::GenericCodeEditorComponent (const File& f, CodeDocument& codeDocument,
                                                         CodeTokeniser* tokeniser)
@@ -384,89 +467,6 @@ void GenericCodeEditorComponent::removeListener (GenericCodeEditorComponent::Lis
 }
 
 //==============================================================================
-class GenericCodeEditorComponent::FindPanel final : public Component
-{
-public:
-    FindPanel()
-    {
-        editor.setColour (CaretComponent::caretColourId, Colours::black);
-
-        addAndMakeVisible (editor);
-        label.setColour (Label::textColourId, Colours::white);
-        label.attachToComponent (&editor, false);
-
-        addAndMakeVisible (caseButton);
-        caseButton.setColour (ToggleButton::textColourId, Colours::white);
-        caseButton.setToggleState (isCaseSensitiveSearch(), dontSendNotification);
-        caseButton.onClick = [this] { setCaseSensitiveSearch (caseButton.getToggleState()); };
-
-        findPrev.setConnectedEdges (Button::ConnectedOnRight);
-        findNext.setConnectedEdges (Button::ConnectedOnLeft);
-        addAndMakeVisible (findPrev);
-        addAndMakeVisible (findNext);
-
-        setWantsKeyboardFocus (false);
-        setFocusContainerType (FocusContainerType::keyboardFocusContainer);
-        findPrev.setWantsKeyboardFocus (false);
-        findNext.setWantsKeyboardFocus (false);
-
-        editor.setText (getSearchString());
-        editor.onTextChange = [this] { changeSearchString(); };
-        editor.onReturnKey  = [] { ProjucerApplication::getCommandManager().invokeDirectly (CommandIDs::findNext, true); };
-        editor.onEscapeKey  = [this]
-        {
-            if (auto* ed = getOwner())
-                ed->hideFindPanel();
-        };
-    }
-
-    void setCommandManager (ApplicationCommandManager* cm)
-    {
-        findPrev.setCommandToTrigger (cm, CommandIDs::findPrevious, true);
-        findNext.setCommandToTrigger (cm, CommandIDs::findNext, true);
-    }
-
-    void paint (Graphics& g) override
-    {
-        Path outline;
-        outline.addRoundedRectangle (1.0f, 1.0f, (float) getWidth() - 2.0f, (float) getHeight() - 2.0f, 8.0f);
-
-        g.setColour (Colours::black.withAlpha (0.6f));
-        g.fillPath (outline);
-        g.setColour (Colours::white.withAlpha (0.8f));
-        g.strokePath (outline, PathStrokeType (1.0f));
-    }
-
-    void resized() override
-    {
-        int y = 30;
-        editor.setBounds (10, y, getWidth() - 20, 24);
-        y += 30;
-        caseButton.setBounds (10, y, getWidth() / 2 - 10, 22);
-        findNext.setBounds (getWidth() - 40, y, 30, 22);
-        findPrev.setBounds (getWidth() - 70, y, 30, 22);
-    }
-
-    void changeSearchString()
-    {
-        setSearchString (editor.getText());
-
-        if (auto* ed = getOwner())
-            ed->findNext (true, false);
-    }
-
-    GenericCodeEditorComponent* getOwner() const
-    {
-        return findParentComponentOfClass <GenericCodeEditorComponent>();
-    }
-
-    TextEditor editor;
-    Label label  { {}, "Find:" };
-    ToggleButton caseButton  { "Case-sensitive" };
-    TextButton findPrev  { "<" },
-               findNext  { ">" };
-};
-
 void GenericCodeEditorComponent::resized()
 {
     CodeEditorComponent::resized();

modules/juce_audio_devices/juce_audio_devices.cpp
@@ -58,7 +58,7 @@
 #include "juce_audio_devices.h"
 
 #include "audio_io/juce_SampleRateHelpers.cpp"
-#include "midi_io/juce_MidiDevices.cpp"
+#include "midi_io/juce_MidiDeviceListConnectionBroadcaster.cpp"
 
 //==============================================================================
 #if JUCE_MAC || JUCE_IOS
@@ -257,6 +257,8 @@ namespace juce
  #endif
 
 #endif
+
+#include "midi_io/juce_MidiDevices.cpp"
 
 #if ! JUCE_SYSTEMAUDIOVOL_IMPLEMENTED
 namespace juce

modules/juce_audio_devices/midi_io/juce_MidiDeviceListConnectionBroadcaster.cpp
@@ -1,0 +1,115 @@
+/*
+  ==============================================================================
+
+   This file is part of the JUCE framework.
+   Copyright (c) Raw Material Software Limited
+
+   JUCE is an open source framework subject to commercial or open source
+   licensing.
+
+   By downloading, installing, or using the JUCE framework, or combining the
+   JUCE framework with any other source code, object code, content or any other
+   copyrightable work, you agree to the terms of the JUCE End User Licence
+   Agreement, and all incorporated terms including the JUCE Privacy Policy and
+   the JUCE Website Terms of Service, as applicable, which will bind you. If you
+   do not agree to the terms of these agreements, we will not license the JUCE
+   framework to you, and you must discontinue the installation or download
+   process and cease use of the JUCE framework.
+
+   JUCE End User Licence Agreement: https://juce.com/legal/juce-8-licence/
+   JUCE Privacy Policy: https://juce.com/juce-privacy-policy
+   JUCE Website Terms of Service: https://juce.com/juce-website-terms-of-service/
+
+   Or:
+
+   You may also use this code under the terms of the AGPLv3:
+   https://www.gnu.org/licenses/agpl-3.0.en.html
+
+   THE JUCE FRAMEWORK IS PROVIDED "AS IS" WITHOUT ANY WARRANTY, AND ALL
+   WARRANTIES, WHETHER EXPRESSED OR IMPLIED, INCLUDING WARRANTY OF
+   MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE, ARE DISCLAIMED.
+
+  ==============================================================================
+*/
+
+namespace juce
+{
+
+class MidiDeviceListConnectionBroadcaster final : private AsyncUpdater
+{
+public:
+    ~MidiDeviceListConnectionBroadcaster() override
+    {
+        cancelPendingUpdate();
+    }
+
+    MidiDeviceListConnection::Key add (std::function<void()> callback)
+    {
+        JUCE_ASSERT_MESSAGE_THREAD
+        return callbacks.emplace (key++, std::move (callback)).first->first;
+    }
+
+    void remove (const MidiDeviceListConnection::Key k)
+    {
+        JUCE_ASSERT_MESSAGE_THREAD
+        callbacks.erase (k);
+    }
+
+    void notify()
+    {
+        if (MessageManager::getInstance()->isThisTheMessageThread())
+        {
+            cancelPendingUpdate();
+
+            const State newState;
+
+            if (std::exchange (lastNotifiedState, newState) != newState)
+                for (auto it = callbacks.begin(); it != callbacks.end();)
+                    NullCheckedInvocation::invoke ((it++)->second);
+        }
+        else
+        {
+            triggerAsyncUpdate();
+        }
+    }
+
+    static auto& get()
+    {
+        static MidiDeviceListConnectionBroadcaster result;
+        return result;
+    }
+
+private:
+    MidiDeviceListConnectionBroadcaster() = default;
+
+    class State
+    {
+        Array<MidiDeviceInfo> ins  = MidiInput::getAvailableDevices(),
+                              outs = MidiOutput::getAvailableDevices();
+        auto tie() const
+        {
+            return std::tie (ins, outs);
+        }
+
+    public:
+        bool operator== (const State& other) const
+        {
+            return tie() == other.tie();
+        }
+        bool operator!= (const State& other) const
+        {
+            return tie() != other.tie();
+        }
+    };
+
+    void handleAsyncUpdate() override
+    {
+        notify();
+    }
+
+    std::map<MidiDeviceListConnection::Key, std::function<void()>> callbacks;
+    State lastNotifiedState;
+    MidiDeviceListConnection::Key key = 0;
+};
+
+} // namespace juce

modules/juce_audio_devices/midi_io/juce_MidiDevices.cpp
@@ -35,74 +35,6 @@
 namespace juce
 {
 
-class MidiDeviceListConnectionBroadcaster final : private AsyncUpdater
-{
-public:
-    ~MidiDeviceListConnectionBroadcaster() override
-    {
-        cancelPendingUpdate();
-    }
-
-    MidiDeviceListConnection::Key add (std::function<void()> callback)
-    {
-        JUCE_ASSERT_MESSAGE_THREAD
-        return callbacks.emplace (key++, std::move (callback)).first->first;
-    }
-
-    void remove (const MidiDeviceListConnection::Key k)
-    {
-        JUCE_ASSERT_MESSAGE_THREAD
-        callbacks.erase (k);
-    }
-
-    void notify()
-    {
-        if (MessageManager::getInstance()->isThisTheMessageThread())
-        {
-            cancelPendingUpdate();
-
-            const State newState;
-
-            if (std::exchange (lastNotifiedState, newState) != newState)
-                for (auto it = callbacks.begin(); it != callbacks.end();)
-                    NullCheckedInvocation::invoke ((it++)->second);
-        }
-        else
-        {
-            triggerAsyncUpdate();
-        }
-    }
-
-    static auto& get()
-    {
-        static MidiDeviceListConnectionBroadcaster result;
-        return result;
-    }
-
-private:
-    MidiDeviceListConnectionBroadcaster() = default;
-
-    class State
-    {
-        Array<MidiDeviceInfo> ins = MidiInput::getAvailableDevices(), outs = MidiOutput::getAvailableDevices();
-        auto tie() const { return std::tie (ins, outs); }
-
-    public:
-        bool operator== (const State& other) const { return tie() == other.tie(); }
-        bool operator!= (const State& other) const { return tie() != other.tie(); }
-    };
-
-    void handleAsyncUpdate() override
-    {
-        notify();
-    }
-
-    std::map<MidiDeviceListConnection::Key, std::function<void()>> callbacks;
-    State lastNotifiedState;
-    MidiDeviceListConnection::Key key = 0;
-};
-
-//==============================================================================
 MidiDeviceListConnection::~MidiDeviceListConnection() noexcept
 {
     if (broadcaster != nullptr)

commit 402bafda0d1c32c8ec6840f324cbfca587d465a8

Fix BSD build
This fixes a regression in
5ce2fc388e25cdf85b9fec5360800a513d49ae1f.

modules/juce_gui_extra/juce_gui_extra.cpp
@@ -154,7 +154,7 @@
 #elif JUCE_WINDOWS
  #include "native/juce_ActiveXComponent_windows.cpp"
  #include "native/juce_SystemTrayIcon_windows.cpp"
-#elif JUCE_LINUX
+#elif JUCE_LINUX || JUCE_BSD
  #include "native/juce_SystemTrayIcon_linux.cpp"
 #endif
 