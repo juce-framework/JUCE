17a3d0efe467239a11b7207329a0116ee446949d
Tom Poole <tom@juce.com>
2024-10-15 22:19:12 +0100

    Resave all projects

--- examples/DemoRunner/JuceLibraryCode/JuceHeader.h
+++ examples/DemoRunner/JuceLibraryCode/JuceHeader.h
@@ -55,7 +55,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "DemoRunner";
     const char* const  companyName    = "Raw Material Software Limited";
-    const char* const  versionString  = "8.0.2";
-    const int          versionNumber  = 0x80002;
+    const char* const  versionString  = "8.0.3";
+    const int          versionNumber  = 0x80003;
 }
 #endif
