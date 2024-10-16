b292ba215a98d79ca9e22fd6b0b8c6b5652c9a82
reuk <reuk@users.noreply.github.com>
2024-10-16 15:32:02 +0100

    Component: Ensure global mouse listeners are notified on mouse up
    Previously, listeners would sometimes fail to receive mouse up events
    blocked by modal components

--- modules/juce_gui_basics/components/juce_Component.cpp
+++ modules/juce_gui_basics/components/juce_Component.cpp
@@ -2226,9 +2226,6 @@ void Component::internalMouseUp (MouseInputSource source,
                                  Time time,
                                  const ModifierKeys oldModifiers)
 {
-    if (flags.mouseDownWasBlocked && isCurrentlyBlockedByAnotherModalComponent())
-        return;
-
     const auto me = makeMouseEvent (source,
                                     relativePointerState,
                                     oldModifiers,
@@ -2241,6 +2238,14 @@ void Component::internalMouseUp (MouseInputSource source,
                                     source.isLongPressOrDrag());
 
     HierarchyChecker checker (this, me);
+
+    if (flags.mouseDownWasBlocked && isCurrentlyBlockedByAnotherModalComponent())
+    {
+        // Global listeners still need to know about the mouse up
+        auto& desktop = Desktop::getInstance();
+        desktop.getMouseListeners().callChecked (checker, [&] (MouseListener& l) { l.mouseUp (checker.eventWithNearestParent()); });
+        return;
+    }
 
     if (flags.repaintOnMouseActivityFlag)
         repaint();
