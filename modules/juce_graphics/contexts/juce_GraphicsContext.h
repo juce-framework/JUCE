15f05443d0b84d58f1c2ac504609e7e6fac31228
reuk <reuk@users.noreply.github.com>
2025-06-03 11:58:59 +0100

    Graphics: Add missing JUCE_API annotation to ScopedSaveState

--- modules/juce_graphics/contexts/juce_GraphicsContext.h
+++ modules/juce_graphics/contexts/juce_GraphicsContext.h
@@ -667,7 +667,7 @@ public:
         On construction, this calls Graphics::saveState(), and on destruction it calls
         Graphics::restoreState() on the Graphics object that you supply.
     */
-    class ScopedSaveState
+    class JUCE_API ScopedSaveState
     {
     public:
         ScopedSaveState (Graphics&);
