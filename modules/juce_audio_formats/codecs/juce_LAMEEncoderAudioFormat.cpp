juce-framework/JUCE

commit 1ce35453db40d61fc090a1ba9e765a95671031fb

Deprecate the old AudioFormat::createWriterFor functions

modules/juce_audio_formats/codecs/juce_LAMEEncoderAudioFormat.cpp
@@ -234,30 +234,6 @@ std::unique_ptr<AudioFormatWriter> LAMEEncoderAudioFormat::createWriterFor (std:
                                      options.getMetadataValues());
 }
 
-AudioFormatWriter* LAMEEncoderAudioFormat::createWriterFor (OutputStream* streamToWriteTo,
-                                                            double sampleRateToUse,
-                                                            unsigned int numberOfChannels,
-                                                            int bitsPerSample,
-                                                            const StringPairArray& metadataValues,
-                                                            int qualityOptionIndex)
-{
-    if (streamToWriteTo == nullptr)
-        return nullptr;
-
-    int vbr = 4;
-    int cbr = 0;
-
-    const String qual (getQualityOptions() [qualityOptionIndex]);
-
-    if (qual.contains ("VBR"))
-        vbr = qual.retainCharacters ("0123456789").getIntValue();
-    else
-        cbr = qual.getIntValue();
-
-    return new Writer (streamToWriteTo, getFormatName(), lameApp, vbr, cbr,
-                       sampleRateToUse, numberOfChannels, bitsPerSample, metadataValues);
-}
-
 #endif
 
 } // namespace juce

modules/juce_audio_formats/codecs/juce_LAMEEncoderAudioFormat.h
@@ -74,10 +74,6 @@ public:
 
     AudioFormatReader* createReaderFor (InputStream*, bool deleteStreamIfOpeningFails) override;
 
-    AudioFormatWriter* createWriterFor (OutputStream*, double sampleRateToUse,
-                                        unsigned int numberOfChannels, int bitsPerSample,
-                                        const StringPairArray& metadataValues, int qualityOptionIndex);
-
     using AudioFormat::createWriterFor;
 
 private:

modules/juce_audio_formats/codecs/juce_AiffAudioFormat.cpp
@@ -1017,20 +1017,6 @@ MemoryMappedAudioFormatReader* AiffAudioFormat::createMemoryMappedReader (FileIn
     return nullptr;
 }
 
-AudioFormatWriter* AiffAudioFormat::createWriterFor (OutputStream* out,
-                                                     double sampleRate,
-                                                     unsigned int numberOfChannels,
-                                                     int bitsPerSample,
-                                                     const StringPairArray& metadataValues,
-                                                     int /*qualityOptionIndex*/)
-{
-    if (out != nullptr && getPossibleBitDepths().contains (bitsPerSample))
-        return new AiffAudioFormatWriter (out, sampleRate, numberOfChannels,
-                                          (unsigned int) bitsPerSample, metadataValues);
-
-    return nullptr;
-}
-
 std::unique_ptr<AudioFormatWriter> AiffAudioFormat::createWriterFor (std::unique_ptr<OutputStream>& streamToWriteTo,
                                                                      const AudioFormatWriterOptions& options)
 {

modules/juce_audio_formats/codecs/juce_AiffAudioFormat.h
@@ -88,13 +88,6 @@ public:
     MemoryMappedAudioFormatReader* createMemoryMappedReader (const File&)      override;
     MemoryMappedAudioFormatReader* createMemoryMappedReader (FileInputStream*) override;
 
-    AudioFormatWriter* createWriterFor (OutputStream* streamToWriteTo,
-                                        double sampleRateToUse,
-                                        unsigned int numberOfChannels,
-                                        int bitsPerSample,
-                                        const StringPairArray& metadataValues,
-                                        int qualityOptionIndex) override;
-
     std::unique_ptr<AudioFormatWriter> createWriterFor (std::unique_ptr<OutputStream>& streamToWriteTo,
                                                         const AudioFormatWriterOptions& options) override;
 

modules/juce_audio_formats/codecs/juce_CoreAudioFormat.cpp
@@ -654,17 +654,6 @@ AudioFormatReader* CoreAudioFormat::createReaderFor (InputStream* sourceStream,
     return nullptr;
 }
 
-AudioFormatWriter* CoreAudioFormat::createWriterFor (OutputStream*,
-                                                     double /*sampleRateToUse*/,
-                                                     unsigned int /*numberOfChannels*/,
-                                                     int /*bitsPerSample*/,
-                                                     const StringPairArray& /*metadataValues*/,
-                                                     int /*qualityOptionIndex*/)
-{
-    jassertfalse; // not yet implemented!
-    return nullptr;
-}
-
 std::unique_ptr<AudioFormatWriter> CoreAudioFormat::createWriterFor (std::unique_ptr<OutputStream>&,
                                                                      const AudioFormatWriterOptions&)
 {

modules/juce_audio_formats/codecs/juce_CoreAudioFormat.h
@@ -106,13 +106,6 @@ public:
     AudioFormatReader* createReaderFor (InputStream*,
                                         bool deleteStreamIfOpeningFails) override;
 
-    AudioFormatWriter* createWriterFor (OutputStream*,
-                                        double sampleRateToUse,
-                                        unsigned int numberOfChannels,
-                                        int bitsPerSample,
-                                        const StringPairArray& metadataValues,
-                                        int qualityOptionIndex) override;
-
     std::unique_ptr<AudioFormatWriter> createWriterFor (std::unique_ptr<OutputStream>& streamToWriteTo,
                                                         const AudioFormatWriterOptions& options) override;
 

modules/juce_audio_formats/codecs/juce_FlacAudioFormat.cpp
@@ -600,24 +600,6 @@ AudioFormatReader* FlacAudioFormat::createReaderFor (InputStream* in, const bool
     return nullptr;
 }
 
-AudioFormatWriter* FlacAudioFormat::createWriterFor (OutputStream* out,
-                                                     double sampleRate,
-                                                     unsigned int numberOfChannels,
-                                                     int bitsPerSample,
-                                                     const StringPairArray& /*metadataValues*/,
-                                                     int qualityOptionIndex)
-{
-    if (out != nullptr && getPossibleBitDepths().contains (bitsPerSample))
-    {
-        std::unique_ptr<FlacWriter> w (new FlacWriter (out, sampleRate, numberOfChannels,
-                                                       (uint32) bitsPerSample, qualityOptionIndex));
-        if (w->ok)
-            return w.release();
-    }
-
-    return nullptr;
-}
-
 std::unique_ptr<AudioFormatWriter> FlacAudioFormat::createWriterFor (std::unique_ptr<OutputStream>& streamToWriteTo,
                                                                      const AudioFormatWriterOptions& options)
 {

modules/juce_audio_formats/codecs/juce_FlacAudioFormat.h
@@ -69,13 +69,6 @@ public:
     AudioFormatReader* createReaderFor (InputStream* sourceStream,
                                         bool deleteStreamIfOpeningFails) override;
 
-    AudioFormatWriter* createWriterFor (OutputStream* streamToWriteTo,
-                                        double sampleRateToUse,
-                                        unsigned int numberOfChannels,
-                                        int bitsPerSample,
-                                        const StringPairArray& metadataValues,
-                                        int qualityOptionIndex) override;
-
     using AudioFormat::createWriterFor;
 
 private:

commit f371fecb344406b0a3e7871d65aed15490e488a1

Fix compilation when JUCE_USE_LAME_AUDIO_FORMAT=1
This fixes a regression in 1ce35453db40d61fc090a1ba9e765a95671031fb.

modules/juce_audio_formats/codecs/juce_LAMEEncoderAudioFormat.cpp
@@ -43,14 +43,15 @@ public:
     Writer (OutputStream* destStream, const String& formatName,
             const File& appFile, int vbr, int cbr,
             double sampleRateIn, unsigned int numberOfChannels,
-            int bitsPerSampleIn, const StringPairArray& metadata)
+            int bitsPerSampleIn, const StringMap& metadata)
         : AudioFormatWriter (destStream, formatName, sampleRateIn,
                              numberOfChannels, (unsigned int) bitsPerSampleIn),
           vbrLevel (vbr), cbrBitrate (cbr)
     {
         WavAudioFormat wavFormat;
 
-        writer = wavFormat.createWriterFor (tempWav.getFile().createOutputStream(),
+        std::unique_ptr<OutputStream> stream = tempWav.getFile().createOutputStream();
+        writer = wavFormat.createWriterFor (stream,
                                             AudioFormatWriter::Options{}.withSampleRate (sampleRateIn)
                                                                         .withNumChannels ((int) numberOfChannels)
                                                                         .withBitsPerSample (bitsPerSampleIn)
@@ -85,14 +86,12 @@ public:
         addMetadataArg (metadata, "id3trackNumber", "--tn");
     }
 
-    void addMetadataArg (const StringPairArray& metadata, const char* key, const char* lameFlag)
+    void addMetadataArg (const StringMap& metadata, const char* key, const char* lameFlag)
     {
-        auto value = metadata.getValue (key, {});
-
-        if (value.isNotEmpty())
+        if (auto it = metadata.find (key); it != metadata.end())
         {
             args.add (lameFlag);
-            args.add (value);
+            args.add (it->second);
         }
     }
 
@@ -207,7 +206,7 @@ AudioFormatReader* LAMEEncoderAudioFormat::createReaderFor (InputStream*, const 
     return nullptr;
 }
 
-std::unique_ptr<AudioFormatWriter> LAMEEncoderAudioFormat::createWriterFor (std::unique_ptr<OutputStream> streamToWriteTo,
+std::unique_ptr<AudioFormatWriter> LAMEEncoderAudioFormat::createWriterFor (std::unique_ptr<OutputStream>& streamToWriteTo,
                                                                             const AudioFormatWriterOptions& options)
 {
     if (streamToWriteTo == nullptr)

modules/juce_audio_formats/codecs/juce_LAMEEncoderAudioFormat.h
@@ -69,7 +69,7 @@ public:
     bool isCompressed() override;
     StringArray getQualityOptions() override;
 
-    std::unique_ptr<AudioFormatWriter> createWriterFor (std::unique_ptr<OutputStream> streamToWriteTo,
+    std::unique_ptr<AudioFormatWriter> createWriterFor (std::unique_ptr<OutputStream>& streamToWriteTo,
                                                         const AudioFormatWriterOptions& options) override;
 
     AudioFormatReader* createReaderFor (InputStream*, bool deleteStreamIfOpeningFails) override;