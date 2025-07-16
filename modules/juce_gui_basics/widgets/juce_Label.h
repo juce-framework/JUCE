26e8d813802bf5d006ddb592603dd0b396cccef3
Sudara <sudara@alonetone.com>
2025-07-16 14:13:19 +0200

    Docs: Clarify that Label get/setFont may be overridden by LookAndFeel
    Thanks to @FigBug for the report.

--- modules/juce_gui_basics/widgets/juce_Label.h
+++ modules/juce_gui_basics/widgets/juce_Label.h
@@ -89,13 +89,15 @@ public:
 
     //==============================================================================
     /** Changes the font to use to draw the text.
-        @see getFont
+        Note that when drawing, the label's LookAndFeel may override the font set here.
+        @see getFont, LookAndFeelMethods
     */
     void setFont (const Font& newFont);
 
-    /** Returns the font currently being used.
-        This may be the one set by setFont(), unless it has been overridden by the current LookAndFeel
-        @see setFont
+    /** Returns the Label's current font.
+        Note that this method will always return the font set by setFont(), even if
+        the LookAndFeel is overriding the label's font during drawing.
+        @see setFont, LookAndFeelMethods
     */
     Font getFont() const noexcept;
 
