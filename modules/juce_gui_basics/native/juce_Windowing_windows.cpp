juce-framework/JUCE

commit 258203706cdfc1357effe8cc752672b793cea37d

Direct2D: Use WM_PAINT and vblank callbacks to drive painting

modules/juce_gui_basics/native/juce_Windowing_windows.cpp
@@ -5082,16 +5082,18 @@ public:
         updateRegion.findRECTAndValidate (peer.getHWND());
 
         for (const auto& rect : updateRegion.getRects())
-            repaint (D2DUtilities::toRectangle (rect));
+            direct2DContext->addDeferredRepaint (D2DUtilities::toRectangle (rect));
 
        #if JUCE_DIRECT2D_METRICS
         lastPaintStartTicks = paintStartTicks;
        #endif
+
+        handleDirect2DPaint();
     }
 
     void repaint (const Rectangle<int>& area) override
     {
-        direct2DContext->addDeferredRepaint (area);
+        deferredRepaints.add (area);
     }
 
     void performAnyPendingRepaintsNow() override {}
@@ -5103,7 +5105,13 @@ public:
 
     void onVBlank() override
     {
-        handleDirect2DPaint();
+        for (auto deferredRect : deferredRepaints)
+        {
+            auto r = D2DUtilities::toRECT (deferredRect);
+            InvalidateRect (peer.getHWND(), &r, FALSE);
+        }
+
+        deferredRepaints.clear();
     }
 
     void handleShowWindow() override
@@ -5472,6 +5480,7 @@ private:
 
     std::unique_ptr<WrappedD2DHwndContextBase> direct2DContext = getContextForPeer (peer);
     UpdateRegion updateRegion;
+    RectangleList<int> deferredRepaints;
 
    #if JUCE_ETW_TRACELOGGING
     struct ETWEventProvider

commit 7685a84c81a9b2a33872e479740220cf1dcbee97

HWNDComponentPeer: Avoid glitches and sluggishness during live resize
258203706cdfc1357effe8cc752672b793cea37d introduced a problem where
live-resize of a window would cause the window content to flash and slow
down.

The cause of the errant behaviour seems to be that, during resize, many
WM_PAINT messages might be dispatched over the course of a single vblank
interval. Then, each of these paint calls was waiting for the next
vblank interval to display. This additional waiting resulted in
sluggishness, since many frames would have to be presented before the
presentation 'caught up' with the current window size. This also meant
that many consecutive frames were presented with stale window size
information.

The solution added here simply checks whether we're live resizing, and
uses the vblank instead of WM_PAINT to synchronise repaints during the
resize operation.

modules/juce_gui_basics/native/juce_Windowing_windows.cpp
@@ -2096,6 +2096,7 @@ public:
     }
 
     bool hasTitleBar() const                 { return (styleFlags & windowHasTitleBar) != 0; }
+    bool isSizing() const                    { return sizing; }
 
 private:
     HWND hwnd, parentToAddTo;
@@ -5077,14 +5078,22 @@ public:
 
         updateRegion.findRECTAndValidate (peer.getHWND());
 
-        for (const auto& rect : updateRegion.getRects())
-            direct2DContext->addDeferredRepaint (D2DUtilities::toRectangle (rect));
+        if (peer.isSizing())
+        {
+            for (const auto& rect : updateRegion.getRects())
+                deferredRepaints.add (D2DUtilities::toRectangle (rect));
+        }
+        else
+        {
+            for (const auto& rect : updateRegion.getRects())
+                direct2DContext->addDeferredRepaint (D2DUtilities::toRectangle (rect));
 
-       #if JUCE_DIRECT2D_METRICS
-        lastPaintStartTicks = paintStartTicks;
-       #endif
+           #if JUCE_DIRECT2D_METRICS
+            lastPaintStartTicks = paintStartTicks;
+           #endif
 
-        handleDirect2DPaint();
+            handleDirect2DPaint();
+        }
     }
 
     void repaint (const Rectangle<int>& area) override
@@ -5101,10 +5110,20 @@ public:
 
     void onVBlank() override
     {
-        for (auto deferredRect : deferredRepaints)
+        if (peer.isSizing())
         {
-            auto r = D2DUtilities::toRECT (deferredRect);
-            InvalidateRect (peer.getHWND(), &r, FALSE);
+            for (const auto& rect : deferredRepaints)
+                direct2DContext->addDeferredRepaint (rect);
+
+            handleDirect2DPaint();
+        }
+        else
+        {
+            for (auto deferredRect : deferredRepaints)
+            {
+                auto r = D2DUtilities::toRECT (deferredRect);
+                InvalidateRect (peer.getHWND(), &r, FALSE);
+            }
         }
 
         deferredRepaints.clear();